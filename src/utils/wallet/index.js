d8512f96059752af17d97adae68285246aea3b8d
joemarct <joemar.ct@gmail.com>
2021-06-26 11:34:38 +0800

    Clean up of old wallet-related functions / scripts

--- src/utils/wallet/index.js
+++ src/utils/wallet/index.js
@@ -2,10 +2,10 @@ import { BchWallet } from './bch'
 import { SlpWallet } from './slp'
 
 class Wallet {
-  constructor () {
-    this.BCH1 = new BchWallet("m/44'/145'/0'/") // Main BCH wallet
-    this.BCH2 = new BchWallet("m/44'/145'/1'/") // Fee funder BCH wallet
-    this.SLP = new SlpWallet("m/44'/245'/0'/") // SLP wallet
+  constructor (projectId, mnemonic) {
+    this.BCH1 = new BchWallet(projectId, mnemonic, "m/44'/145'/0'/") // Main BCH wallet
+    this.BCH2 = new BchWallet(projectId, mnemonic, "m/44'/145'/1'/") // Fee funder BCH wallet
+    this.SLP = new SlpWallet(projectId, mnemonic, "m/44'/245'/0'/") // SLP wallet
   }
 
   async generateMnemonic () {
--- src/utils/wallet/slp.js
+++ src/utils/wallet/slp.js
@@ -1,16 +1,60 @@
 const Watchtower = require('watchtower-cash-js')
+const BCHJS = require('@psf/bch-js')
+const sha256 = require('js-sha256')
+const bchjs = new BCHJS()
 
 class SlpWallet {
-  constructor (path) {
+  constructor (projectId, mnemonic, path) {
+    this.mnemonic = mnemonic
     this.derivationPath = path
     this.watchtower = new Watchtower()
+    this.projectId = projectId
   }
 
-  async newAddress () {}
-  async getPrivateKey () {}
-  async getBalance () {}
+  getWalletHash () {
+    const mnemonicHash = sha256(this.mnemonic)
+    const derivationPath = sha256(this.derivationPath)
+    const walletHash = sha256(mnemonicHash + derivationPath)
+    return walletHash
+  }
+
+  async _getChildNode (index) {
+    const seedBuffer = await bchjs.Mnemonic.toSeed(this.mnemonic)
+    const masterHDNode = bchjs.HDNode.fromSeed(seedBuffer)
+    const childNode = masterHDNode.derivePath(this.derivationPath + index)
+    return childNode
+  }
+
+  async getAddress (index) {
+    const childNode = await this._getChildNode(index)
+    const address = bchjs.HDNode.toSLPAddress(childNode)
+    const data = {
+      address: address,
+      projectId: this.projectId,
+      walletHash: this.walletHash,
+      walletIndex: index
+    }
+    return this.watchtower.subscribe(data)
+  }
+
+  async getPrivateKey (index) {
+    const childNode = await this._getChildNode(index)
+    return bchjs.HDNode.toWIF(childNode)
+  }
+
+  async getBalance (tokenId) {
+    const walletHash = this.getWalletHash()
+    const request = await this.watchtower.Wallet.getBalance({ walletHash, tokenId })
+    return request
+  }
+
+  async getTransactions (tokenId) {
+    const walletHash = this.getWalletHash()
+    const request = await this.watchtower.Wallet.getTransactions({ walletHash, tokenId })
+    return request
+  }
+
   async sendSlp () {}
-  async getTransactions () {}
 }
 
 module.exports = SlpWallet
