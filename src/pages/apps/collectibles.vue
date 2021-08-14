fd7437fdb23e008ddca18b2765cb7b91de994d28
joemarct <joemar.ct@gmail.com>
2021-08-14 16:40:09 +0800

    Use token ID gravatar as default image for collectible tokens

--- src/pages/apps/collectibles.vue
+++ src/pages/apps/collectibles.vue
@@ -1,17 +1,42 @@
 <template>
   <div>
     <header-nav title="Collectibles" backnavpath="/apps" style="position: fixed; top: 0; z-index: 10 !important; background: #ECF3F3;"></header-nav>
-    <div style="margin-top:70px; padding: 30px;">
-      <q-card
-        v-for="(collectible, index) in collectibles"
-        :key="index"
-        class="collectible-card"
-      >
-        <img :src="collectible.image_url">
-        <q-card-section style="text-align: center;">
-          <div class="text-h6">{{ collectible.name }}</div>
-        </q-card-section>
-      </q-card>
+    <div style="margin-top:70px; padding: 10px 50px;">
+      <template v-if="collectibles.length > 0">
+        <q-card
+          v-for="(collectible, index) in collectibles"
+          :key="index"
+          class="collectible-card"
+        >
+          <template v-if="collectible.image_url.length > 0">
+            <img :src="collectible.image_url">
+          </template>
+          <template v-else>
+            <gravatar
+              :hash="collectible.token_id"
+            />
+          </template>
+          <q-card-section style="text-align: center;">
+            <div class="text-h6">{{ collectible.name }}</div>
+            <a
+              :href="'https://simpleledger.info/#token/' + collectible.token_id"
+              style="text-decoration: none; color: gray;"
+              target="_blank"
+            >
+              {{ collectible.token_id.slice(0, 15) }}
+              <q-icon name="exit_to_app" />
+            </a>
+          </q-card-section>
+        </q-card>
+      </template>
+      <template v-else>
+        <p style="font-size: 24px; color: gray; text-align: center; margin-top: 30px;">
+          You don't own any collectibles yet. You can buy some from
+          <a href="https://juungle.net" target="_blank" style="text-decoration: none;">
+            Juungle
+          </a>.
+        </p>
+      </template>
     </div>
   </div>
 </template>
@@ -19,10 +44,11 @@
 <script>
 import HeaderNav from '../../components/header-nav'
 import { getMnemonic, Wallet } from '../../utils/wallet'
+import Gravatar from 'vue-gravatar'
 
 export default {
   name: 'app-wallet-info',
-  components: { HeaderNav },
+  components: { HeaderNav, Gravatar },
   data () {
     return {
       collectibles: [],
@@ -36,7 +62,6 @@ export default {
       vm.wallet.SLP.getCollectibles().then(function (collectibles) {
         vm.collectibles = collectibles
         vm.collectiblesLoaded = true
-        console.log(vm.collectibles)
       })
     }
   },
@@ -44,8 +69,15 @@ export default {
     const vm = this
     getMnemonic().then(function (mnemonic) {
       vm.wallet = new Wallet(mnemonic)
-      vm.getCollectibles()
+      // vm.getCollectibles()
     })
   }
 }
 </script>
+
+<style scoped>
+.collectible-card {
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+</style>
