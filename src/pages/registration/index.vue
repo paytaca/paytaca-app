51d44cba39e66c45eedfab1b7912867ac24988e4
chie_ai <bacuetesarchie@gmail.com>
2021-11-29 18:46:27 +0800

    On goind development on fingerprint authentication

--- src/pages/registration/index.vue
+++ src/pages/registration/index.vue
@@ -55,6 +55,7 @@
 
 <script>
 import Loader from '../../components/loader'
+import { NativeBiometric } from 'capacitor-native-biometric'
 
 export default {
   name: 'registration-index',
@@ -69,9 +70,27 @@ export default {
     submitForm (event) {
       event.preventDefault()
       this.$router.push('/registration/accounts')
+    },
+    fingerPrintAuth () {
+      NativeBiometric.isAvailable()
+        .then(result => {
+          console.log('Biometric type: ', result.biometryType)
+        })
+        .catch(_err => {
+          console.log('Error: ', _err)
+        })
     }
   },
-  created () {
+  async created () {
+    NativeBiometric.isAvailable()
+      .then(res => {
+        console.log('Is available: ', res.isAvailable)
+        this.fingerPrintAuth()
+      })
+      .catch(_err => {
+        console.log('Error: ', _err)
+      })
+
     this.$q.dark.set(false)
     const vm = this
     vm.$axios.get('https://watchtower.cash', { timeout: 30000 }).then(function (response) {
