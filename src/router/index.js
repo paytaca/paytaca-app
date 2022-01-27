052afd820565d919909759737c55c7fdedd1013c
khirvy019 <khirvy_19@yahoo.com>
2022-01-27 16:17:37 +0800

    fix bug unable to resolve deep link intent when using custom url scheme

--- src/router/index.js
+++ src/router/index.js
@@ -43,11 +43,13 @@ export default function ({ store }) {
       next()
     }
   })
+  console.log(process.env)
+  console.log(Plugins)
 
   Plugins.App.addListener('appUrlOpen', function (event) {
     const url = new URL(event.url)
     console.log(url)
-    if (/^\/payment-request\/?$/.test(url.pathname) || /^\/apps\/connecta\/?$/.test(url.pathname)) {
+    if (/\/payment-request\/?$/.test(url.pathname) || /\/apps\/connecta\/?$/.test(url.pathname)) {
       const query = {}
       if (url.searchParams.has('d')) query.paymentRequestData = url.searchParams.get('d')
       else if (url.searchParams.has('orderId')) query.orderId = url.searchParams.get('orderId')
