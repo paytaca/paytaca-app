628f6652aa1aea40720bc0032f7920867315a574
khirvy019 <khirvy_19@yahoo.com>
2022-03-03 16:58:06 +0800

    add function for finding and waiting for outgoing txs in hopcash

--- src/wallet/hopcash.js
+++ src/wallet/hopcash.js
@@ -1,5 +1,5 @@
 import BCHJS from '@psf/bch-js'
-import { utils, BigNumber } from 'ethers'
+import { ethers, utils, BigNumber } from 'ethers'
 import { getProvider } from './sbch/utils'
 
 const bchjs = new BCHJS()
@@ -8,10 +8,36 @@ const provider = getProvider(false)
 const bchBridge = 'bitcoincash:qqa0dj5rwaw2s4tz88m3xmcpjyzry356gglq7zvu80'
 const sbchBridge = '0x3207d65b4D45CF617253467625AF6C1b687F720b'
 
+// address of sender in main chain for s2c outgoing txs, from examples
+const bchSender = 'bitcoincash:qzteyuny2hdvvcd4tu6dktwx9f04jarzkyt57qel0y'
+
+// address of sender in smart chain for c2s outgoing txs, from examples
+const sbchSender = '0xBAe8Af26E08D3332C7163462538B82F0CBe45f2a'
+
+const bridgeContract = new ethers.Contract(
+  sbchSender,
+  [
+    'event Bridged(bytes32 indexed sourceTransaction, address indexed liquidityProviderAddress, address indexed outputAddress, uint256 outputAmount);',
+  ],
+  provider,
+)
+
 function toBigNumber(value) {
   return BigNumber.from('0x' + BigInt(value).toString(16)) 
 }
 
+/**
+ * To reverse value set feeInfo:
+ * nf = -f/(1-p), np = 1 - 1/(1-p), Where:
+ *  nf: new fixed, f = prev fixed, np = new pctg, p = prev pctg     
+ *  
+ * @param {number} amount
+ * @param {Object} feeInfo ex: { pctg: 0.001, fixed: 1 }
+ * @returns {Number}
+ */
+export function deductFromFee(amount, feeInfo={pctg: 0.0, fixed: 0}) {
+  return amount * (1-feeInfo.pctg) - feeInfo.fixed
+}
 
 export async function bchBridgeBalance() {
   const response = await bchjs.Electrumx.balance(bchBridge);
@@ -99,4 +125,173 @@ export async function s2c(wallet, amount, recipientAddress) {
     sbchBridge,
     '0x' + Buffer.from(bchjs.Address.toLegacyAddress(recipientAddress), 'utf8').toString('hex')
   )
+}
+
+export async function findC2SOutgoingTx(txId='') {
+  if (!txId.startsWith('0x')) txId = '0x' + txId
+
+  const eventFilter = bridgeContract.filters.Bridged(txId)
+  const logs = await bridgeContract.provider.send(
+    'sbch_queryLogs',
+    [
+      bridgeContract.address,
+      eventFilter.topics,
+      'latest', // before block
+      '0x0', // after block
+      '0x1', // limit
+    ]
+  )
+
+  if (!Array.isArray(logs)) return {
+    success: false,
+    error: 'Unable to find logs'
+  }
+
+  if (!logs.length) return {
+    success: false,
+    error: 'Not found'
+  }
+
+  const log = logs[0]
+  const parsedLog = bridgeContract.interface.parseLog(log)
+  return {
+    success: true,
+    tx: {
+      hash: log.transactionHash,
+      block: BigNumber.from(log.blockNumber).toNumber(),
+
+      sourceTransaction: parsedLog.args.sourceTransaction,
+      liquidityProviderAddress: parsedLog.args.liquidityProviderAddress,
+      outputAddress: parsedLog.args.outputAddress,
+      outputAmount: utils.formatEther(parsedLog.args.outputAmount),
+
+      _raw: parsedLog
+    },
+  }
+}
+
+export async function findS2COutgoingTx(txId='') {
+  return {
+    success: false,
+    error: 'Not implemented'
+  }
+}
+
+/**
+ * 
+ * @param {String} txId transaction id from the BCH chain
+ * @param {function} callback callback function when bridge transfer for the source tx is made
+ * @returns {function} function to call to stop the listener
+ */
+ export function c2sOutgoingListener(txId='', callback=() =>{}) {
+  if (!txId.startsWith('0x')) txId = '0x' + txId
+
+  const contract = bridgeContract
+  const filter = contract.filters.Bridged(txId)
+  const eventCallback = (...args) => {
+    const tx = args[args.length-1]
+    callback({
+      hash: tx.transactionHash,
+
+      sourceTransaction: tx.args.sourceTransaction,
+      liquidityProviderAddress: tx.args.liquidityProviderAddress,
+      outputAddress: tx.args.outputAddress,
+      outputAmount: utils.formatEther(tx.args.outputAmount),
+
+      _raw: tx,
+    })
+  }
+
+  contract.on(filter, eventCallback)
+  return () => {
+    contract.removeListener(eventFilter, eventCallback) 
+  }
+}
+
+
+function matchOpReturn(txId, txData) {
+  const asmRegex = /^OP_RETURN ([0-9a-f]{64})$/
+  const outputs = txData?.vout
+  if (!Array.isArray(outputs)) return false
+
+  for(var i = 0; i < outputs.length; i++) {
+    const match = String(out?.scriptPubKey?.asm).match(asmRegex)
+    if (!match) continue
+    if (String(txId).substring(2) === match[1]) {
+      return true
+    }
+  }
+  return false
+}
+
+async function matchOpReturnFromHash(txId, txHash) {
+  try {
+    const { details: txData } = await bchjs.Electrumx.txData(txHash)
+    return {
+      success: true,
+      match: matchOpReturn(txId, txData),
+      tx: txData,
+    }
+  } catch(err) {
+    return {
+      success: false,
+      error: err,
+    }
+  }
+}
+
+
+/**
+ * 
+ * @param {String} txId transaction id from the Smart BCH chain
+ * @param {function} callback callback function when bridge transfer for the source tx is made
+ * @returns {function} function to call to stop the listener
+ */
+export function s2cOutgoingListener(txId='', callback=() => {}) {
+  const txHashRegex = /[0-9a-f]{64}/
+  const websocket = new WebSocket(`wss://watchtower.cash/ws/watch/bch/${bchSender}/`)
+  websocket.onmessage = async (message) => {
+    const data = JSON.parse(message)
+    const txid = data?.txid
+    if (!txHashRegex.test(txid)) return
+
+    const response = await matchOpReturnFromHash(txId, txid)
+    if (!response.success) return
+    callback(response.tx)
+  }
+  
+  return () => {
+    websocket.close()
+  }
+}
+
+
+export function waitC2SOutgoing(txId) {
+  const promise = new Promise(resolve => {
+    const stopListener = c2sOutgoingListener(txId, (tx) => {
+      stopListener()
+      resolve(tx)
+    })
+    setTimeout(() => {
+      promise.cancelWatch = stopListener
+    }, 100)
+  })
+
+  return promise
+}
+
+
+export function waitS2COutgoing(txId) {
+  const promise = new Promise(resolve => {
+    const stopListener = s2cOutgoingListener(txId, (tx) => {
+      stopListener()
+      resolve(tx)
+    })
+
+    setTimeout(() => {
+      promise.cancelWatch = stopListener
+    }, 100)
+  })
+
+  return promise
 }
