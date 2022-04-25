4e8d572b2e82345910285090542bcc3eaa6b8d84
khirvy019 <khirvy_19@yahoo.com>
2022-04-25 21:33:00 +0800

    split functions for fetching market prices between smartchain & mainchain assets

--- src/store/market/actions.js
+++ src/store/market/actions.js
@@ -64,7 +64,7 @@ export function getAllAssetList(context) {
 }
 
 
-export async function updateAssetPrices(context, { clearExisting = false }) {
+export async function updateAssetPrices2(context, { clearExisting = false }) {
   const assetList = await context.dispatch('getAllAssetList')
   console.log(assetList)
   const coinIds = [] 
@@ -97,6 +97,95 @@ export async function updateAssetPrices(context, { clearExisting = false }) {
     }
   })
 
+  if (clearExisting) context.commit('clearAssetPrices')
+  context.commit('updateAssetPrices', newAssetPrices)
+}
+
+
+export async function updateMainchainAssetPrices(context, { commit=true }) {
+  const mainchainAssets = context.rootGetters['assets/getAssets'].map(asset => {
+    const filteredSymbol = context.state.coinsList
+      .filter(coin => String(coin.symbol).toLowerCase() === String(asset.symbol).toLowerCase())
+    const filteredPlatform = filteredSymbol
+      .filter(coin => !coin.platforms || Object.getOwnPropertyNames(coin.platforms).length <= 1)
+    const coin = filteredPlatform.length ? filteredPlatform[0] : filteredSymbol[0]
+    // console.log(asset.name, filteredSymbol, filteredPlatform)
+    return { asset, coin }
+  })
+
+  const coinIds = mainchainAssets.map(({ coin }) => coin && coin.id).filter(Boolean)
+  const { data: prices } = await axios.get(
+    'https://api.coingecko.com/api/v3/simple/price',
+    {
+      params: {
+        ids: coinIds.join(','),
+        vs_currencies: context.state.selectedCurrency,
+      }
+    }
+  )
+
+  const marketPrices = mainchainAssets.map(({ asset, coin }) => {
+    return {
+      assetId: asset.id,
+      coinId: coin.id,
+      prices: prices[coin.id],
+    }
+  })
+
+  if (commit) context.commit('updateAssetPrices', marketPrices)
+  return marketPrices
+}
+
+export async function updateSmartchainPrices(context, { commit=true }) {
+  const smartchainAssets = context.rootGetters['sep20/getAssets']
+    .map(asset => {
+      const match = String(asset.id).match(/^sep20\/(0x[0-9a-fA-F]*)$/)
+      console.log(match)
+      if (!match) return
+      return { asset, address: match[1] }
+    })
+    .filter(Boolean)
+  const addresses = smartchainAssets.map(asset => asset.address).filter(Boolean)
+    
+
+  const { data: prices } = await axios.get(
+    'https://api.coingecko.com/api/v3/simple/token_price/smartbch',
+    {
+      params: {
+        contract_addresses: addresses.join(','),
+        vs_currencies: context.state.selectedCurrency,
+      }
+    }
+  )
+
+  const marketPrices = smartchainAssets
+    .map(({ asset, address }) => {
+      if (!address) return
+      if (!prices[address.toLowerCase()]) return
+
+      return {
+        assetId: asset.id,
+        contractAddress: address,
+        prices: prices[address.toLowerCase()],
+      }
+    })
+    .filter(Boolean)
+
+  if (commit) context.commit('updateAssetPrices', marketPrices)
+  console.log(marketPrices)
+  return marketPrices
+}
+
+export async function updateAssetPrices(context, { clearExisting = false }) {
+  const mainchainMarketPrices = await context.dispatch('updateMainchainAssetPrices', { commit: false })
+  const smartchainMarketPrices = await context.dispatch('updateSmartchainPrices', { commit: false })
+  console.log(smartchainMarketPrices)
+
+  const newAssetPrices = [
+    ...mainchainMarketPrices,
+    ...smartchainMarketPrices,
+  ]
+  
   if (clearExisting) context.commit('clearAssetPrices')
   context.commit('updateAssetPrices', newAssetPrices)
 }
