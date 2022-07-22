7c994d4d8b70ee430e1dd97bed5b6c3ac024be2b
joemarct <joemar.ct@gmail.com>
2022-07-22 18:41:18 +0800

    Refactored sweeping functions into a utility class

--- src/wallet/sweep.js
+++ src/wallet/sweep.js
@@ -3,98 +3,83 @@ const bchjs = new BCHJS()
 const axios = require('axios')
 const Watchtower = require('watchtower-cash-js')
 
-function sweepToken (slpAddress, slpWif, tokenId, balance, bchFundingAddress, bchFundingWif, recipient) {
-  const watchtower = new Watchtower()
-  const data = {
-    sender: {
-      address: slpAddress,
-      wif: slpWif
-    },
-    tokenId: tokenId,
-    feeFunder: {
-      address: bchFundingAddress,
-      wif: bchFundingWif
-    },
-    recipients: [
-      {
-        address: recipient,
-        amount: balance
-      }
-    ],
-    broadcast: false
+export class SweepPrivateKey {
+  constructor (wif) {
+    this.wif = wif
   }
 
-  watchtower.SLP.Type1.send(data).then(function (result) {
-    console.log(`\n\n####### Token: ${tokenId}`)
-    console.log(result)
-  })
-}
+  async getTokensList () {
+    const ecpair = bchjs.ECPair.fromWIF(this.wif)
+    const bchAddress = bchjs.ECPair.toCashAddress(ecpair)
+    const slpAddress = bchjs.SLP.Address.toSLPAddress(bchAddress)
 
-function sweepBch (bchAddress, bchWif, spendableBalance, recipient) {
-  const watchtower = new Watchtower()
-  const data = {
-    sender: {
-      address: bchAddress,
-      wif: bchWif
-    },
-    recipients: [
-      {
-        address: recipient,
-        amount: spendableBalance - (10 / (10 ** 8))
+    const respSlp = await axios.post('https://watchtower.cash/api/subscription/', {
+      address: slpAddress
+    })
+
+    if (respSlp.data.success) {
+      const url = `https://watchtower.cash/api/tokens/?address=${slpAddress}&has_balance=true&token_type=1&limit=20`
+      const resp = await axios.get(url)
+      const _tokens = resp.data.results
+      const tokens = []
+      for (let i = 0; i < _tokens.length; i++) {
+        const item = _tokens[i]
+        const tokenId = item.id.split('/')[1]
+        const resp = await axios.get(`https://watchtower.cash/api/balance/slp/${slpAddress}/${tokenId}/`)
+        const data = resp.data
+        resp.data.token_id = tokenId
+        resp.data.symbol = item.symbol
+        resp.data.image_url = item.image_url
+        tokens.push(data)
       }
-    ],
-    broadcast: false
+      return tokens
+    }
   }
 
-  watchtower.BCH.send(data).then(function (result) {
-    console.log('\n\n####### BCH:')
-    console.log(result)
-  })
-}
-
-async function sweepPrivateKey (wif, bchRecipient, slpRecipient) {
-  const ecpair = bchjs.ECPair.fromWIF(wif)
-  const bchAddress = bchjs.ECPair.toCashAddress(ecpair)
-  console.log(bchAddress)
-  const slpAddress = bchjs.SLP.Address.toSLPAddress(bchAddress)
-  console.log(slpAddress)
-
-  const respBch = await axios.post('https://watchtower.cash/api/subscription/', {
-    address: bchAddress
-  })
-
-  const respSlp = await axios.post('https://watchtower.cash/api/subscription/', {
-    address: slpAddress
-  })
-
-  if (respBch.data.success && respSlp.data.success) {
-    // Get tokens
-    const url = `https://watchtower.cash/api/tokens/?address=${slpAddress}&has_balance=true&token_type=1&limit=20`
-    const resp = await axios.get(url)
-    const promises = resp.data.results.map(function (item) {
-      const tokenId = item.id.split('/')[1]
-      axios.get(`https://watchtower.cash/api/balance/slp/${slpAddress}/${tokenId}/`).then(function (resp) {
-        // const data = resp.data
-        // resp.data.token_id = tokenId
-        // resp.data.symbol = item.symbol
-        // resp.data.image_url = item.image_url
-        // console.log(data)
-        if (resp.data.balance > 0) {
-          sweepToken(slpAddress, wif, tokenId, resp.data.spendable, bchAddress, wif, slpRecipient)
+  sweepToken (slpAddress, slpWif, tokenId, balance, bchFundingAddress, bchFundingWif, recipient) {
+    const watchtower = new Watchtower()
+    const data = {
+      sender: {
+        address: slpAddress,
+        wif: slpWif
+      },
+      tokenId: tokenId,
+      feeFunder: {
+        address: bchFundingAddress,
+        wif: bchFundingWif
+      },
+      recipients: [
+        {
+          address: recipient,
+          amount: balance
         }
-      })
-    })
+      ],
+      broadcast: false
+    }
 
-    Promise.all(promises).then(function () {
-      axios.get(`https://watchtower.cash/api/balance/bch/${bchAddress}`).then(function (resp) {
-        sweepBch(bchAddress, wif, resp.data.spendable, bchRecipient)
-      })
-      // console.log(bchBalance.data)
+    watchtower.SLP.Type1.send(data).then(function (result) {
+      console.log(result)
+    })
+  }
+
+  sweepBch (bchAddress, bchWif, spendableBalance, recipient) {
+    const watchtower = new Watchtower()
+    const data = {
+      sender: {
+        address: bchAddress,
+        wif: bchWif
+      },
+      recipients: [
+        {
+          address: recipient,
+          amount: spendableBalance - (10 / (10 ** 8))
+        }
+      ],
+      broadcast: false
+    }
+
+    watchtower.BCH.send(data).then(function (result) {
+      console.log(result)
     })
   }
 }
-
-const wif = ''
-const bchRecipient = 'bitcoincash:qz0hlxg5shuzp9mmr2dypa7afp96s4pzeyaqvrqw5a'
-const slpRecipient = 'simpleledger:qz7nux3ngafxtcwfy24ua30g8vq8hz6rucr0sqx8rw'
-sweepPrivateKey(wif, bchRecipient, slpRecipient)
