7f595949966a4aa8caf45ef249becb9e1e134b15
joemarct <joemar.ct@gmail.com>
2022-09-04 08:53:43 +0800

    Adjusted header padding for Android

--- src/components/header-nav.vue
+++ src/components/header-nav.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="row">
       <div class="col 12">
-        <div class="pt-header" :style="{ 'padding-top': this.$q.platform.is.ios ? '45px' : '0px'}" :class="{'pt-dark': $store.getters['darkmode/getStatus']}">
+        <div class="pt-header" :style="{ 'padding-top': this.$q.platform.is.ios ? '45px' : '18px'}" :class="{'pt-dark': $store.getters['darkmode/getStatus']}">
           <router-link :to="{ path: backnavpath }" class="pt-arrow-left-link" :class="{'text-grad': $store.getters['darkmode/getStatus']}" :style="{width: $q.platform.is.bex ? '375px' : '100%'}">
             <span class="material-icons" @click="!backnavpath ? $router.go(-1) : null">
                 arrow_back
