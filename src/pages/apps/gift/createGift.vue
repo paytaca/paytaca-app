1a7b76cac52b416edbb2b33c9e958e5702223667
jed <adijed3@gmail.com>
2022-09-19 14:46:38 +0800

    restructured code to integrate VUEX

--- src/pages/apps/gift/createGift.vue
+++ src/pages/apps/gift/createGift.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="static-container">
-    <div
-      dense
-      id="app-container"
-      style="background-color: #ECF3F3; min-height: 100vh;"
-      class="flex flex-center"
-      :class="{ 'pt-dark': darkMode }"
+<div class="static-container">
+  <div
+    dense
+    id="app-container"
+    style="background-color: #ECF3F3; min-height: 100vh;"
+    class="flex flex-center"
     >
-      <HeaderNav
-        title="Gifts"
-        backnavpath="/apps/chooseGift"
-        style="position: fixed; top: 0; background: #ECF3F3; width: 100%; z-index: 100 !important;"
-        class="q-px-sm"
-      />
-    <div class="q-pa-lg" style="width: 20%">
+    <HeaderNav
+      title="Gifts"
+      backnavpath="/apps/chooseGift"
+      style="position: fixed; top: 0; background: #ECF3F3; width: 100%; z-index: 100 !important;"
+      class="q-px-sm"
+      @click="this.$store.dispatch('gift/retAddress')"
+    />
+<!--       <div>
+        {{ $store.getters['gift/getAmount'] }}
+        {{ $store.getters['gift/getTitle'] }}s
+      </div> -->
+      <div class="q-pa-lg" style="width: 32%">
       <div class="q-pt-xl">
           <label>
             Enter Amount:
@@ -22,13 +26,11 @@
             required
             placeholder="Amount"
             filled
-            :dark="darkMode"
             clearable
-            v-model="input.amount"
-            :disable="handshakeOnProgress"
             :rules="[val => !!val || 'Field is required']"
-            type="text"
-            id="amount"
+            type="number"
+            @input="this.amount"
+            v-model="amount"
           >
           </q-input>
           <div class="q-pa-sm">
@@ -42,12 +44,8 @@
             placeholder="Instances"
             filled
             type="number"
-            v-model="input.instance"
-            :dark="darkMode"
-            :max="input.quantity"
             oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
             maxlength="3"
-            :disable="handshakeOnProgress"
             clearable
             :rules="[val => !!val || 'Field is required']"
           >
@@ -62,8 +60,6 @@
             placeholder="Campaign ID"
             filled
             type="string"
-            v-model="input.campID"
-            :dark="darkMode"
             clearable
           >
           </q-input>
@@ -77,8 +73,6 @@
             placeholder="Amount"
             filled
             type="number"
-            v-model="input.maxPerAddress"
-            :dark="darkMode"
             clearable
           >
           </q-input>
@@ -89,52 +83,46 @@
                 color="blue-9"
                 type="submit"
                 label="Generate"
-                @click="splitSecret(), card = true"
                 class="flex flex-center"
+                @click="this.pkToCashAdd()"
               >
               </q-btn>
               <div class="q-pa-sm q-pb-lg"></div>
-<!--               <q-btn
+<!--             <div class="q-pa-sm q-pt-lg flex flex-center" >
+              <q-btn
                 no-caps
                 rounded
                 color="blue-9"
                 type="submit"
-                label="recover"
+                label="cash address"
+                class="flex flex-center"
                 @click="recoverSecret()"
               >
-              </q-btn> -->
+              </q-btn>
+              </div> -->
         </div>
-        <q-dialog v-model="card" no-backdrop-dismiss full-height>
-          <q-card class="my-card" >
-            <q-card-section>
-              <div class="flex flex-center">
+      <div>
+          <div class="flex flex-center" >
+            <div class="flex flex-center col-qr-code">
                 <canvas id="canvas" class="flex flex-center"></canvas>
-                </div>
-              </q-card-section>
-              <q-card-section>
-                <div>
-                  <div @click="copyToClipboard(this.cashAdd)"> {{ this.cashAdd }}</div>
-                </div>
-              <!-- </div> -->
-            </q-card-section>
-          </q-card>
-        </q-dialog>
-    </div>
+            </div>
+            <div class="flex flex-center myStyle">
+              <!-- <h5>{{ $store.state.gift.cashAddress }}</h5> -->
+            </div>
+          </div>
+          <div class="flex flex-center" @click="copyToClipboard(this.$store.state.gift.cashAddress)">
+          <p class="fontStyle">{{ $store.state.gift.cashAddress }}</p>
+          </div>
+      </div>
+        </div>
+      </div>
   </div>
-  </div>
-  </div>
+</div>
 </template>
 <script>
-
-// import HeaderNav from '../../components/header-nav'
 import HeaderNav from '../../../components/header-nav'
-// import { Notify } from 'quasar'
-import { ECPair } from '@psf/bitcoincashjs-lib'
-import { toHex } from 'hex-my-bytes'
-// import { formatsByName, formatsByCoinType } from '@ensdomains/address-encoder'
-// import { QRGenerator } from 'dynamic-qr-code-generator'
+// import { mapState } from 'vuex'
 import { getMnemonic, Wallet } from '../../../wallet'
-import { ref } from 'vue'
 
 export default {
   name: 'Gifts',
@@ -147,69 +135,46 @@ export default {
   },
   data () {
     return {
-      input: {
-        amount: '',
-        instance: '',
-        campID: '',
-        quantity: '100',
-        maxPerAddress: '',
-        shares: [],
-        pk: '',
-        wallet: '',
-        txid: ''
-      },
-      generatingAddress: false,
-      walletType: '',
-      card: ref(false),
-      cashAdd: null
+      wallet: '',
+      txid: '',
+      amount: ''
     }
   },
+
   methods: {
-    // method for splitting private key to 3 shares
-    async splitSecret () {
-      this.pk = this.generatePrivateKey()
-      this.cashAdd = this.convertToCashAddress(this.pk)
-      console.log(this.pk)
-      console.log(this.cashAdd)
-      const sss = require('shamirs-secret-sharing')
-      const secret = Buffer.from(this.pk)
-      const shares = sss.split(secret, { shares: 3, threshold: 2 })
-      this.shares = shares.map((share) => { return toHex(share) })
-      this.handleSubmit(this.cashAdd)
-      this.qrCode()
-      console.log(this.shares)
-    },
-    // method for recovering private key from 2 of 3 shares(splitsecret)
     recoverSecret () {
-      const sss = require('shamirs-secret-sharing')
-      const recovery = sss.combine([this.shares[0], this.shares[1]])
-      console.log(recovery.toString())
+      this.$store.dispatch('gift/recoverSecret')
     },
-
-    // method for generating private key
-    generatePrivateKey () {
-      const keyPair = ECPair.makeRandom()
-      return keyPair.toWIF()
+    pkToCashAdd () {
+      this.$store.dispatch('gift/changeAmount')
+      this.$store.dispatch('gift/generatePrivateKey')
+      this.$store.dispatch('gift/convertToCashAddress')
+      this.$store.dispatch('gift/splitSecret')
+      this.$store.dispatch('gift/storeShare')
+      // this.$store.dispatch('gift/handleSubmit')
+      const cAdd = this.$store.state.gift.cashAddress
+      this.handleSubmit(cAdd)
+      this.qrCode()
+      // console.log(this.updateAmount())
     },
-
-    // method for converting private key to cash address
-    convertToCashAddress (wif) {
-      const BCHJS = require('@psf/bch-js')
-      const bchjs = new BCHJS()
-      const pair = bchjs.ECPair.fromWIF(wif)
-      const cAddress = bchjs.ECPair.toCashAddress(pair)
-      // console.log(cAddress)
-      return cAddress
-    },
-
-    // generate qr code
     qrCode () {
-      const key = this.pk
+      const key = this.$store.state.gift.share[0]
       const QRCode = require('qrcode')
       const canvas = document.getElementById('canvas')
       QRCode.toCanvas(canvas, key, function (error) {
         if (error) console.error(error)
         console.log('success!')
+      })
+    },
+    async handleSubmit (cAdd) {
+      const vm = this
+      const address = cAdd
+      vm.wallet.BCH.sendBch(this.amount, address).then(function (result, err) {
+        if (result.success) {
+          vm.txid = result.txid
+        } else {
+          console.error(err)
+        }
       })
     },
     copyToClipboard (value) {
@@ -220,27 +185,9 @@ export default {
         color: 'blue-9',
         icon: 'mdi-clipboard-check'
       })
-    },
-    // send bch to generated wallet
-    async handleSubmit (cAdd) {
-      const vm = this
-      const address = cAdd
-      vm.wallet.BCH.sendBch(vm.input.amount, address).then(function (result, err) {
-        if (result.success) {
-          vm.input.txid = result.txid
-        } else {
-          console.error(err)
-        }
-      })
     }
-  },
-  computed: {
-    getAddress () {
-      const address = this.cashAdd
-      return address
-    }
-  },
 
+  },
   // wallet call function when mounted
   mounted () {
     const vm = this
@@ -254,3 +201,20 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+    .col-qr-code {
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 10px;
+    text-align: center;
+    width: 500px;
+    height: 300px;
+    border-radius: 16px;
+    border: 4px solid #ed5f59;
+    padding: 10px 10px 32px 10px;
+    background: white;
+  }
+  .fontStyle {
+    font-size:medium;
+  }
+</style>
