7ef7be1be362dceb0a15dd92d045770488b1e6b0
jed <adijed3@gmail.com>
2022-09-29 14:08:20 +0800

    refactored codes to fix UI

--- src/pages/apps/gifts/createGift.vue
+++ src/pages/apps/gifts/createGift.vue
@@ -8,7 +8,7 @@
       >
       <HeaderNav
         title="Gifts"
-        backnavpath="/apps/chooseGift"
+        backnavpath="/apps/gifts"
         style="position: fixed; top: 0; background: #ECF3F3; width: 100%; z-index: 100 !important;"
         class="q-px-sm"
       />
@@ -76,7 +76,7 @@
             v-model="text"
           >
           </q-input>
-            <div class="q-pa-sm q-pt-lg flex flex-center">
+            <div class="q-pa-sm q-pt-lg flex flex-center" style="width:100%">
               <q-btn
                 no-caps
                 rounded
@@ -84,58 +84,42 @@
                 type="submit"
                 label="Generate"
                 class="flex flex-center"
+                id="canvasButton"
                 @click="clickMe()"
               >
               </q-btn>
               <div class="q-pa-sm q-pb-lg"></div>
-              <div class="q-pa-sm q-pt-lg flex flex-center" >
-              <q-btn
-                no-caps
-                rounded
-                color="blue-9"
-                type="submit"
-                label="cash address"
-                class="flex flex-center"
-                @click="spliceAll()"
-              >
-              </q-btn>
-              </div>
         </div>
-
-        <div>
+        <!-- <div>
         {{ this.$store.state.gifts.privKey }}
         {{this.$store.state.gifts.cashAdd}}
         {{ this.$store.state.gifts.storeShare }}
-        <!-- {{ this.$store.state.gift.recShare }} -->
+        {{ this.$store.state.gift.recShare }}
         {{ this.$store.state.gifts.genGifts }}
-        <!-- {{ this.shares }} -->
-
-        <!-- {{ this.$store.state.gift.apKeyStore }} -->
-        </div>
-      <div>
-          <div class="flex flex-center" >
+        </div> -->
+        <div>
+          <div class="flex flex-center q-pb-none">
+            <div class="flex flex-center">
             <div class="flex flex-center col-qr-code">
                 <canvas id="canvas" class="flex flex-center"></canvas>
-                <!-- {{ this.$store.state.gift.gAmount }} -->
             </div>
+              </div>
             <div class="flex flex-center myStyle">
-              <!-- <h5>{{ $store.state.gift.cashAddress }}</h5> -->
             </div>
           </div>
-          <!-- <div class="flex flex-center" @click="copyToClipboard(this.$store.state.gift.cashAddress)">
-            <div class="flex flex-center" v-if="$store.state.gift.cashAddress !== 0">
-          <p class="fontStyle">{{ $store.state.gift.cashAddress }}</p>
-          <p class="fontStyle">Click to Copy Address</p>
+          <div class="flex flex-center q-pt-sm" @click="copyToClipboard(this.cAdd)"  v-if="$store.cAdd !== 0">
+              <p class="fontStyle flex flex-center">{{ this.cAdd }}</p>
             </div>
-            <div class="flex flex-center" v-else>
-              <p class="fontStyle">{{ $store.state.gift.cashAddress }}</p>
-            </div> -->
-        <!-- </div> -->
+              <div class="flex flex-center q-pt-none">
+              <p class="fontStyle">Click to Copy Address</p>
+            </div>
+        </div>
+       </div>
       </div>
         </div>
       </div>
-    </div>
-  </div>
+   <!--  </div>
+  </div> -->
   </template>
 <script>
 import HeaderNav from '../../../components/header-nav'
@@ -158,57 +142,59 @@ export default {
       wallet: '',
       txid: '',
       amountBCH: '',
-      instances: 0,
-      text: ['hot', 'dog', 'chili'],
-      id: 1,
+      instances: null,
       gift: {},
+      text: null,
       // test
       vWif: '',
-      str: '',
+      cAdd: '',
       localShare: [],
       uid: '',
-      gifts: {}
+      gifts: {},
+      hotdog: {},
+      url: ''
     }
   },
 
   methods: {
-    // test 9 22 2022
-
     // test 9 23 2022
     clickMe () {
-      this.genPrivKey()
-      this.genCashAddress()
-      this.genShares()
-      // this.recoverSec()
+      for (let i = 0; i < this.instances; i++) {
+        this.genPrivKey()
+        // this.genCashAddress()
+        this.genShares()
+        this.qrCode()
+        this.handleSubmit(this.cAdd)
+        // this.recoverSec()
+      }
     },
     genPrivKey () {
       const privateKey = ECPair.makeRandom()
       this.vWif = privateKey.toWIF()
       console.log('Private key:', this.vWif)
       this.$store.dispatch('gifts/genPrivKey', this.vWif)
-    },
-    genCashAddress () {
+      // generate cash address
       const BCHJS = require('@psf/bch-js')
       const bchjs = new BCHJS()
-      // const pk = this.vWif
       const pair = bchjs.ECPair.fromWIF(this.vWif)
-      this.str = bchjs.ECPair.toCashAddress(pair)
-      console.log('Address:', this.str)
-      this.$store.dispatch('gifts/genCashAdd', this.str)
+      this.cAdd = bchjs.ECPair.toCashAddress(pair)
+      console.log('Address:', this.cAdd)
+      this.$store.dispatch('gifts/genCashAdd', this.cAdd)
     },
     genShares () {
+      // create shares and send to store
       const sss = require('shamirs-secret-sharing')
       const secret = Buffer.from(this.vWif)
       const stateShare = sss.split(secret, { shares: 3, threshold: 2 })
       this.shares = stateShare.map((share) => { return toHex(share) })
-      // commit('aSplitSecret', this.shares)
       this.$store.dispatch('gifts/genShares', this.shares)
+      this.localShare = this.shares
       console.log('Shares:', this.shares)
-      console.log('xxx:', this.shares)
-      // this.gifts[uuidv4()] = this.text
+
+      // create uid and put in dict with shares
       this.uid = uuidv4()
       console.log('Gift ID:', this.uid)
-      this.gifts[this.uid] = this.localShares
+      this.gifts[this.uid] = this.shares
       this.$store.dispatch('gifts/storeGift', { uid: this.uid, shares: this.shares })
     },
     recoverSec () {
@@ -218,16 +204,14 @@ export default {
       this.$store.dispatch('gifts/recoverSec', recovery.toString())
     },
     // /end test
-    recoverSecret () {
-      this.$store.dispatch('gifts/recoverSecret')
-    },
     qrCode () {
-      const key = this.$store.state.gifts.share[0]
+      const key = this.localShare[0]
+      // const key = this.vWif
       const QRCode = require('qrcode')
       const canvas = document.getElementById('canvas')
       QRCode.toCanvas(canvas, key, function (error) {
         if (error) console.error(error)
-        console.log('success!')
+        console.log('success')
       })
     },
     async handleSubmit (cAdd) {
@@ -280,5 +264,8 @@ export default {
     }
     .fontStyle {
       font-size:medium;
+    }
+    display {
+      display: none;
     }
   </style>
