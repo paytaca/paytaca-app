5dcf6986bea173a4d3f56c3294525f7bb7905c72
joemarct <joemar.ct@gmail.com>
2022-10-11 21:24:51 +0800

    Recovery of private key from combined shares, funding of gift

--- src/pages/apps/gifts/claim-gift.vue
+++ src/pages/apps/gifts/claim-gift.vue
@@ -9,93 +9,31 @@
         <header-nav title="Claim Gift" backnavpath="/apps/gifts" style="position: fixed; top: 0; background: #ECF3F3; width: 100%; z-index: 100 !important;"></header-nav>
         <div style="margin-top: 60px;">
           <div id="app" ref="app" :class="{'text-black': !darkMode}">
-            <div v-if="fetching && tokens.length === 0" style="text-align: center; margin-top: 25px;">
-              <p>Scanning...</p>
+            <div v-if="processing" style="text-align: center; margin-top: 25px;">
+              <p>Claiming gift...</p>
               <progress-loader />
             </div>
-            <q-form v-if="!submitted" class="text-center" style="margin-top: 25px;">
+            <q-form v-if="!processing" class="text-center" style="margin-top: 25px;">
               <textarea
-                v-if="tokens.length === 0" v-model="wif"
+                v-model="scannedShare"
                 style="width: 100%; font-size: 18px; color: black; background: white;" rows="2"
                 placeholder="Paste your share here"
               >
               </textarea>
               <br>
-              <template v-if="!wif">
+              <template v-if="!scannedShare">
                 <div style="margin-top: 20px; margin-bottom: 20px; font-size: 15px; color: grey;">
                   OR
                 </div>
                 <q-btn round size="lg" class="btn-scan text-white" icon="mdi-qrcode" @click="showQrScanner = true" />
               </template>
               <div style="margin-top: 20px; ">
-                <q-btn color="primary" v-if="tokens.length === 0 && wif" @click.prevent="getTokens">Scan</q-btn>
-                <p v-if="wif && error" style="color: red;">
+                <q-btn color="primary" v-if="scannedShare.length > 0 && !error" @click.prevent="claimGift">Claim</q-btn>
+                <p v-if="error" style="color: red;">
                   {{ error }}
                 </p>
               </div>
             </q-form>
-            <div>
-              <div v-if="sweeper && bchBalance">
-                <p><strong>BCH</strong></p>
-                <p>
-                  BCH Address: {{ ellipsisText(sweeper.bchAddress) }}
-                  <q-icon name="mdi-content-copy" @click="copyToClipboard(sweeper.bchAddress)" />
-                </p>
-                <div style="border: 1px solid black; padding: 10px;">
-                  <p>BCH Balance: {{ bchBalance }}</p>
-                  <q-btn color="primary" v-if="selectedToken !== 'bch'" @click.prevent="sweepBch" :disabled="(tokens.length - skippedTokens.length) > 0">Sweep</q-btn>
-                  <span v-if="(tokens.length - skippedTokens.length) > 0" style="color: red;"><i> Sweep the tokens first</i></span>
-                  <div v-if="sweeping && selectedToken === 'bch'">
-                    <progress-loader />
-                  </div>
-                </div>
-              </div>
-              <div v-if="sweeper && (bchBalance === 0 || bchBalance < 0)">
-                <p><strong>BCH</strong></p>
-                <p>
-                  BCH Address: {{ ellipsisText(sweeper.bchAddress) }}
-                  <q-icon name="mdi-content-copy" @click="copyToClipboard(sweeper.bchAddress)" />
-                </p>
-                <div style="border: 1px solid black; padding: 10px;">
-                  <p>BCH Balance: {{ bchBalance }}</p>
-                  <template v-if="tokens.length === 0">
-                    <span style="color: red;">This address is empty</span>
-                  </template>
-                  <template v-else>
-                    <span style="color: red;"><i>You will need sufficient BCH balance to be able to sweep the token(s) below</i></span>
-                  </template>
-                </div>
-              </div>
-              <div v-if="tokens.length > 0" style="margin-top: 15px">
-                <p><strong>Tokens ({{ tokens.length }})</strong></p>
-                <p>
-                  SLP Address: {{ ellipsisText(sweeper.slpAddress) }}
-                  <q-icon name="mdi-content-copy" @click="copyToClipboard(sweeper.slpAddress)" />
-                </p>
-                <div v-if="tokens.length > 0" class="p-lg" style="margin-bottom: 20px;">
-                  <q-select filled v-model="payFeeFrom" :options="feeOptions" label="Pay for transaction fee from" :dark="darkMode" />
-                </div>
-                <div v-for="(token, index) in tokens" :key="index">
-                  <div style="border: 1px solid black; padding: 10px; margin-top: 10px;">
-                    <p>
-                      Token ID: {{ ellipsisText(token.token_id) }}
-                      <q-icon name="mdi-content-copy" @click="copyToClipboard(token.token_id)" />
-                    </p>
-                    <p>Symbol: {{ token.symbol }}</p>
-                    <img v-if="token.image_url.length > 0" :src="token.image_url" height="50" />
-                    <p>Amount: {{ token.spendable }}</p>
-                    <template v-if="selectedToken !== token.token_id">
-                      <q-btn color="primary" @click.prevent="sweepToken(token)" :disabled="sweeping || skippedTokens.includes(token.token_id)">
-                        Sweep
-                      </q-btn>&nbsp;&nbsp;&nbsp; OR <q-checkbox v-model="skippedTokens" v-bind:val="token.token_id" label="Skip" />
-                    </template>
-                    <div v-if="sweeping && selectedToken === token.token_id">
-                      <progress-loader />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
@@ -105,9 +43,11 @@
 
 <script>
 import { markRaw } from '@vue/reactivity'
+import axios from 'axios'
+import sha256 from 'js-sha256'
 import HeaderNav from '../../../components/header-nav'
 import ProgressLoader from '../../../components/ProgressLoader'
-import SweepPrivateKey from '../../../wallet/sweep'
+// import SweepPrivateKey from '../../../wallet/sweep'
 import QrScanner from '../../../components/qr-scanner.vue'
 import { getMnemonic, Wallet } from '../../../wallet'
 
@@ -121,132 +61,75 @@ export default {
   data () {
     return {
       wallet: null,
-      wif: '',
-      tokens: [],
-      skippedTokens: [],
-      bchBalance: null,
-      sweeper: null,
-      submitted: false,
-      fetching: false,
-      sweeping: false,
-      selectedToken: null,
-      showQrScanner: false,
+      scannedShare: '',
+      processing: false,
       error: null,
-      feeOptions: [
-        { label: 'Wallet', value: 'wallet' },
-        { label: 'Address', value: 'address' }
-      ],
-      payFeeFrom: { label: 'Wallet', value: 'wallet' },
+      showQrScanner: false,
       darkMode: this.$store.getters['darkmode/getStatus']
     }
   },
-  watch: {
-    wif (value) {
-      if (value.length === 0) {
-        this.error = null
-      }
-    }
-  },
-  computed: {
-    feeFunder () {
-      let funder
-      if (this.payFeeFrom.value === 'address') {
-        funder = {
-          address: this.sweeper.bchAddress,
-          wif: this.wif
-        }
-      } else if (this.payFeeFrom.value === 'wallet') {
-        funder = {
-          walletHash: this.wallet.BCH.walletHash,
-          mnemonic: this.wallet.mnemonic,
-          derivationPath: this.wallet.BCH.derivationPath
-        }
-      }
-      return funder
-    }
-  },
   methods: {
-    ellipsisText (value) {
-      if (typeof value !== 'string') return ''
-      if (value.length <= 20) return value
-      return value.substr(0, 18) + '...' + value.substr(value.length - 10, value.length)
-    },
-    copyToClipboard (value) {
-      this.$copyText(value)
-      this.$q.notify({
-        message: 'Copied to clipboard',
-        timeout: 200,
-        icon: 'mdi-clipboard-check',
-        color: 'blue-9'
-      })
-    },
-    validatePrivateKey (value) {
-      return /^[5KL][1-9A-HJ-NP-Za-km-z]{50,51}$/.test(String(value))
-    },
-    getTokens (signalFetch) {
+    recoverPrivateKey () {
       const vm = this
-      if (vm.validatePrivateKey(vm.wif)) {
-        vm.submitted = true
-        if (signalFetch) {
-          vm.fetching = true
+      const sss = require('shamirs-secret-sharing')
+      const giftId = sha256(this.scannedShare)
+      const url = `https://gifts.paytaca.com/api/gifts/${giftId}/claim`
+      const walletHash = this.wallet.BCH.getWalletHash()
+      axios.post(url, { wallet_hash: walletHash }).then((resp) => {
+        const privateKey = sss.combine([this.scannedShare, resp.data.share])
+        console.log('Recovered private key:', privateKey.toString())
+        vm.processing = false
+      }).catch((error) => {
+        if (error.response.status === 409) {
+          vm.error = 'You have exceeded the limit of gifts to claim for this campaign!'
+          vm.processing = false
         }
-        if (vm.wif.length > 0) {
-          vm.sweeper = new SweepPrivateKey(this.wif)
-          vm.sweeper.getTokensList().then(function (tokens) {
-            vm.tokens = tokens.filter(function (token) {
-              // if (token.spendable > 0) {
-              // vm.skippedTokens[token.token_id] = false
-              return token
-              // }
-            })
-            vm.sweeper.getBchBalance().then(function (data) {
-              vm.bchBalance = data.spendable || 0
-              vm.fetching = false
-              vm.sweeping = false
-            })
-          })
-        }
-      } else {
-        vm.error = 'Invalid private key!'
-      }
-    },
-    sweepToken (token) {
-      const vm = this
-      vm.sweeping = true
-      vm.selectedToken = token.token_id
-      vm.sweeper.sweepToken(
-        token.address,
-        vm.wif,
-        token.token_id,
-        token.spendable,
-        vm.feeFunder,
-        vm.$store.getters['global/getAddress']('slp')
-      ).then(function (result) {
-        if (!result.success) {
-          vm.$q.notify({
-            message: result.error,
-            icon: 'mdi-close-circle',
-            color: 'red-5'
-          })
-          vm.selectedToken = null
-        }
-        vm.getTokens()
       })
+      // this.$store.dispatch('gifts/recoverSec', recovery.toString())
     },
-    sweepBch () {
-      this.sweeping = true
-      this.selectedToken = 'bch'
-      this.sweeper.sweepBch(
-        this.sweeper.bchAddress,
-        this.wif,
-        this.bchBalance,
-        this.$store.getters['global/getAddress']('bch')
-      )
-      this.getTokens(false)
+    claimGift () {
+      this.processing = true
+      this.recoverPrivateKey()
+      // const vm = this
+      // if (vm.validatePrivateKey(vm.wif)) {
+      //   vm.submitted = true
+      //   if (signalFetch) {
+      //     vm.fetching = true
+      //   }
+      //   if (vm.wif.length > 0) {
+      //     vm.sweeper = new SweepPrivateKey(this.wif)
+      //     vm.sweeper.getTokensList().then(function (tokens) {
+      //       vm.tokens = tokens.filter(function (token) {
+      //         // if (token.spendable > 0) {
+      //         // vm.skippedTokens[token.token_id] = false
+      //         return token
+      //         // }
+      //       })
+      //       vm.sweeper.getBchBalance().then(function (data) {
+      //         vm.bchBalance = data.spendable || 0
+      //         vm.fetching = false
+      //         vm.sweeping = false
+      //       })
+      //     })
+      //   }
+      // } else {
+      //   vm.error = 'Invalid private key!'
+      // }
     },
+    // sweepBch () {
+    //   this.sweeping = true
+    //   this.selectedToken = 'bch'
+    //   this.sweeper.sweepBch(
+    //     this.sweeper.bchAddress,
+    //     this.wif,
+    //     this.bchBalance,
+    //     this.$store.getters['global/getAddress']('bch')
+    //   )
+    //   this.getTokens(false)
+    // },
     onScannerDecode (content) {
       this.showQrScanner = false
-      this.wif = content
+      this.scannedShare = content
     }
   },
   mounted () {
