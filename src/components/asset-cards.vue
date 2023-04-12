efee7381a67a4298c5f4057eb2973097301befe1
joemarct <joemar.ct@gmail.com>
2023-04-12 11:12:09 +0800

    Added token standard name in token asset card

--- src/components/asset-cards.vue
+++ src/components/asset-cards.vue
@@ -32,6 +32,7 @@
           {{ String(asset.balance).substring(0, 10) }}
         </p>
       </div>
+      <div style="position: absolute; bottom: 25px; background: gray; border-radius: 5px; font-size: 11px; padding-left: 3px; padding-right: 3px;">{{ asset.id.split('/')[0].toUpperCase() }}</div>
       <div v-if="getAssetMarketBalance(asset)" class="text-caption text-right" style="overflow: hidden; text-overflow: ellipsis; color: #EAEEFF; margin-top: -18px;">
         <template v-if="!(!balanceLoaded && asset.id === selectedAsset.id)">
           {{ getAssetMarketBalance(asset) }} {{ String(selectedMarketCurrency).toUpperCase() }}
