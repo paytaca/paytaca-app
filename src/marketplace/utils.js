cbaf1faa05652fe534a5042d7f0383d0011624df
khirvy019 <khirvy_19@yahoo.com>
2023-05-12 10:59:38 +0800

    handle form errors when updating delivery address

--- src/marketplace/utils.js
+++ src/marketplace/utils.js
@@ -5,3 +5,18 @@ export function formatTimestampToText(timestamp) {
   const dateObj = new Date(timestamp)
   return new Intl.DateTimeFormat('en-US', { dateStyle: 'medium', timeStyle: 'medium' }).format(dateObj)
 }
+
+
+export const errorParser = {
+  toArray(value) {
+    if (Array.isArray(value)) return value
+    if (value?.detail) return [value?.detail]
+    if (value) return [value]
+    return []
+  },
+  firstElementOrValue(array=[]){
+    let data = Array.isArray(array) ? array[0] : array
+    if (data?.detail) return data?.detail
+    return data
+  },
+}
