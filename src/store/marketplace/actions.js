7d002174db2d84ad1316581c9a59e11ddf48b6b4
khirvy019 <khirvy_19@yahoo.com>
2023-05-15 10:36:24 +0800

    update active cart after completing checkout
    filter out closed cart cached in vuex
    filter out closed cart when resolving active cart

--- src/store/marketplace/actions.js
+++ src/store/marketplace/actions.js
@@ -7,10 +7,18 @@ export function getCartRef(context) {
   return ''
 }
 
+export async function refreshActiveStorefrontCarts(context) {
+  const storefront = context.getters['activeStorefront']
+  if (!storefront?.id) return Promise.reject('No active storefront')
+  return context.dispatch('refreshStorefrontCarts', { storefrontId: storefront?.id })
+}
+
 export async function refreshStorefrontCarts(context, opts={ storefrontId: 0 }) {
   const params = {
     storefront_id: opts?.storefrontId || '',
+    is_open: true,
     ref: await context.dispatch('getCartRef'),
+    limit: 5,
   }
 
   return backend.get(`connecta/carts`, { params })
@@ -30,6 +38,34 @@ export function saveCart(context, cart) {
   return cart.save().then(() => {
     context.commit('cacheCart', cart.raw)
   })
+}
+
+/**
+ * 
+ * @param {Object} context 
+ * @param {Object} opts 
+ * @param {Number} opts.cartId id of the cart to fetch
+ * @param {Boolean} opts.existsInCache if true, the cart must be list of cached carts already
+ * 
+ */
+export function refreshCart(context, opts={ cartId: 0, existsInCache: false }) {
+  if (!opts?.cartId) return Promise.reject('No cart id')
+  const cart = context.getters.carts.find(cart => cart?.id == opts?.cartId)
+
+  if (cart) {
+    return cart.refetch().then(response => {
+      context.commit('cacheCart', cart.raw)
+      return response
+    })
+  } else if (!opts?.existsInCache) {
+    return backend.get(`connecta/carts/${cartId}/`).then(response => {
+      if (!response?.data?.id) return Promise.reject({ response })
+      context.commit('cacheCart', response?.data)
+      return response
+    })
+  }
+
+  return Promise.reject('Skipped')
 }
 
 export async function removeCart(context, opts={ cartId: 0, excludeApi: false }) {
