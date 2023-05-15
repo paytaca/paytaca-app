32d539c16b422f41990bc4018ee7efbd10369e32
khirvy019 <khirvy_19@yahoo.com>
2023-05-15 09:38:10 +0800

    add Order parser class & formatters

--- src/marketplace/utils.js
+++ src/marketplace/utils.js
@@ -1,3 +1,45 @@
+import { capitalize } from 'vue'
+
+/**
+ * @typedef {'pending' | 'confirmed' | 'preparing' | 'ready_for_pickup' | 'on_delivery' | 'delivered' | 'completed' | 'cancelled'} OrderStatus
+ */
+
+/**
+ * @param {OrderStatus} value
+ */
+export function formatOrderStatus(value) {
+  if (typeof value !== 'string') return ''
+
+  return capitalize(value.replaceAll('_', ' '))
+}
+
+/**
+ * 
+ * @param {OrderStatus} value 
+ */
+export function parseOrderStatusColor(value) {
+  switch (value) {
+    case 'pending':
+      return 'amber'
+    case 'confirmed':
+      return 'blue'
+    case 'preparing':
+      return 'amber-7'
+    case 'ready_for_pickup':
+      return 'amber-8'
+    case 'on_delivery':
+      return 'green'
+    case 'delivered':
+      return 'green-7'
+    case 'completed':
+      return 'green-8'
+    case 'cancelled':
+      return 'red'
+    default:
+      return undefined
+  }
+}
+
 /**
  * @param {Date | String | Number} timestamp 
  */
