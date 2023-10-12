281dca1bfd420e4bbfdadc66b80f9119662535c4
SeanNikki123 <sslino@up.edu.ph>
2023-10-12 14:43:14 +0800

    added log in dialog on fiat ramp

--- src/components/ramp/fiat/Index.vue
+++ src/components/ramp/fiat/Index.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-    <div v-if="isLoading" class="row justify-center q-ma-lg q-pa-lg">
-      <ProgressLoader/>
+    <div v-if="isLoading">
+      <!-- <ProgressLoader/> -->
+      <q-dialog maximized v-model="isLoading">
+        <q-card class="bg-blue-6 text-white text-center">
+          <div style="margin-top: 75%; font-weight: 500; font-size: 30px;">
+            Logging in...
+          </div>
+          <!-- <q-inner-loading :showing="visible"> -->
+          <q-spinner-hourglass class="q-pt-sm" size="80px" color="white" />
+          <!-- </q-inner-loading> -->
+        </q-card>
+      </q-dialog>
     </div>
     <div v-else>
       <div v-if="proceed">
@@ -46,11 +56,13 @@ import ProgressLoader from 'src/components/ProgressLoader.vue'
 import { loadWallet } from 'src/wallet'
 import { markRaw } from 'vue'
 import { loadP2PWalletInfo } from 'src/wallet/ramp'
+import { signMessage } from 'src/wallet/ramp/signature'
 
 export default {
   data () {
     return {
       darkMode: this.$store.getters['darkmode/getStatus'],
+      apiURL: process.env.WATCHTOWER_BASE_URL,
       walletIndex: this.$store.getters['global/getWalletIndex'],
       network: 'BCH',
       menu: 'store',
@@ -88,6 +100,31 @@ export default {
     }
   },
   methods: {
+    async login () {
+      try {
+        const { data } = await this.$axios.get(`${this.apiURL}/auth/otp`, { headers: { 'wallet-hash': this.wallet.walletHash } })
+        const signature = await signMessage(this.wallet.privateKeyWif, data.otp)
+        const body = {
+          wallet_hash: this.wallet.walletHash,
+          signature: signature,
+          public_key: this.wallet.publicKey
+        }
+        await this.$axios.post(`${this.apiURL}/auth/login`, body)
+          .then(response => {
+            // save token as cookie and set to expire 1h later
+            document.cookie = `token=${response.data.token}; expires=${new Date(Date.now() + 3600000).toUTCString()}; path=/`
+            this.user = response.data.user
+            if (this.user) {
+              this.$store.commit('ramp/updateUser', response.data.user)
+              this.proceed = true
+            }
+            this.isLoading = false
+          })
+      } catch (error) {
+        console.error(error.response)
+        this.isLoading = false
+      }
+    },
     switchMenu (item) {
       this.menu = item
       this.$refs.footer.selectMenu(this.menu)
