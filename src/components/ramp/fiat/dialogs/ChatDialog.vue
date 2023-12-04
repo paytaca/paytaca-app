55669a4bfb55d05c6697d5daecca6dc9b3f8f4ee
SeanNikki123 <sslino@up.edu.ph>
2023-12-04 17:17:52 +0800

    WIP Chat dialog for p2p ramp | added working floating button on ramp order | small fixes

---
+++ src/components/ramp/fiat/dialogs/ChatDialog.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-dialog
+    v-model="openChat" @hide="$emit('close')"
+    position="bottom"
+    full-width
+  >
+  <q-card class="br-15" :style="`max-height: ${maxHeight}px;`">
+    <div>Hello World</div>
+    <!-- <div>Chat Title</div>
+    <div>Chat Body</div>
+    <div>Input Here + Button</div> -->
+  </q-card>
+  </q-dialog>
+</template>
+<script>
+
+export default {
+  data () {
+    return {
+      openChat: this.openDialog,
+      maxHeight: this.$q.screen.height * 0.75
+    }
+  },
+  props: {
+    openDialog: Boolean
+  },
+  emits: ['close'],
+  async mounted () {
+    console.log('Hello World')
+  }
+}
+</script>
