0403ecb9967473d6f5d2ea5c08bfe82eeec6254f
SeanNikki123 <sslino@up.edu.ph>
2024-02-06 15:23:48 +0800

    added input validation fot payment method identifier | refresh data on editing or creating payment method | small fix

--- src/components/ramp/fiat/AddPaymentMethods.vue
+++ src/components/ramp/fiat/AddPaymentMethods.vue
@@ -268,6 +268,7 @@ export default {
       if (data !== undefined) {
         this.paymentMethods = data
       }
+      this.dialogType = ''
       this.openDialog = false
     },
     receiveDialogInfo (data) {
@@ -323,7 +324,7 @@ export default {
       this.openDialog = true
     },
     editMethod (data) {
-      this.info = data
+      this.info = { ...data }
       this.selectedMethodIndex = data.id
 
       this.dialogType = 'editPaymentMethod'
@@ -348,7 +349,6 @@ export default {
       } else {
         this.selectedMethods.push(data)
       }
-      // console.log(this.selectedMethods)
     },
     selectButtonColor (type) {
       const temp = this.selectedMethods.map(p => p.payment_type.name)
@@ -482,9 +482,11 @@ export default {
         }
       }
 
-      if (this.dialogType === 'addMethodFromAd') {
-        this.fetchPaymentMethod()
-      }
+      this.fetchPaymentMethod()
+
+      // if (this.dialogType === 'addMethodFromAd') {
+      //   this.fetchPaymentMethod()
+      // }
     },
     submitPaymentMethod () {
       if (this.type === 'General') {
