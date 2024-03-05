5de0060dc9a3bbfaa125cb9a64812feefc1f24f4
khirvy019 <khirvy_19@yahoo.com>
2024-03-05 16:46:14 +0800

    create photo selector component for chat dialog & review form dialog

---
+++ src/components/marketplace/PhotoSelector.vue
@@ -0,0 +1,130 @@
+<template>
+  <div>
+    <q-file
+      v-show="false"
+      ref="fileAttachmentField"
+      borderless
+      :model-value="null"
+      accept="image/*"
+      :filter="files => files.filter(file => file.type?.match(/image\/.*/))"
+      @update:model-value="attemptResize"
+    />
+    <slot v-bind="{ innerVal, selectPhoto }"></slot>
+  </div>
+</template>
+<script>
+import { base64ImageToFile, dataUrlToFile, resizeImage } from 'src/marketplace/chat/attachment'
+import { Camera } from '@capacitor/camera'
+import { useQuasar } from 'quasar'
+import { defineComponent, ref, watch } from 'vue'
+
+export default defineComponent({
+  name: 'PhotoSelector',
+  emits: [
+    'update:modelValue',
+  ],
+  props: {
+    modelValue: [String, File],
+  },
+  setup(props, { emit: $emit }) {
+    const $q = useQuasar()
+
+    const innerVal = ref(props?.modelValue)
+    watch(() => props.modelValue, () => innerVal.value = props.modelValue)
+    watch(innerVal, (newVal, oldVal) => {
+      $emit('update:modelValue', innerVal.value)
+      if (oldVal instanceof File) URL.revokeObjectURL(oldVal)
+      if (newVal instanceof File) newVal.objectUrl = URL.createObjectURL(newVal)
+    })
+
+    const fileAttachmentField = ref()
+    function openFileAttachementField(evt) {
+      if (!fileAttachmentField.value?.pickFiles) {
+        $q.dialog({
+          title: 'Unable to access photo selector',
+          color: 'brandblue',
+        })
+      }
+      fileAttachmentField.value?.pickFiles?.(evt)
+    }
+
+    async function attemptResize(file) {
+      const isFile = file instanceof File
+      if (!isFile) return
+      innerVal.value = await resizeImage({
+        file: file,
+        maxWidthHeight: 640, // based on recommended dimensions for mobile
+      })
+    }
+
+    async function checkOrRequestCameraPermissions() {
+      let permission = await Camera.checkPermissions()
+      const promptStatuses = ['prompt','prompt-with-rationale', 'limited']
+      const request = promptStatuses.includes(permission.photos) || promptStatuses.includes(permission.camera)
+      if (request) permission = await Camera.requestPermissions()
+      return {
+        camera: permission.camera === 'granted',
+        photos: permission.photos === 'photos',
+      }
+    }
+
+    async function getPhotoFromCamera() {
+      const granted = await checkOrRequestCameraPermissions()
+      if (!granted.camera && !granted.photos) {
+        $q.dialog({
+          title: 'Select photo', message: 'Permission denied',
+          color: 'brandblue',
+          class: `br-15 pt-card text-bow ${getDarkModeClass(darkMode.value)}`
+        })
+        return Promise.reject(new Error('Permission Denied'))
+      }
+      return Camera.getPhoto({
+        presentationStyle: 'popover',
+        resultType: 'dataUrl',
+      })
+        .then(async (photo) => {
+          let file
+          if (photo?.base64String) file = base64ImageToFile(photo?.base64String)
+          else if (photo?.dataUrl) file = dataUrlToFile(photo?.dataUrl)
+          if (!file) return photo
+          const resized = await resizeImage({ file, maxWidthHeight: 640 })
+          innerVal.value = resized
+          return photo
+        })
+    }
+
+    function selectPhoto(evt) {
+      return getPhotoFromCamera()
+        .catch(error => {
+          console.error(error)
+          let errorMsg = error?.message
+          if (typeof errorMsg !== 'string') return Promise.reject(error)
+          if (errorMsg?.includes('cancel')) return Promise.resolve()
+          if (errorMsg.match(/user.*denied.*access/i)) {
+            openFileAttachementField(evt)
+            return Promise.resolve()
+          }
+          if (errorMsg?.length < 250) return Promise.reject(error)
+          $q.dialog({
+            title: 'Select photo', message: errorMsg || 'Unknown error occurred',
+            color: 'brandblue',
+            class: `br-15 pt-card text-bow ${getDarkModeClass(darkMode.value)}`
+          })
+        })
+    }
+
+    return {
+      innerVal,
+      fileAttachmentField,
+      openFileAttachementField,
+      attemptResize,
+      selectPhoto,
+    } 
+  },
+})
+</script>
+<style>
+pwa-camera-modal-instance {
+  z-index: 10000
+}
+</style>
--- src/components/marketplace/reviews/ReviewFormDialog.vue
+++ src/components/marketplace/reviews/ReviewFormDialog.vue
@@ -45,30 +45,36 @@
               color="brandblue"
             />
           </div>
-          <q-input
-            dense
-            outlined
-            :disable="loading"
-            autogrow
-            label="Message"
-            v-model="formData.text"
-            type="textarea"
-            input-style="min-height:4rem;"
-            :error="Boolean(formErrors?.text)"
-            :error-message="formErrors?.text"
-            bottom-slots
+          <PhotoSelector
+            :model-value="null"
+            @update:modelValue="photo => photo ? formData.images.push(photo) : null"
+            v-slot="{ selectPhoto }"
           >
-            <template v-slot:append>
-              <q-btn
-                flat
-                :disable="loading"
-                icon="camera_alt"
-                padding="sm"
-                class="q-mt-sm q-r-mr-md"
-                @click="addPhoto"
-              />
-            </template>
-          </q-input>
+            <q-input
+              dense
+              outlined
+              :disable="loading"
+              autogrow
+              label="Message"
+              v-model="formData.text"
+              type="textarea"
+              input-style="min-height:4rem;"
+              :error="Boolean(formErrors?.text)"
+              :error-message="formErrors?.text"
+              bottom-slots
+            >
+              <template v-slot:append>
+                <q-btn
+                  flat
+                  :disable="loading"
+                  icon="camera_alt"
+                  padding="sm"
+                  class="q-mt-sm q-r-mr-md"
+                  @click="selectPhoto"
+                />
+              </template>
+            </q-input>
+          </PhotoSelector>
 
           <div class="row items-start q-gutter-md no-wrap q-mt-sm" style="overflow:auto;">
             <TransitionGroup name="slide-group">
@@ -124,16 +130,18 @@
 <script>
 import { backend } from 'src/marketplace/backend'
 import { Review } from 'src/marketplace/objects'
-import { base64ImageToFile, dataUrlToFile, resizeImage } from 'src/marketplace/chat/attachment'
 import { errorParser } from 'src/marketplace/utils'
 import { getDarkModeClass } from 'src/utils/theme-darkmode-utils'
-import { Camera } from '@capacitor/camera'
 import { useDialogPluginComponent, useQuasar } from 'quasar'
 import { useStore } from 'vuex'
 import { computed, defineComponent, onMounted, ref, watch } from 'vue'
+import PhotoSelector from 'src/components/marketplace/PhotoSelector.vue'
 
 export default defineComponent({
   name: 'ReviewFormDialog',
+  components: {
+    PhotoSelector,
+  },
   emits: [
     'update:modelValue',
     'created',
@@ -178,15 +186,6 @@ export default defineComponent({
       // ]
     })
 
-    watch(() => formData.value.images, (newVal, oldVal) => {
-      if (!Array.isArray(oldVal)) return
-      const isNewValArray = Array.isArray(newVal)
-
-      oldVal
-        .filter(val => !isNewValArray || newVal.includes(val))
-        .forEach(URL.revokeObjectURL)
-    })
-
     function resetFormData() {
       formData.value = {
         rating: parseFloat(props.review?.rating || 0) * (5 / 100),
@@ -209,51 +208,6 @@ export default defineComponent({
         text: '',
         images: '',
       }
-    }
-
-    async function checkOrRequestCameraPermissions() {
-      let permission = await Camera.checkPermissions()
-      const promptStatuses = ['prompt','prompt-with-rationale', 'limited']
-      const request = promptStatuses.includes(permission.photos) || promptStatuses.includes(permission.camera)
-      if (request) permission = await Camera.requestPermissions()
-      return {
-        camera: permission.camera === 'granted',
-        photos: permission.photos === 'photos',
-      }
-    }
-
-    async function getPhotoFromCamera() {
-      const granted = await checkOrRequestCameraPermissions()
-      if (!granted.camera && !granted.photos) {
-        $q.dialog({
-          title: 'Select photo', message: 'Permission denied',
-          color: 'brandblue',
-          class: `br-15 pt-card text-bow ${getDarkModeClass(darkMode.value)}`
-        })
-        return Promise.reject(new Error('Permission Denied'))
-      }
-      return Camera.getPhoto({
-        presentationStyle: 'popover',
-        resultType: 'dataUrl',
-      })
-        .then(async (photo) => {
-          let file
-          if (photo?.base64String) file = base64ImageToFile(photo?.base64String)
-          else if (photo?.dataUrl) file = dataUrlToFile(photo?.dataUrl)
-          if (!file) return
-
-          const resized = await resizeImage({ file, maxWidthHeight: 640 })
-          resized.objectUrl = URL.createObjectURL(resized)
-          return resized
-        })
-    }
-
-    function addPhoto(evt) {
-      return getPhotoFromCamera()
-        .then(file => {
-          formData.value.images.push(file)
-          return file
-        })
     }
 
     function serializeFormData() {
@@ -377,8 +331,6 @@ export default defineComponent({
       loading,
       formData,
       formErrors,
-
-      addPhoto,
 
       onSubmit,
       deleteReview,
