27be992a181323ceb93ac3f93d6bd3eeed81eff8
LukeHeimen <kdlanante@up.edu.ph>
2024-05-27 13:36:31 +0800

    Removed qrjs; used a different library for reading uploaded QR

--- src/components/registration/ShardsImport.vue
+++ src/components/registration/ShardsImport.vue
@@ -4,23 +4,16 @@
     @decode="onScannerDecode"
   />
 
-  <!-- hidden q-uploader -->
-  <q-uploader
-    ref="file-upload"
-    v-model="fileModel"
-    accept=".jpg, .png, image/*"
+  <qrcode-capture
+    ref="qr-upload"
+    capture="null"
     style="display: none;"
-    @added="uploadedQrImage"
+    @detect="onUploadDetect"
   />
 
   <div class="text-bow q-px-lg" :class="getDarkModeClass(darkMode)">
     <p class="text-center text-subtitle1">
       Restore your Paytaca wallet from the QR code of its shards
-    </p>
-
-    <p class="q-pa-sm text-center" style="border: 3px solid orange;">
-      Before uploading a screenshot containing the QR, make sure that you crop
-      the image up to the red square containing the QR before uploading.
     </p>
 
     <div class="row flex items-center justify-between q-py-md">
@@ -42,7 +35,7 @@
           class="btn-scan button text-white bg-grad"
           icon="upload"
           :disable="disablePersonal"
-          @click="triggerUpload(), isPersonalClicked = true"
+          @click="isPersonalClicked = true, $refs['qr-upload'].$el.click()"
         />
       </div>
     </div>
@@ -66,7 +59,7 @@
           class="btn-scan button text-white bg-grad"
           icon="upload"
           :disable="disableForSharing"
-          @click="triggerUpload(), isForSharingClicked = true"
+          @click="isForSharingClicked = true, $refs['qr-upload'].$el.click()"
         />
       </div>
     </div>
@@ -99,7 +92,6 @@
         <div class="qr-placeholder-div"></div>
       </template>
     </div>
-    <!-- request another shard from us -->
 
     <div class="flex flex-center">
       <q-btn
@@ -121,8 +113,8 @@
 </template>
 
 <script>
-import jsqr from 'jsqr'
 import sss from 'shamirs-secret-sharing'
+import { QrcodeCapture } from 'vue-qrcode-reader'
 import { getDarkModeClass } from 'src/utils/theme-darkmode-utils'
 
 import QrScanner from 'src/components/qr-scanner.vue'
@@ -136,7 +128,8 @@ export default {
   ],
 
   components: {
-    QrScanner
+    QrScanner,
+    QrcodeCapture
   },
 
   data () {
@@ -168,63 +161,18 @@ export default {
       vm.addData(content)
       vm.validateQRCodes()
     },
-    triggerUpload () {
-      const fileUploadRef = this.$refs['file-upload']
-      fileUploadRef.removeUploadedFiles()
-      fileUploadRef.removeQueuedFiles()
-      fileUploadRef.pickFiles()
-    },
-    uploadedQrImage (qrFile) {
-      const vm = this
-      const reader = new FileReader()
-      reader.onload = () => {
-        const image = new Image()
-        image.onload = () => {
-          const canvas = document.createElement('canvas')
-          const context = canvas.getContext('2d')
-          canvas.width = image.width
-          canvas.height = image.height
-          context.drawImage(image, 0, 0)
-          let imageData = context.getImageData(0, 0, image.width, image.height)
-
-          const cropCoords = vm.processUploadedQr(imageData)
-          if (cropCoords) {
-            context.clearRect(0, 0, image.width, image.height)
-            canvas.width = cropCoords.x1 - cropCoords.x
-            canvas.height = cropCoords.y1 - cropCoords.y
-            context.drawImage(image, cropCoords.x, cropCoords.y, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height)
-            imageData = context.getImageData(0, 0, canvas.width, canvas.height)
-          }
-
-          const qrCode = jsqr(imageData.data, imageData.width, imageData.height, { inversionAttempts: 'attemptBoth' })
-
-          if (qrCode) {
-            vm.addData(qrCode.data)
-            vm.validateQRCodes()
-          } else {
-            vm.$q.notify({
-              message: 'No QR code found in the uploaded image.',
-              timeout: 800,
-              color: 'red-9',
-              icon: 'mdi-qrcode-remove'
-            })
-
-            if (vm.isPersonalClicked && vm.retrievedCodes[0] === null) {
-              vm.isPersonalClicked = false
-            }
-
-            if (vm.isForSharingClicked && vm.retrievedCodes[1] === null) {
-              vm.isForSharingClicked = false
-            }
-          }
-        }
-        image.src = reader.result
+    onUploadDetect (detectedCode) {
+      if (detectedCode.length > 0) {
+        this.addData(detectedCode[0].rawValue)
+        this.validateQRCodes()
+      } else {
+        this.$q.notify({
+          message: 'No QR code found in the uploaded image.',
+          timeout: 800,
+          color: 'red-9',
+          icon: 'mdi-qrcode-remove'
+        })
       }
-      reader.readAsDataURL(qrFile[0])
-
-      const fileUploadRef = this.$refs['file-upload']
-      fileUploadRef.removeUploadedFiles()
-      fileUploadRef.removeQueuedFiles()
     },
     validateQRCodes () {
       const vm = this
@@ -234,6 +182,14 @@ export default {
         vm.areQRCodesValid = recovered.split(' ').length === 12
         vm.qrCodeDivClass = vm.areQRCodesValid ? 'match' : 'not-match'
         vm.$emit('set-seed-phrase', recovered)
+      }
+
+      if (vm.isPersonalClicked && vm.retrievedCodes[0] === null) {
+        vm.isPersonalClicked = false
+      }
+
+      if (vm.isForSharingClicked && vm.retrievedCodes[1] === null) {
+        vm.isForSharingClicked = false
       }
     },
     clearQRs () {
@@ -259,47 +215,6 @@ export default {
         vm.disableForSharing = true
         vm.isForSharingClicked = false
       }
-    },
-    processUploadedQr (imageData) {
-      // crop qr code from the image if the image contains the red square with the QR inside
-      const data = imageData.data
-      let j = 1, k = 1
-      let hasDetectedRedSquarePixel = false
-      const startingRedSquarePixel = { x: -1, y: -1 }
-
-      for (let i = 0; i < data.length; i += 4) {
-        // rgb 237, 95, 89 color of red square
-        const r = data[i]
-        const g = data[i + 1]
-        const b = data[i + 2]
-        const rThreshold = r >= 199 && r <= 237
-        const gThreshold = g >= 95 && g <= 109
-        const bThreshold = b >= 89 && b <= 108
-
-        if (rThreshold && gThreshold && bThreshold && !hasDetectedRedSquarePixel) {
-          hasDetectedRedSquarePixel = true
-          startingRedSquarePixel.x = k
-          startingRedSquarePixel.y = j
-        }
-
-        if (i / 4 / j === imageData.width) {
-          j += 1
-          k = 1
-        }
-
-        k++
-      }
-
-      if (startingRedSquarePixel.x > 5 && startingRedSquarePixel.y > 5) {
-        const x = startingRedSquarePixel.x + 15 // border thickness
-        const y = startingRedSquarePixel.y + 4 + 45 // border thickness + space occupied by text
-        const x1 = x + 225 // width of qr - border thickness - padding
-        const y1 = y + 225 // height of qr - border thickness - padding
-
-        return { x, y, x1, y1 }
-      }
-
-      return null
     }
   }
 }
