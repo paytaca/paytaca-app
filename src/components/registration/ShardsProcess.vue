26fd4bd879ef46ebbeca01f2d9c8667ad7e40aa3
LukeHeimen <kdlanante@up.edu.ph>
2024-05-29 15:30:49 +0800

    Separated shard backup phase desktop and mobile UI

--- src/components/registration/ShardsProcess.vue
+++ src/components/registration/ShardsProcess.vue
@@ -16,59 +16,65 @@
   </template>
 
   <template v-else>
-    <h5
-      v-if="!fromWalletInfo"
-      class="q-ma-none text-bow"
-      :class="getDarkModeClass(darkMode)"
-    >
-      {{ $t('ShardsBackupPhase') }}
-    </h5>
-    <p
-      :class="[fromWalletInfo ? 'text-bow' : 'dim-text', getDarkModeClass(darkMode)]"
-      style="margin-top: 10px;"
-    >
-      {{ fromWalletInfo ? $t('ShardsBackupPhaseDescription1') : $t('ShardsBackupPhaseDescription2') }}
-      {{ $t('ShardsBackupPhaseDescription3') }}
-    </p>
+    <template v-if="isDesktop">
+      <h5
+        v-if="!fromWalletInfo"
+        class="q-ma-none text-bow"
+        :class="getDarkModeClass(darkMode)"
+      >
+        {{ $t('ShardsBackupPhase') }}
+      </h5>
+      <p
+        :class="[fromWalletInfo ? 'text-bow' : 'dim-text', getDarkModeClass(darkMode)]"
+        style="margin-top: 10px;"
+      >
+        {{ fromWalletInfo ? $t('ShardsBackupPhaseDescription1') : $t('ShardsBackupPhaseDescription2') }}
+        {{ $t('ShardsBackupPhaseDescription3') }}
+      </p>
 
-    <div class="q-mt-lg text-bow" :class="getDarkModeClass(darkMode)">
-      <div
-        class="q-pa-sm br-15 pt-card"
-        :class="getDarkModeClass(darkMode)"
-        style="border: 2px solid gray;"
-      >
-        <div class="text-center q-mb-sm">
-          {{ $t('PersonalQRDescription1') }}
+      <div class="q-mt-lg text-bow" :class="getDarkModeClass(darkMode)">
+        <div
+          class="q-pa-sm br-15 pt-card"
+          :class="getDarkModeClass(darkMode)"
+          style="border: 2px solid gray;"
+        >
+          <div class="text-center q-mb-sm">
+            {{ $t('PersonalQRDescription1') }}
+          </div>
+          <div id="personal-qr" class="flex flex-center q-py-md col-qr-code">
+            <p style="color: black; margin-bottom: 0;">First Shard</p>
+            <p style="color: black">{{ $t('PersonalQRDescription2') }}</p>
+            <qr-code :text="shards[1]" color="#253933" :size="200" error-level="H" />
+          </div>
         </div>
-        <div id="personal-qr" class="flex flex-center q-py-md col-qr-code">
-          <p style="color: black; margin-bottom: 0;">First Shard</p>
-          <p style="color: black">{{ $t('PersonalQRDescription2') }}</p>
-          <qr-code :text="shards[1]" color="#253933" :size="200" error-level="H" />
+        <div
+          class="q-pa-sm q-mt-md br-15 pt-card"
+          :class="getDarkModeClass(darkMode)"
+          style="border: 2px solid gray;"
+        >
+          <div class="text-center q-mb-sm">
+            {{ $t('ForSharingQRDescription1') }}
+          </div>
+          <div id="sharing-qr" class="flex flex-center q-py-md col-qr-code">
+            <p style="color: black; margin-bottom: 0;">Second Shard</p>
+            <p style="color: black">{{ $t('ForSharingQRDescription2') }}</p>
+            <qr-code :text="shards[2]" color="#253933" :size="200" error-level="H" />
+          </div>
+        </div>
+        <div class="flex flex-center q-mt-md">
+          <q-btn
+            rounded
+            :label="$t('DownloadQRCodeImages')"
+            class="button"
+            @click="takeScreenshot()"
+          />
         </div>
       </div>
-      <div
-        class="q-pa-sm q-mt-md br-15 pt-card"
-        :class="getDarkModeClass(darkMode)"
-        style="border: 2px solid gray;"
-      >
-        <div class="text-center q-mb-sm">
-          {{ $t('ForSharingQRDescription1') }}
-        </div>
-        <div id="sharing-qr" class="flex flex-center q-py-md col-qr-code">
-          <p style="color: black; margin-bottom: 0;">Second Shard</p>
-          <p style="color: black">{{ $t('ForSharingQRDescription2') }}</p>
-          <qr-code :text="shards[2]" color="#253933" :size="200" error-level="H" />
-        </div>
-      </div>
-      <div class="flex flex-center q-mt-md">
-        <q-btn
-          rounded
-          :label="$t('DownloadQRCodeImages')"
-          class="button"
-          @click="takeScreenshot()"
-        />
-      </div>
-    </div>
+    </template>
+
+    <template v-else>
+      for mobile yey
+    </template>
 
     <q-btn
       v-if="!fromWalletInfo"
@@ -85,7 +91,6 @@
 import sss from 'shamirs-secret-sharing'
 import html2canvas from 'html2canvas'
 
-import { Camera } from '@capacitor/camera'
 import { toHex } from 'hex-my-bytes'
 import { isNotDefaultTheme, getDarkModeClass } from 'src/utils/theme-darkmode-utils'
 import { saveShardToWatchtower } from 'src/wallet/shards'
@@ -166,6 +171,21 @@ export default {
       this.$q.notify({ message, timeout: 800, color, icon })
     },
     takeScreenshot () {
+      const saveToDesktop = (image, fileName, shouldDisplayNotif = false) => {
+        try {
+          const link = document.createElement('a')
+          link.href = image
+          link.download = fileName
+          link.click()
+
+          if (shouldDisplayNotif) {
+            this.displayNotif('QR code images saved successfully.', 'blue-9', 'mdi-qrcode-plus')
+          }
+        } catch (error) {
+          this.displayNotif('An error occurred while saving the QR code images.', 'red-9', 'mdi-qrcode-remove')
+        }
+      }
+
       const vm = this
       document.addEventListener('deviceready', () => {}, false)
 
@@ -173,73 +193,18 @@ export default {
       html2canvas(personalQrElement).then((canvas) => {
         const image = canvas.toDataURL('image/png')
         const fileName = `qr-first-shard-${vm.walletHash.substring(0, 10)}.png`
-
-        if (vm.$q.platform.is.mobile) {
-          vm.saveToMobile(image, fileName)
-        } else if (this.isDesktop) {
-          vm.saveToDesktop(image, fileName)
-        }
+        saveToDesktop(image, fileName)
       })
 
       const sharingQrElement = document.getElementById('sharing-qr')
       html2canvas(sharingQrElement).then((canvas) => {
         const image = canvas.toDataURL('image/png')
         const fileName = `qr-second-shard-${vm.walletHash.substring(0, 10)}.png`
-
-        if (vm.$q.platform.is.mobile) {
-          vm.saveToMobile(image, fileName, true)
-        } else if (this.isDesktop) {
-          vm.saveToDesktop(image, fileName, true)
-        }
+        saveToDesktop(image, fileName, true)
       })
 
       if (!vm.fromWalletInfo) {
         vm.disableContinue = false
-      }
-    },
-    async saveToMobile (image, fileName, shouldDisplayNotif = false) {
-      if (this.$q.platform.is.android) {
-        const filePath = `${cordova.file.externalRootDirectory}Pictures/${fileName}`
-        // eslint-disable-next-line no-undef
-        const fileTransfer = new FileTransfer()
-        try {
-          fileTransfer.download(image, filePath, () => {
-            if (shouldDisplayNotif) {
-              this.displayNotif('QR code images saved successfully.', 'blue-9', 'mdi-qrcode-plus')
-            }
-          }, (error) => {
-            console.log(error)
-            this.displayNotif('An error occurred while saving the QR code images.', 'red-9', 'mdi-qrcode-remove')
-          })
-        } catch (error) {
-          console.log(error)
-          this.displayNotif('An error occurred while saving the QR code images.', 'red-9', 'mdi-qrcode-remove')
-        }
-      } else if (this.$q.platform.is.ios) {
-        try {
-          await Camera.savePhoto({
-            path: fileName,
-            data: image,
-            directory: 'photos'
-          })
-          this.displayNotif('QR code image saved successfully.', 'blue-9', 'mdi-qrcode-plus')
-        } catch (error) {
-          this.displayNotif('An error occurred while saving the QR code image.', 'red-9', 'mdi-qrcode-remove')
-        }
-      }
-    },
-    saveToDesktop (image, fileName, shouldDisplayNotif = false) {
-      try {
-        const link = document.createElement('a')
-        link.href = image
-        link.download = fileName
-        link.click()
-
-        if (shouldDisplayNotif) {
-          this.displayNotif('QR code images saved successfully.', 'blue-9', 'mdi-qrcode-plus')
-        }
-      } catch (error) {
-        this.displayNotif('An error occurred while saving the QR code images.', 'red-9', 'mdi-qrcode-remove')
       }
     }
   }
