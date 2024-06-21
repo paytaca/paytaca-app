e725c609fb3e60f961e67d62629ae9133c810d97
khirvy019 <khirvy_19@yahoo.com>
2024-06-21 13:41:17 +0800

    force cashtoken assets' decimals to be an int

--- src/pages/transaction/send.vue
+++ src/pages/transaction/send.vue
@@ -1023,14 +1023,21 @@ export default {
         getter = 'sep20/getAsset'
       }
       const assets = this.$store.getters[getter](id)
+
+      let asset
       if (assets.length > 0) {
-        return assets[0]
+        asset = assets[0]
       } else {
-        return {
+        asset = {
           id: this.assetId,
           symbol: this.symbol
         }
       }
+
+      if (id?.startsWith?.('ct/') && asset) {
+        asset.decimals = parseInt(asset.decimals) || 0
+      }
+      return asset
     },
     async setMaximumSendAmount () {
       const currentInputExtras = this.inputExtras[this.currentActiveRecipientIndex]
