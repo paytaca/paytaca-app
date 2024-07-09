2d3a97dc4e622a791ab974d2368e53c673edc1a1
SeanNikki123 <sslino@up.edu.ph>
2024-07-09 15:24:21 +0800

    added previous fixes

--- src/pages/apps/exchange/peer_to_peer/order-form.vue
+++ src/pages/apps/exchange/peer_to_peer/order-form.vue
@@ -606,6 +606,7 @@ export default {
       }
     },
     updateInput (max = false, min = false) {
+      console.log('testing')
       if (!this.isloaded) return
       let amount = this.amount
       if (min) {
@@ -648,7 +649,44 @@ export default {
           amount = amount / price
         }
       }
+
       this.amount = parseFloat(amount.toFixed(this.byFiat ? 2 : 8))
+
+      // check if valid amount
+      let temp = this.amount
+      let cont = true
+
+      if (this.byFiat) {
+        temp = this.equivalentAmount
+      }
+
+      if (temp < parseFloat(this.ad.trade_floor)) {
+        cont = false
+        temp = parseFloat(this.ad.trade_floor)
+
+        if (this.byFiat) {
+          temp = temp * this.ad.price
+          temp = parseFloat((Number(temp) + 0.01).toFixed(2))
+        } else {
+          temp = parseFloat((Number(temp) + 0.00000001).toFixed(8))
+        }
+      }
+
+      if ((temp > this.minTradeAmount(this.ad)) && cont) {
+        cont = false
+        temp = parseFloat(this.minTradeAmount(this.ad))
+
+        if (this.byFiat) {
+          temp = temp * this.ad.price
+          temp = parseFloat((Number(temp) - 0.01).toFixed(2))
+        } else {
+          temp = parseFloat((Number(temp) - 0.00000001).toFixed(8))
+        }
+      }
+
+      if (!cont) {
+        this.amount = temp
+      }
     },
     tradeLimitsCurrency (ad) {
       return (ad.trade_limits_in_fiat ? ad.fiat_currency.symbol : ad.crypto_currency.symbol)
