a53153af42f2ce9dd6652b8c6bbc8b7aa39f5d7f
Marie Edcel Balite <edcel@scibizinformatics.com>
2024-07-24 16:02:40 +0800

    Fix missing handling of 404 error when fetching nonexisting peer

--- src/pages/apps/exchange/index.vue
+++ src/pages/apps/exchange/index.vue
@@ -36,14 +36,21 @@ export default {
     getDarkModeClass,
     isNotDefaultTheme,
     async getUser () {
-      await backend.get('auth').then((response) => {
-        this.showLogin = !response?.data?.is_authenticated
-        this.user = response.data
-        if (!this.showLogin) {
-          this.isloaded = true
-          this.goToMainPage()
-        }
-      })
+      await backend.get('auth')
+        .then((response) => {
+          this.showLogin = !response?.data?.is_authenticated
+          this.user = response.data
+          if (!this.showLogin) {
+            this.isloaded = true
+            this.goToMainPage()
+          }
+        })
+        .catch(error => {
+          console.log(error.response || error)
+          if (error.response?.status === 404) {
+            this.showLogin = true
+          }
+        })
     },
     onLoggedIn () {
       this.showLogin = false
