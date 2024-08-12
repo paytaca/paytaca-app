094308aa8cdd7b68c03044f68331191404256f0e
Marie Edcel Balite <edcel@scibizinformatics.com>
2024-08-12 10:47:38 +0800

    Removed duplicate import of NetworkError component

--- src/pages/apps/exchange/index.vue
+++ src/pages/apps/exchange/index.vue
@@ -14,7 +14,6 @@
 import RampLogin from 'src/components/ramp/fiat/RampLogin.vue'
 import ProgressLoader from 'src/components/ProgressLoader.vue'
 import NetworkError from 'src/components/ramp/fiat/NetworkError.vue'
-import NetworkError from 'src/components/ramp/fiat/NetworkError.vue'
 import { getDarkModeClass, isNotDefaultTheme } from 'src/utils/theme-darkmode-utils'
 import { backend } from 'src/exchange/backend'
 import { bus } from 'src/wallet/event-bus.js'
