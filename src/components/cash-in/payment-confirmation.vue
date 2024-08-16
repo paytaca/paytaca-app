f4ab6ecb48c88e2b3043f154a4c91e389b225ddb
Marie Edcel Balite <edcel@scibizinformatics.com>
2024-08-16 13:30:32 +0800

    Resolved merge conflict

--- src/components/cash-in/payment-confirmation.vue
+++ src/components/cash-in/payment-confirmation.vue
@@ -56,11 +56,33 @@
         </q-expansion-item>
       </q-card>
 
-      <!-- <q-file clearable class="q-pt-md" filled dense outlined color="blue-12" v-model="attachment" label="Upload Receipt">
-        <template v-slot:prepend>
-          <q-icon name="attach_file" />
-        </template>
-      </q-file> -->
+    <div v-if="order?.payment_methods_selected[0]?.attachments[0]?.image?.url">
+      <div class="row q-mt-md justify-center">
+        <q-img
+          :src="order?.payment_methods_selected[0]?.attachments[0]?.image?.url"
+          style="height: 140px; max-width: 150px">
+          <template v-slot:error>
+            <div class="absolute-full flex flex-center text-white">
+              Cannot load image
+            </div>
+          </template>
+        </q-img>
+      </div>
+      <div class="text-center q-my-sm" style="font-style: italic">Proof of payment</div>
+    </div>
+    <div v-else class="text-center q-mt-md" style="font-style: italic; color: red">Please upload your proof of payment</div>
+
+    <q-file :clearable="!uploading" class="q-pt-sm" filled dense outlined color="blue-12" v-model="attachment" label="Upload Receipt">
+      <template v-slot:prepend>
+        <q-icon name="image" />
+      </template>
+      <template v-slot:append>
+        <q-btn flat dense :disable="!attachment" :loading="uploading" padding="none" icon="cloud_upload" @click="uploadPaymentAttachment" />
+      </template>
+    </q-file>
+
+    <div class="row justify-center q-mt-sm">
+      <q-btn class="col q-mx-lg" rounded color="blue-6" label="I have Paid" @click="onPaid"/>
     </div>
   </q-scroll-area>
   <div class="row justify-center q-mt-md">
@@ -69,16 +91,23 @@
 </template>
 <script>
 import { getDarkModeClass } from 'src/utils/theme-darkmode-utils'
+import { backend } from 'src/exchange/backend'
 
 export default {
   data () {
     return {
-      attachment: null
+      attachment: null,
+      uploading: false
     }
   },
   emits: ['confirm-payment'],
   props: {
     order: Object
+  },
+  watch: {
+    attachment (val) {
+      console.log('attachment:', val)
+    }
   },
   computed: {
     darkMode () {
@@ -88,8 +117,29 @@ export default {
       return Number((Number(this.order?.crypto_amount) * Number(this.order?.locked_price)).toFixed(2)).toLocaleString()
     }
   },
+  mounted () {
+    console.log('order:', this.order)
+  },
   methods: {
     getDarkModeClass,
+    async uploadPaymentAttachment () {
+      this.uploading = true
+      const formData = new FormData()
+      console.log('payment_methods_selected:', this.order?.payment_methods_selected)
+      formData.append('payment_id', this.order?.payment_methods_selected[0]?.order_payment_id)
+      formData.append('image', this.attachment)
+      console.log('attachment___:', this.attachment)
+      await backend.post(
+        '/ramp-p2p/order/payment/upload-attachment',
+        formData, { headers: { 'Content-Type': 'multipart/form-data' }, authorize: true })
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => {
+          console.error(error.response || error)
+        })
+      this.uploading = false
+    },
     onPaid () {
       this.$emit('confirm-payment')
     },
