c13db9933ff7d0258709ca46d83527c41bea7f52
joemarct <joemar.ct@gmail.com>
2024-09-07 00:39:11 +0800

    Encrypt shard to shorten gift code when creating a gift

--- src/pages/apps/gifts/create-gift.vue
+++ src/pages/apps/gifts/create-gift.vue
@@ -6,6 +6,7 @@
         backnavpath="/apps/gifts"
         class="q-px-sm apps-header gift-app-header"
       />
+      <button @click="encryptShard()">Generate Key</button>
       <div class="q-pa-lg" style="width: 100%; color: black;">
         <div class="text-center" v-if="processing">
           <p :class="{'text-white': darkMode}" >{{ $t('CreatingGift') }}</p>
@@ -162,6 +163,10 @@ import sha256 from 'js-sha256'
 import { getAssetDenomination, parseFiatCurrency } from 'src/utils/denomination-utils'
 import { getDarkModeClass, isNotDefaultTheme } from 'src/utils/theme-darkmode-utils'
 
+const aesjs = require('aes-js')
+const short = require('short-uuid')
+const pbkdf2 = require('pbkdf2')
+
 export default {
   name: 'Gifts',
   components: {
@@ -250,6 +255,25 @@ export default {
     parseFiatCurrency,
     getDarkModeClass,
     isNotDefaultTheme,
+    encryptShard(shard) {
+      const password = short.generate()
+      const key = pbkdf2.pbkdf2Sync(password, '_saltDefault2024', 1, 128 / 8, 'sha512')
+      const textBytes = aesjs.utils.utf8.toBytes(shard)
+      
+      const aesCtr = new aesjs.ModeOfOperation.ctr(key)
+      const encryptedBytes = aesCtr.encrypt(textBytes)
+      const encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes)
+
+      // const encryptedBytes2 = aesjs.utils.hex.toBytes(encryptedHex)
+      // const aesCtr2 = new aesjs.ModeOfOperation.ctr(key)
+      // const decryptedBytes = aesCtr2.decrypt(encryptedBytes2)
+      // var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes)
+      // console.log(decryptedText)
+      return {
+        code: password,
+        encryptedHex: encryptedHex
+      }
+    },
     disableGenerateButton () {
       if (this.amountBCH > 0) {
         if (this.$refs.amountInput && !this.$refs.amountInput.hasError) {
@@ -278,19 +302,31 @@ export default {
       const bchjs = new BCHJS()
       const pair = bchjs.ECPair.fromWIF(wif)
       const address = bchjs.ECPair.toCashAddress(pair)
+      console.log(address)
 
       const sss = require('shamirs-secret-sharing')
       const secret = Buffer.from(wif)
       const stateShare = sss.split(secret, { shares: 3, threshold: 2 })
       const shares = stateShare.map((share) => { return toHex(share) })
       // console.log(shares)
-      vm.giftCodeHash = sha256(shares[0])
+      const encryptedShard = this.encryptShard(shares[0])
+
+      // vm.giftCodeHash = sha256(shares[0])
+      // const payload = {
+      //   gift_code_hash: vm.giftCodeHash,
+      //   address: address,
+      //   share: shares[1],
+      //   amount: parseFloat(vm.amountBCH)
+      // }
+      vm.giftCodeHash = sha256(encryptedShard.code)
       const payload = {
         gift_code_hash: vm.giftCodeHash,
+        encryptedShare: encryptedShard.encryptedHex,
         address: address,
         share: shares[1],
-        amount: parseFloat(vm.amountBCH)
+        amount: parseFloat(vm.amountBCH),
       }
+      console.log(payload)
       if (vm.selectedCampaign) {
         if (vm.createNewCampaign) {
           payload.campaign = {
@@ -307,12 +343,12 @@ export default {
       const url = `https://gifts.paytaca.com/api/gifts/${walletHash}/create/`
       axios.post(url, payload).then((resp) => {
         if (resp.status === 200) {
-          vm.qrCodeContents = shares[0]
+          vm.qrCodeContents = encryptedShard.code
           vm.wallet.BCH.sendBch(this.amountBCH, address).then(function (result, err) {
             if (result.success) {
               vm.processing = false
               vm.$store.dispatch('gifts/saveGift', { giftCodeHash: vm.giftCodeHash, share: shares[2] })
-              vm.$store.dispatch('gifts/saveQr', { giftCodeHash: vm.giftCodeHash, qr: shares[0] })
+              vm.$store.dispatch('gifts/saveQr', { giftCodeHash: vm.giftCodeHash, qr: encryptedShard.code })
               vm.completed = true
 
               vm.wallet.BCH.getBalance().then(function (response) {
