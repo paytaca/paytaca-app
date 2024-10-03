954a5af312c537d92a9e969610bfce8b1f50f2a9
LukeHeimen <kdlanante@up.edu.ph>
2024-10-03 09:57:08 +0800

    Added pull-to-refresh component to refresh notifs list aside from refresh button

--- src/components/notifications/index.vue
+++ src/components/notifications/index.vue
@@ -20,106 +20,108 @@
       </div>
 
       <div>
-        <div class="row justify-end items-center q-mb-sm q-gutter-x-md">
-          <q-btn
-            flat
-            round
-            icon="refresh"
-            :disable="isLoading"
-            @click="refreshNotifsList()"
-          />
-          <q-btn
-            flat
-            round
-            :disable="isLoading"
-            icon="filter_alt"
-          />
-          <q-btn
-            flat
-            round
-            :disable="isLoading"
-            icon="settings"
-          />
-        </div>
+        <q-pull-to-refresh @refresh="refreshNotifsList">
+          <div class="row justify-end items-center q-mb-sm q-gutter-x-md">
+            <q-btn
+              flat
+              round
+              icon="refresh"
+              :disable="isLoading"
+              @click="refreshNotifsList()"
+            />
+            <q-btn
+              flat
+              round
+              :disable="isLoading"
+              icon="filter_alt"
+            />
+            <q-btn
+              flat
+              round
+              :disable="isLoading"
+              icon="settings"
+            />
+          </div>
 
-        <template v-if="isLoading">
-          <q-card-section class="q-pt-sm flex flex-center">
-            <ProgressLoader :color="isNotDefaultTheme(theme) ? theme : 'pink'"/>
-          </q-card-section>
-        </template>
+          <template v-if="isLoading">
+            <q-card-section class="q-pt-sm flex flex-center">
+              <ProgressLoader :color="isNotDefaultTheme(theme) ? theme : 'pink'"/>
+            </q-card-section>
+          </template>
 
-        <template v-else>
-          <div v-if="notifsList.length > 0">
-            <div
-              class="q-pb-sm q-gutter-y-sm"
-              style="height: 70vh; overflow-y: scroll;"
-            >
-              <transition-group
-                appear
-                leave-active-class="animated zoomOut fast"
-                v-for="(notif, index) in notifsList"
-                :key="`notif-${index}`"
+          <template v-else>
+            <div v-if="notifsList.length > 0">
+              <div
+                class="q-pb-sm q-gutter-y-sm"
+                style="height: 70vh; overflow-y: scroll;"
               >
-                <q-slide-item
-                  left-color="red"
-                  right-color="red"
-                  class="pt-card-2 text-bow item-border"
-                  :class="getDarkModeClass(darkMode)"
+                <transition-group
+                  appear
+                  leave-active-class="animated zoomOut fast"
+                  v-for="(notif, index) in notifsList"
                   :key="`notif-${index}`"
-                  @left="(event) => onSwipe(event, index)"
-                  @right="(event) => onSwipe(event, index)"
-                  v-if="!notif.is_hidden"
                 >
-                  <template v-slot:left>
-                    <q-icon name="delete" /> Delete
-                  </template>
-                  <template v-slot:right>
-                    Delete <q-icon name="delete" />
-                  </template>
-
-                  <transition
-                    appear
-                    leave-active-class="animated zoomOut fast"
+                  <q-slide-item
+                    left-color="red"
+                    right-color="red"
+                    class="pt-card-2 text-bow item-border"
+                    :class="getDarkModeClass(darkMode)"
+                    :key="`notif-${index}`"
+                    @left="(event) => onSwipe(event, index)"
+                    @right="(event) => onSwipe(event, index)"
+                    v-if="!notif.is_hidden"
                   >
-                    <div class="row q-py-sm q-px-md">
-                      <span class="row col-12 q-mb-sm text-bold" style="font-size: 17px;">
-                        {{ notif.title }}
-                      </span><br/>
-                      <span class="col-12">{{ notif.message }}</span>
-                      <span
-                        class="col-12 q-mt-xs text-caption"
-                        align="right"
-                        style="color: gray;"
-                      >
-                        {{ parseNotifType(notif.notif_type) }} | {{ formatDate(notif.date_posted) }}
-                      </span>
-                    </div>
-                  </transition>
-                </q-slide-item>
-              </transition-group>
+                    <template v-slot:left>
+                      <q-icon name="delete" /> Delete
+                    </template>
+                    <template v-slot:right>
+                      Delete <q-icon name="delete" />
+                    </template>
+
+                    <transition
+                      appear
+                      leave-active-class="animated zoomOut fast"
+                    >
+                      <div class="row q-py-sm q-px-md">
+                        <span class="row col-12 q-mb-sm text-bold" style="font-size: 17px;">
+                          {{ notif.title }}
+                        </span><br/>
+                        <span class="col-12">{{ notif.message }}</span>
+                        <span
+                          class="col-12 q-mt-xs text-caption"
+                          align="right"
+                          style="color: gray;"
+                        >
+                          {{ parseNotifType(notif.notif_type) }} | {{ formatDate(notif.date_posted) }}
+                        </span>
+                      </div>
+                    </transition>
+                  </q-slide-item>
+                </transition-group>
+              </div>
+
+              <div class="row flex-center q-mt-sm">
+                <q-pagination
+                  padding="xs"
+                  :modelValue="notifsPage"
+                  :max="20"
+                  :max-pages="6"
+                  :dark="darkMode"
+                  :class="getDarkModeClass(darkMode)"
+                  :hide-below-pages="2"
+                />
+              </div>
             </div>
 
-            <div class="row flex-center q-mt-sm">
-              <q-pagination
-                padding="xs"
-                :modelValue="notifsPage"
-                :max="20"
-                :max-pages="6"
-                :dark="darkMode"
-                :class="getDarkModeClass(darkMode)"
-                :hide-below-pages="2"
-              />
+            <div
+              class="text-center text-subtitle1"
+              style="color: gray"
+              v-else
+            >
+              No notifications
             </div>
-          </div>
-
-          <div
-            class="text-center text-subtitle1"
-            style="color: gray"
-            v-else
-          >
-            No notifications
-          </div>
-        </template>
+          </template>
+        </q-pull-to-refresh>
       </div>
     </q-card>
   </q-dialog>
@@ -171,8 +173,10 @@ export default {
     isNotDefaultTheme,
     parseNotifType,
 
-    async refreshNotifsList () {
+    async refreshNotifsList (done) {
       const vm = this
+
+      if (done) done()
 
       vm.isLoading = true
       vm.notifsList = await getWalletNotifications(vm.currentWalletHash)
