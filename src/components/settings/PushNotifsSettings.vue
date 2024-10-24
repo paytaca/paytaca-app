a3da8dbf4670b1c5015ced96374842048dcc358c
LukeHeimen <kdlanante@up.edu.ph>
2024-10-24 15:02:12 +0800

    Added loading animation in push notifications toggle while doing promise tasks in background

--- src/components/settings/PushNotifsSettings.vue
+++ src/components/settings/PushNotifsSettings.vue
@@ -9,16 +9,21 @@
           </q-item-label>
         </q-item-section>
         <q-item-section avatar>
-          <q-toggle
-            v-model="enablePushNotifs"
-            color="blue-9"
-            keep-color
-            @click="handleNotifsSubscription"
-          />
+          <template v-if="isEnablePushNotifsLoading">
+            <ProgressLoader :color="isNotDefaultTheme(theme) ? theme : 'pink'" />
+          </template>
+          <template v-else>
+            <q-toggle
+              v-model="enablePushNotifs"
+              color="blue-9"
+              keep-color
+              @click="handleNotifsSubscription"
+            />
+          </template>
         </q-item-section>
       </q-item>
 
-      <template v-if="enablePushNotifs">
+      <template v-if="enablePushNotifs && !isEnablePushNotifsLoading">
         <q-item
           v-for="(item, index) in notifsList"
           :key="`${item}-${index}`"
@@ -46,17 +51,24 @@
 import Watchtower from 'watchtower-cash-js'
 
 import { BigNumber } from 'ethers'
-import { getDarkModeClass } from 'src/utils/theme-darkmode-utils'
+import { getDarkModeClass, isNotDefaultTheme } from 'src/utils/theme-darkmode-utils'
 import { loadWallet } from 'src/wallet'
 import { getWalletByNetwork } from 'src/wallet/chipnet'
 import { getPushNotifConfigs } from 'src/utils/engagementhub-utils'
 
+import ProgressLoader from 'src/components/ProgressLoader.vue'
+
 export default {
   name: 'PushNotifsSettings',
+
+  components: {
+    ProgressLoader
+  },
 
   data () {
     return {
       enablePushNotifs: false,
+      isEnablePushNotifsLoading: false,
 
       notifsList: [
         // {
@@ -99,20 +111,29 @@ export default {
   computed: {
     darkMode () {
       return this.$store.getters['darkmode/getStatus']
+    },
+    theme () {
+      return this.$store.getters['global/theme']
     }
   },
 
   async mounted () {
+    this.isEnablePushNotifsLoading = true
+
     const deviceId = BigNumber.from('0x' + this.$pushNotifications.deviceId).toString()
     const data = await getPushNotifConfigs(deviceId)
     console.log(data)
     this.enablePushNotifs = data.is_enabled
+
+    this.isEnablePushNotifsLoading = false
   },
 
   methods: {
     getDarkModeClass,
+    isNotDefaultTheme,
     async handleNotifsSubscription () {
       const vm = this
+      vm.isEnablePushNotifsLoading = true
       const multiWalletIndex = vm.$store.getters['global/getWalletIndex']
       const wallet = await loadWallet('BCH', multiWalletIndex)
       const walletHashes = [
@@ -134,7 +155,19 @@ export default {
       } else {
         await vm.$pushNotifications.unsubscribe(walletHashes)
       }
+
+      vm.isEnablePushNotifsLoading = false
     }
   }
 }
 </script>
+
+<style lang="scss">
+.lds-ellipsis {
+  height: 32px !important;
+
+  & div {
+    top: 10px !important;
+  }
+}
+</style>
