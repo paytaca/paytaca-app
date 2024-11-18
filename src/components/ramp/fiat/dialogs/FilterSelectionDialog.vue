0bc46ea3f3f0fc080710a0dccd5060abb5f8b427
SeanNikki123 <sslino@up.edu.ph>
2024-11-18 17:13:05 +0800

    added currency type toggle button + added customKeyboard on amount filter form | small fix

--- src/components/ramp/fiat/dialogs/FilterSelectionDialog.vue
+++ src/components/ramp/fiat/dialogs/FilterSelectionDialog.vue
@@ -1,12 +1,43 @@
 <!-- Individual Filtering in Store Page -->
 <template>
-  <q-dialog ref="dialog" full-width position="top" transition-show="slide-down">
+  <q-dialog ref="dialog" full-width persistent position="top" transition-show="slide-down" @before-hide="customKeyboard = 'dismiss'" no-shake>
     <q-card class="br-15 pt-card-2 text-bow">
-      <div class="q-px-lg q-pt-lg text-center text-bold" style="font-size: medium;">{{ filterTypeText }}</div>
+      <div class="text-right q-pr-lg q-pt-md">
+        <q-icon size="sm" color="red" name="close" @click="$refs.dialog.hide()"/>
+      </div>
+      <div class="q-px-lg q-pt-sm text-center text-bold" style="font-size: medium;">{{ filterTypeText }}</div>
       <div class="q-pt-sm q-pb-md q-px-lg">
         <div v-if="type === 'amount'">
-          <q-input dense rounded outlined v-model="amount" placeholder="Enter Amount">
+          <q-input
+            dense
+            rounded
+            outlined
+            v-model="amount"
+            placeholder="Enter Amount"
+            @focus="openCustomKeyboard(true)"
+            :readonly="readonlyState"
+            >
+            <template v-slot:append>
+              <span class="text-bold" style="font-size: 15px;">{{ byFiat ? currency.symbol : 'BCH' }}</span>
+            </template>
           </q-input>
+          <div class="q-pl-sm q-pt-sm">
+            <q-btn
+              class="sm-font-size button button-text-primary"
+              padding="none"
+              flat
+              no-caps
+              :class="getDarkModeClass(darkMode)"
+              @click="byFiat = !byFiat">
+              {{
+                $t(
+                  'SetAmountInCurrency',
+                  { currency: byFiat ? 'BCH' : currency?.symbol },
+                  `Set amount in ${ byFiat ? 'BCH' : currency?.symbol }`
+                )
+              }}
+            </q-btn>
+          </div>
         </div>
         <div v-if="type === 'paymentTypes'">
           <div class="q-gutter-sm q-pt-sm">
@@ -32,24 +63,38 @@
             </q-badge>
           </div>
         </div>
-        <div class="text-center">
+        <div class="text-center q-pt-xs">
           <q-btn rounded class="text-center q-mt-sm" color="blue" label="filter" @click="onOKClick"/>
         </div>
       </div>
     </q-card>
+    <customKeyboard
+      :custom-keyboard-state="customKeyboardState"
+      v-on:addKey="setAmount"
+      v-on:makeKeyAction="makeKeyAction"
+    />
   </q-dialog>
 </template>
 <script>
+import { getDarkModeClass } from 'src/utils/theme-darkmode-utils'
+
+import customKeyboard from 'src/pages/transaction/dialog/CustomKeyboard.vue';
 
 export default {
   data () {
     return {
       darkMode: this.$store.getters['darkmode/getStatus'],
       paymentTypes: [],
-      amount: null,
+      amount: 0,
       // selectedPaymentTypes: [],
-      filter: null
+      filter: null,
+      byFiat: true,
+      readonlyState: false,
+      customKeyboardState: 'dismiss'
     }
+  },
+  components: {
+    customKeyboard
   },
   computed: {
     filterTypeText () {
@@ -70,6 +115,7 @@ export default {
     // this.selectedPaymentTypes = this.filter.payment_types
   },
   methods: {
+    getDarkModeClass,
     selectAllPaymentTypes () {
       this.filter.payment_types = this.paymentTypes.map(e => e.id)
     },
@@ -84,6 +130,74 @@ export default {
     onOKClick () {
       this.$emit('ok', this.filter)
       this.$refs.dialog.hide()
+    },
+    setAmount (key) {
+      let receiveAmount, finalAmount, tempAmountFormatted = ''
+      let proceed = false
+      receiveAmount = this.amount
+
+      // see if # of decimal valid
+      let temp = receiveAmount.toString()
+      temp = temp.split('.')
+      if (temp.length === 2) {
+        if (this.byFiat) {
+          if (temp[1].length < 2) {
+            proceed = true
+          }
+        } else {
+          if (temp[1].length < 8) {
+            proceed = true
+          }
+        }
+      } else {
+        proceed = true
+      }
+
+      if (proceed) {
+        receiveAmount = receiveAmount === null ? '' : receiveAmount
+        if (key === '.' && receiveAmount === '') {
+          finalAmount = '0.'
+        } else {
+          finalAmount = receiveAmount.toString()
+          const hasPeriod = finalAmount.indexOf('.')
+          if (hasPeriod < 1) {
+            if (Number(finalAmount) === 0 && Number(key) > 0) {
+              finalAmount = key
+            } else {
+              // Check amount if still zero
+              if (Number(finalAmount) === 0 && Number(finalAmount) === Number(key)) {
+                finalAmount = 0
+              } else {
+                finalAmount += key.toString()
+              }
+            }
+          } else {
+            finalAmount += key !== '.' ? key.toString() : ''
+          }
+        }
+        this.amount = finalAmount
+      }
+    },
+    makeKeyAction (action) {
+      if (action === 'backspace') {
+        // Backspace
+        this.amount = String(this.amount).slice(0, -1)
+      } else if (action === 'delete') {
+        // Delete
+        this.amount = '0'
+      } else {
+        this.customKeyboardState = 'dismiss'
+        this.readonlyState = false
+      }
+    },
+    openCustomKeyboard (state) {
+      this.readonlyState = state
+
+      if (state) {
+        this.customKeyboardState = 'show'
+      } else {
+        this.customKeyboardState = 'dismiss'
+      }
     }
   }
 }
