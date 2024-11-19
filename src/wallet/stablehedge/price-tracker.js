0038aac0fb1559095ded0485752f5caedefbe8c0
khirvy019 <khirvy_19@yahoo.com>
2024-11-19 13:32:17 +0800

    update stablhedge price tracker to subscribe token prices not in token balances

--- src/wallet/stablehedge/price-tracker.js
+++ src/wallet/stablehedge/price-tracker.js
@@ -10,8 +10,11 @@ class StablehedgeTokenPriceTracker {
    */
   constructor(store) {
     this.$store = store
-    this.subscribedKeys = {};
-    
+
+    // using Map over Object: https://www.zhenghao.io/posts/object-vs-map
+    /** @type {Map<String, String[] | Number>} */
+    this.subscribedKeys = new Map();
+
     this.intervalId = null;
     this.minAge = 45 * 1000;
     this.interval = 30 * 1000;
@@ -19,12 +22,27 @@ class StablehedgeTokenPriceTracker {
     this.updatingPrices = false
   }
 
+  get subscribedCategories() {
+    const categories = new Set([].map(String))
+    this.subscribedKeys.forEach(value => {
+      if (!Array.isArray(value)) return
+      value.forEach(val => categories.add(val))
+    })
+
+    return Array.from(categories)
+  }
+
   async updatePrices() {
     try {
       if (this.updatingPrices) return Promise.resolve()
       this.updatingPrices = true
       this._updatePricePromise = this.$store
-        .dispatch('stablehedge/updateTokenPrices', { minAge: this.minAge })
+        .dispatch(
+          'stablehedge/updateTokenPrices',
+          {
+            minAge: this.minAge,
+            includeCategories: this.subscribedCategories,
+          })
       await this._updatePricePromise
     } finally {
       this.updatingPrices = false
@@ -37,7 +55,7 @@ class StablehedgeTokenPriceTracker {
     if (!this.intervalFunc) {
       this.intervalFunc = () => {
         const minTokenBalanceTimestamp = this.$store.getters['stablehedge/minTokenBalanceTimestamp']
-        if (minTokenBalanceTimestamp == null) return
+        if (minTokenBalanceTimestamp == null && !this.subscribedCategories?.length) return
         const tokenAge = Date.now() - minTokenBalanceTimestamp;
 
         if (tokenAge > this.minAge) {
@@ -55,24 +73,30 @@ class StablehedgeTokenPriceTracker {
     this.intervalId = null
   }
 
-  subscribe(key='') {
+  /**
+   * @param {String} key 
+   * @param {String[]} [categories]
+   * @returns 
+   */
+  subscribe(key='', categories) {
     if (!key) {
       key = String(Date.now())
     }
 
-    if (!this.subscribedKeys?.[key]) this.subscribedKeys[key] = 1;
+    let keyValue = 1
+    if (Array.isArray(categories)) keyValue = categories;
+    this.subscribedKeys.set(key, keyValue)
     if (this.shouldStartInterval()) this.startInterval()
     return () => this.unsubscribe(key)
   }
 
   unsubscribe(key='') {
-    if (typeof key !== 'string') return
-    delete this.subscribedKeys[key]
+    this.subscribedKeys.delete(key)
     if (this.shouldStopInterval()) this.stopInterval()
   }
 
   shouldStartInterval() {
-    return Object.getOwnPropertyNames(this.subscribedKeys).length > 0
+    return this.subscribedKeys.size > 0
   }
 
   shouldStopInterval() {
