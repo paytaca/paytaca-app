5083b77d8ab2c8e54e341c193f7a58b5c7e3377e
ubuntu <rongrammer@hotmail.com>
2024-12-07 00:00:23 +0800

    add tooltip to app icons, show app urls

--- src/components/SelectChangeAddress.vue
+++ src/components/SelectChangeAddress.vue
@@ -3,14 +3,14 @@
     <q-card class="q-dialog-plugin br-15 q-pb-xs pt-card" :class="getDarkModeClass(darkMode)">
       <div class="text-grad text-center q-my-md text-h6">{{$t('SelectChangeAddress')}}</div>
       <div class="row q-my-md justify-center items-center q-gutter-md q-px-md">
-        <q-checkbox v-model="useSystemGeneratedChangeAddress" :label="$t('UseSystemGeneratedChangeAddress')" /> 
-        <q-icon name="help" @click="showHelpDialog" size="lg"></q-icon>
+        <q-checkbox v-model="useSystemGeneratedChangeAddress" :label="$t('UseSystemGeneratedChangeAddress')" dense/> 
+        <q-icon name="help" @click="showHelpDialog" size="sm"></q-icon>
       </div>
       <q-card-section>
         <q-list bordered separator>
         <q-item v-if="useSystemGeneratedChangeAddress" active focused>
           <q-item-section>
-            <div class="text-caption" >{{ shortenAddressForDisplay(defaultChangeAddress) }}</div>
+            <div class="text-caption" >{{ formatAddressForDisplay(defaultChangeAddress) }}</div>
           </q-item-section>
           <q-item-section side>
               <div class="row flex q-gutter-x-sm">
@@ -32,7 +32,7 @@
             :focused="addressOption.selected"
           >
             <q-item-section>
-              <div class="text-caption" >{{ `${addressOption.label}` }}</div>
+              <div class="text-caption" >{{ formatAddressForDisplay(addressOption.label) }}</div>
               <div class="row">
                 <div class="q-pa-md q-gutter-xs">
                   <q-avatar
@@ -40,7 +40,8 @@
                     :key="i"
                     size="sm"
                     :style="!connectedApp.app_icon ? 'border: 1px solid grey': ''"
-                  >
+                  > 
+                    <q-tooltip>{{ connectedApp?.app_url }}</q-tooltip>
                     <img v-if="connectedApp.app_icon" :src="connectedApp.app_icon">
                     <q-icon v-else name="image_not_supported" />
                   </q-avatar>
@@ -87,16 +88,21 @@
   </q-dialog>
 </template>
 <script setup>
-import { ref, onMounted, watch } from 'vue'
+import { ref, onMounted, watch, computed } from 'vue'
 import { useDialogPluginComponent, useQuasar } from 'quasar'
 import { useI18n } from 'vue-i18n'
+import { useStore } from 'vuex';
 import { getDarkModeClass } from 'src/utils/theme-darkmode-utils'
 import { shortenAddressForDisplay } from 'src/utils/address-utils'
+import { convertCashAddress } from 'src/wallet/chipnet';
+
 const { dialogRef, onDialogHide, onDialogOK, onDialogCancel } 
   = useDialogPluginComponent()
+
 const props = defineProps({
   walletAddresses: Array,
   connectedApps: Array,
+  defaultSelectedAddress: String,
   defaultChangeAddress: {
     type: String,
     required: true
@@ -104,11 +110,19 @@ const props = defineProps({
   darkMode: Boolean
 })
 
+
 const $q = useQuasar()
+const $store = useStore()
 const { t: $t } = useI18n()
 const addressSelected = ref /*<string>*/ ('')
 const addressOptions  = ref /*<{label: string, value: string }[]>*/ ([])
 const useSystemGeneratedChangeAddress = ref(false)
+
+const settings = computed(() => $store.getters['walletconnect/settings'])
+
+const formatAddressForDisplay = (address) => {
+  return shortenAddressForDisplay(convertCashAddress(address, $store.getters['global/isChipnet'], true))
+}
 
 const onConfirmSelect = () => {
   if (useSystemGeneratedChangeAddress.value) {
@@ -124,8 +138,6 @@ const onCancelClick = () => {
 }
 
 const selectAddress = (address /*:string*/) => {
-  console.log('🚀 ~ selectAddress ~ address:', address)
-  
   addressOptions.value.forEach(addressOption => {
     addressOption.selected = (addressOption.address === address);
     if (addressOption.selected) {
@@ -153,13 +165,17 @@ watch(() => useSystemGeneratedChangeAddress.value, (yesUse) => {
 
 onMounted(() => {
   // walletAddresses has wif we don't want to pass it as options to the dialog
-  addressOptions.value = props.walletAddresses?.map((item) => ({ label: shortenAddressForDisplay(item.address), address: item.address }))
+  addressOptions.value = props.walletAddresses?.map((item) => ({ label: item.address, address: item.address }))
   addressOptions.value?.forEach((addressOption) => {
     const connectedAppsForAddressOption = props.connectedApps?.filter((connectedApp) => {
       return connectedApp.wallet_address === addressOption.address
     })
     addressOption.connectedApps = connectedAppsForAddressOption
+    if (props.defaultSelectedAddress && addressOption.address === props.defaultSelectedAddress) {
+      addressOption.selected = true
+    }
   })
+  
 })
 </script>
 
