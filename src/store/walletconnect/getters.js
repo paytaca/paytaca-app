79052d117a2c2dbe726bf6cec0b19ede2bb05396
ubuntu <rongrammer@hotmail.com>
2024-12-06 16:47:51 +0800

    add walletconnect settings

--- src/store/walletconnect/getters.js
+++ src/store/walletconnect/getters.js
@@ -6,3 +6,7 @@ export function callRequests (state) {
   if (!Array.isArray(state.callRequests)) return []
   return state.callRequests
 }
+
+export function settings (state) {
+  return state.settings
+}
