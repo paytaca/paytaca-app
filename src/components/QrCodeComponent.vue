53f39388c4a99bea2f739c885b5ea6542455bbf5
joemarct <joemar.ct@gmail.com>
2024-12-09 11:07:58 +0800

    Make QR text reactive so it re-renders when text is updated

--- src/components/QrCodeComponent.vue
+++ src/components/QrCodeComponent.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="col row justify-center">
-    <img v-if="icon" class="icon" :src="icon" :width="iconSize" :style="{'margin-top': ((size / 2) - (iconSize / 2)) + 'px'}" />
+    <img
+      v-if="icon"
+      class="icon"
+      :src="icon"
+      :width="iconSize"
+      :style="{'margin-top': ((size / 2) - (iconSize / 2)) + 'px'}"
+    />
     <div :id="name"></div>
   </div>
 </template>
@@ -11,53 +17,76 @@ qr.defineCustomElements(window)
 
 export default {
   props: {
-      name: {
-        type: String,
-        default: 'bch-qr'
-      },
-      text: {
-          type: String,
-          required: true
-      },
-      size: {
-        type: Number,
-        required: true
-      },
-      icon: {
-        type: String,
-        default: null
-      },
-      iconSize: {
-        type: Number,
-        default: 60
-      }
+    name: {
+      type: String,
+      default: 'bch-qr'
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: Number,
+      required: true
+    },
+    icon: {
+      type: String,
+      default: null
+    },
+    iconSize: {
+      type: Number,
+      default: 60
+    }
   },
-  mounted () {
-      const vm = this
-      document.getElementById(vm.name).addEventListener('codeRendered', () => {
-          document.getElementById(this.name + '-qr').animateQRCode('FadeInCenterOut');
-      });
-      setTimeout(() => {
-          document.querySelector('#' + vm.name).innerHTML = `
-          <div>
+  mounted() {
+    this.renderQRCode();
+  },
+  watch: {
+    text: 'renderQRCode' // Watch for changes to `text` and re-render the QR code
+  },
+  methods: {
+    renderQRCode() {
+      const vm = this;
+      const container = document.getElementById(vm.name);
+
+      if (container) {
+        container.innerHTML = ''; // Clear the container before rendering new QR code
+        container.addEventListener('codeRendered', () => {
+          document
+            .getElementById(this.name + '-qr')
+            .animateQRCode('FadeInCenterOut');
+        });
+
+        setTimeout(() => {
+          container.innerHTML = `
+            <div>
               <qr-code
-                  id="${vm.name + '-qr'}"
-                  contents="${vm.text}"
-                  style="
-                    width: ${vm.size}px;
-                    height: ${vm.size}px;
-                    background-color: #fff;
-                    border-radius: 10px;
-                    border: 4px solid #ed5f59;
-                  "
+                id="${vm.name + '-qr'}"
+                contents="${vm.text}"
+                style="
+                  width: ${vm.size}px;
+                  height: ${vm.size}px;
+                  background-color: #fff;
+                  border-radius: 10px;
+                  border: 4px solid #ed5f59;
+                "
               >
               </qr-code>
-          </div>
-      `;
-      }, 100);
+            </div>
+          `;
+        }, 100);
+      }
+    },
+    animate() {
+      const container = document.getElementById(this.name);
+      if (container) {
+        document
+          .getElementById(this.name + '-qr')
+          .animateQRCode('RadialRipple');
+      }
+    }
   }
 }
-
 </script>
 
 <style>
