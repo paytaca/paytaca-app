6dc36732cb802433af2435f3469de896816bbadc
SeanNikki123 <sslino@up.edu.ph>
2025-01-20 16:54:32 +0800

    added fetching list of merchant transactions and list of cashout orders | added creating cashout order function | WIP display data

--- src/pages/apps/paytacapos-admin/merchant-cashout/index.vue
+++ src/pages/apps/paytacapos-admin/merchant-cashout/index.vue
@@ -103,6 +103,7 @@
 <script>
 import { getDarkModeClass } from 'src/utils/theme-darkmode-utils'
 import { formatCurrency } from 'src/exchange'
+import { backend } from 'src/exchange/backend'
 import { ref } from 'vue'
 import HeaderNav from 'src/components/header-nav.vue';
 import CashoutOrderForm from 'src/components/paytacapos/CashoutOrderForm.vue';
@@ -179,7 +180,9 @@ export default {
           selected: false
         }
       ],
-      selectedTransactions: []
+      selectedTransactions: [],
+      merchantTransactions: null,
+      // cashoutOrders: null
     }
   },
   computed: {
@@ -216,6 +219,48 @@ export default {
       if (parent !== void 0 && parent.scrollTop > 0) {
         e.stopPropagation()
       }
+    },
+    async fetchMerchantTransactions () {
+      const vm = this
+      const url = '/paytacapos/cash-out/list_unspent_txns'
+
+      await backend.get(url, { authorize: true})
+        .then(response => {
+          console.log(response)
+          vm.merchantTransactions = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    async fetchCashoutOrders () {
+      const vm = this
+      const url = '/paytacapos/cashout/'
+
+      await backend.get(url, { authorize: true })
+        .then(response => {
+          console.log(response)
+          vm.cashoutOrders = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    async createCashoutOrder () {
+      const vm = this
+      const url = '/paytacapos/cash-out/'
+      const params = {
+        currency: vm.currency,
+        txids: vm.selectTransaction
+      }
+
+      await backend.post(url, { params: params, authorize: true })
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => {
+          console.log(error)
+        })
     }
   }
 }
