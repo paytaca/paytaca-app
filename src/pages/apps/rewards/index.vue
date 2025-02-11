dc625a73aa183ac0f2045b96fa889ece0e16d4e1
LukeHeimen <kdlanante@up.edu.ph>
2025-02-11 15:10:40 +0800

    Update UI of Rewards landing page

--- src/pages/apps/rewards/index.vue
+++ src/pages/apps/rewards/index.vue
@@ -2,14 +2,46 @@
   <div id="app-container" :class="getDarkModeClass(darkMode)">
     <header-nav :title="`Rewards`" backnavpath="/apps" class="apps-header" />
 
-    <div class="row" :style="{ 'margin-top': $q.platform.is.ios ? '0px' : '-30px'}">
-      yey
+    <div
+      class="row q-mx-lg q-gutter-y-md"
+      style="padding-top: 50px; font-size: 18px;"
+      :style="{ 'margin-top': $q.platform.is.ios ? '0px' : '-30px'}"
+    >
+      <div
+        class="row col-12 justify-between items-center q-pa-md group-currency"
+        :class="getDarkModeClass(darkMode)"
+        v-for="(promo, index) in promos"
+        :key="index"
+      >
+        <div class="col-8">
+          <span
+            class="text-token"
+            :class="darkMode ? isNotDefaultTheme(theme) ? 'text-grad' : 'dark' : 'light'"
+          >
+            {{ promo.name }}
+          </span><br/>
+          <span
+            class="amount-text"
+            :class="getDarkModeClass(darkMode, '', 'text-grad')"
+          >
+            {{ promo.points }} {{ pointsType[index] }}
+          </span>
+        </div>
+
+        <div class="row col-3 justify-end">
+          <q-btn
+            rounded
+            class="btn-scan button text-white bg-grad"
+            icon="chevron_right"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { getDarkModeClass } from 'src/utils/theme-darkmode-utils'
+import { getDarkModeClass, isNotDefaultTheme } from 'src/utils/theme-darkmode-utils'
 
 import HeaderNav from 'src/components/header-nav'
 
@@ -20,14 +52,31 @@ export default {
     HeaderNav
   },
 
+  data () {
+    return {
+      pointsType: ['UP', 'RFP'/*, 'LP', 'CP', 'MP' */],
+      promos: [
+        { name: 'User Rewards', points: 0 },
+        { name: 'Refer-a-Friend Promo', points: 0 } //,
+        // { name: 'Loyalty Promo', points: 0 },
+        // { name: 'Champion Promo', points: 0 },
+        // { name: 'Paytaca Partner Rewards Promo', points: 0 }
+      ]
+    }
+  },
+
   computed: {
     darkMode () {
       return this.$store.getters['darkmode/getStatus']
+    },
+    theme () {
+      return this.$store.getters['global/theme']
     }
   },
 
   methods: {
-    getDarkModeClass
+    getDarkModeClass,
+    isNotDefaultTheme
   }
 }
 </script>
