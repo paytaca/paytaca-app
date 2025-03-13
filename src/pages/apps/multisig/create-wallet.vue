5fd07e56a5405ab35d2722937ec12562657370da
ubuntu <rongrammer@hotmail.com>
2025-03-13 11:51:29 +0800

    create and save template draft in store, implement commit draft --Wip

--- src/pages/apps/multisig/create-wallet.vue
+++ src/pages/apps/multisig/create-wallet.vue
@@ -26,6 +26,9 @@
                                 outlined
                             />
                         </div>
+                        <div class="col-6 q-pl-sm">
+                            <q-input v-model="template.name" label="Wallet Label"></q-input>
+                        </div>
                         <div class="col-12 text-center">
                             <span class="text-italic text-bow">
                               {{ m }} of {{ n }} multisig wallet
@@ -36,35 +39,36 @@
                       <div class="col-12 q-px-m">
                         <q-form
                           @submit="onSubmit"
-                          @reset="onReset"
+                          @reset="onResetClicked"
                           class="q-gutter-md"
                           >
                           <template v-for="i, index in Array(m)" :key="`cosigner-${index}`">
-                            <q-card >
+                            <q-card class="br-15 pt-card-2 text-bow" :class="getDarkModeClass(darkMode)">
                               <q-card-section>Signer {{ index + 1 }}</q-card-section>
                               <q-card-section class="q-pa-md q-gutter-y-sm">
                                 <q-input
-                              v-model="cosigners[index + 1].xPubKey"
-                              :label="`Paste Cosigner ${index + 1}'s xPubKey`"
-                              style="color:black"
-                              outlined
-                              :disable="index === 0"
-                            >
-                            </q-input>
-                            <q-input
-                              v-if="cosigners[index + 1].xPubKey"
-                              v-model="cosigners[index + 1].derivationPath"
-                              :key="`derivation-path-${index}`"
-                              :label="`Derivation Path`"
-                              style="color:black"
-                              outlined
-                            ></q-input>
+                                  v-model="cosigners[index + 1].xPubKey"
+                                  :label="`Paste Cosigner ${index + 1}'s xPubKey`"
+                                  style="color:black"
+                                  outlined
+                                  :disable="index === 0"
+                                >
+                                </q-input>
+                                <q-input
+                                  v-if="cosigners[index + 1].xPubKey"
+                                  v-model="cosigners[index + 1].derivationPath"
+                                  :key="`derivation-path-${index}`"
+                                  :label="`Derivation Path`"
+                                  style="color:black"
+                                  outlined
+                                ></q-input>
                               </q-card-section>
                             </q-card>
                           </template>
                           <div>
-                              <q-btn label="Create" type="submit" color="primary"/>
-                              <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+                              <q-btn v-if="cosigners?.length === m" :to="{ path: 'draft' }" label="Preview" type="button" color="primary" flat class="q-ml-sm" />
+                              <q-btn @click.stop="onResetClicked" label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+                              <q-btn @click.stop="onCreateClicked" label="Create" type="button" color="primary"/>
                           </div>
                         </q-form>
                       </div>
@@ -84,9 +88,10 @@
 import { useStore } from 'vuex'
 import { useI18n } from 'vue-i18n'
 import { computed, ref, onMounted, watch } from 'vue'
+import { CashAddressNetworkPrefix } from 'bitauth-libauth-v3'
 import HeaderNav from 'components/header-nav'
 import { getDarkModeClass } from 'src/utils/theme-darkmode-utils'
-import { derivePubKeyFromXPubKey } from 'src/lib/multisig-wallet'
+import { createTemplate, createWallet } from 'src/lib/multisig'
 
 const $store = useStore()
 const { t: $t } = useI18n()
@@ -95,6 +100,7 @@ const n = ref()
 const mOptions = ref()
 const nOptions = ref()
 const cosigners = ref()
+const template = ref()
 
 const mOptionsComputed = computed(() => {
   return mOptions.value
@@ -108,6 +114,12 @@ const darkMode = computed(() => {
   return $store.getters['darkmode/getStatus']
 })
 
+const cashAddressNetworkPrefix = computed(() => {
+  return $store.getters['global/isChipnet']
+    ? CashAddressNetworkPrefix.testnet
+    : CashAddressNetworkPrefix.mainnet
+})
+
 const initCosigners = () => {
   cosigners.value = {}
   for (let i = 0; i < 20; i++) {
@@ -116,10 +128,24 @@ const initCosigners = () => {
   }
 }
 
-const onReset = () => {
+const onResetClicked = () => {
   initCosigners()
   m.value = 2
   n.value = 3
+}
+
+const onCreateClicked = () => {
+  if (!m.value || cosigners.value?.length !== m.value) {
+    return
+  }
+  const { address, lockingBytecode } = createWallet({
+    m: m.value,
+    n: n.value,
+    xPubKeys: cosigners.value.map(item => item.xPubKey),
+    cashAddressNetworkPrefix: cashAddressNetworkPrefix.value,
+    template: template.value
+  })
+  $store.getters['global/commitTemplateDraft']({ address, lockingBytecode, template: template.value })
 }
 
 watch(() => m.value, (valueOfM) => {
@@ -139,7 +165,7 @@ onMounted(() => {
     xPubKey,
     derivationPath: derivationPath + '/0/0'
   }
-  console.log('DERIVE', derivePubKeyFromXPubKey(xPubKey))
+  template.value = createTemplate({ name: '', m: m.value, n: n.value })
 })
 </script>
 
