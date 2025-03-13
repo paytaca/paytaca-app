5cd11d0002ca329fda5418d249b7926d4a2477ed
ubuntu <rongrammer@hotmail.com>
2025-03-13 11:53:31 +0800

    correct arg name

--- src/store/multisig/mutations.js
+++ src/store/multisig/mutations.js
@@ -6,6 +6,6 @@ export function deleteTemplateDraft (state) {
   state.templateDraft = null
 }
 
-export function commitTemplateDraft (state, { address, lockingBytecode, templateDraft }) {
-  state.templates.push({ address, lockingBytecode, templateDraft })
+export function commitTemplateDraft (state, { address, lockingBytecode, template }) {
+  state.templates.push({ address, lockingBytecode, template })
 }
