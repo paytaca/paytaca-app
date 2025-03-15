105f3cc5c83faf58dd0b221614ee8b117dd9fdd8
ubuntu <rongrammer@hotmail.com>
2025-03-15 13:29:14 +0800

    wallet store getter

--- src/store/multisig/getters.js
+++ src/store/multisig/getters.js
@@ -1,10 +1,10 @@
-export function getWalletDraft (state) {
-  return state.walletDraft
-}
-
 export function getWallet (state, { address }) {
   const wallet = state.wallets.filter((wallet) => {
     return wallet.address === address
   })
   return wallet
+}
+
+export function getWallets (state) {
+  return state.wallets
 }
