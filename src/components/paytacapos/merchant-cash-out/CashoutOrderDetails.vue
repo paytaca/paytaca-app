75fa72ca42649a790f1d496ae64e32d825f9a08c
SeanNikki123 <sslino@up.edu.ph>
2025-04-02 09:53:00 +0800

    added view in explorer txid & payout address | updated cashout details dialog | small fixes

--- src/components/paytacapos/merchant-cash-out/CashoutOrderDetails.vue
+++ src/components/paytacapos/merchant-cash-out/CashoutOrderDetails.vue
@@ -6,37 +6,49 @@
     Cashout Order #{{ order.id }}
   </div>
 
-  <div class="q-mx-lg q-my-md" style="font-size: 13px" v-if="order">
-    <div class="row">
-      <div class="text-bold col">Payout Amount</div>
-      <div class="text-right col">{{ order.payout_amount }} BCH</div>
-    </div>
-    <div class="row">
-      <div class="text-bold col-4">Payout Address</div>
-      <div class="text-right col-8">{{ arrangeAddressText(order.payout_address) }} <q-icon name="content_copy" color="grey" size="sm" @click="copyToClipboard(order.payout_address)"/></div>
+  <div class="q-mx-lg q-my-md q-pb-md" style="font-size: 13px;" v-if="order">
+    <div class="">
+      <div class="text-grey text-bold">Payout Amount</div>
+      <div style="font-size: 15px">{{ order.payout_amount }} BCH</div>
     </div>
 
-    <div class="row q-pt-md">
-      <div class="text-bold col-4">Created At</div>
-      <div class="text-right col-8">{{ arrangeDate(order.created_at) }}</div>
-    </div>
-    <div class="row">
-      <div class="text-bold col">Payment Method</div>
-      <div class="text-right col">{{ arrangePaymentMethod(order.payment_method) }}</div>
+    <div class="q-pt-sm">
+      <div class="text-grey text-bold">Payout Address</div>
+      <div style="overflow-wrap: anywhere; font-size: 15px;">
+        <span @click="copyToClipboard(order.payout_address)">{{ order.payout_address }}</span>
+        <q-icon name="content_copy" color="grey" size="sm" @click="copyToClipboard(order.payout_address)"/>
+        <q-icon name="open_in_new" color="grey" size="sm" @click="openURL(explorerLink('address'))"/>
+      </div>
+      <!-- <div class="text-right" style="overflow-wrap: anywhere;">{{ arrangeAddressText(order.payout_address) }} <q-icon name="content_copy" color="grey" size="sm" @click="copyToClipboard(order.payout_address)"/></div> -->
     </div>
 
-
-    <div class="row q-pt-md">
-      <div class="text-bold col-4">Transaction ID</div>
-      <div class="text-right col-8 text-primary text-underline" style="overflow: auto;">{{ order.transactions.outputs[0].txid}} <q-icon name="content_copy" color="grey" size="sm" @click="copyToClipboard(order.payout_address)"/></div>
+    <div class="q-pt-sm">
+      <div class="text-grey text-bold">Created At</div>
+      <div style="font-size: 15px;">{{ arrangeDate(order.created_at) }}</div>
     </div>
-    <div class="row">
-      <div class="text-bold col">Status</div>
-      <div class="text-right col" style="font-size: 15px;">{{ order.status }}</div>
+
+    <div class="q-pt-sm">
+      <div class="text-grey text-bold">Payment Method</div>
+      <div style="font-size: 15px;"><span class="text-bold">{{ order?.payment_method.payment_type.full_name }}:</span> {{ arrangePaymentMethod(order.payment_method) }}</div>
+    </div>
+
+    <div class="q-pt-sm">
+      <div class="text-grey text-bold">Transaction ID</div>
+      <div class="text-primary text-underline" style="overflow-wrap: anywhere; font-size: 15px;">
+        <span @click="copyToClipboard(order.payout_address)">{{ order.transactions.outputs[0].txid}}</span>
+        <q-icon name="content_copy" color="grey" size="sm" @click="copyToClipboard(order.payout_address)"/>
+        <q-icon name="open_in_new" color="grey" size="sm" @click="openURL(explorerLink())"/>
+      </div>
+    </div>
+
+    <div class="q-pt-sm">
+      <div class="text-grey text-bold">Status</div>
+      <div class="text-bold" style="font-size: 15px;">{{ order.status }}</div>
     </div>
   </div>
 </template>
 <script>
+import { openURL } from 'quasar'
 
 export default {
   data() {
@@ -47,24 +59,52 @@ export default {
   props: {
     order: Object
   },
-  emits: ['close'],
-  mounted () {
-    // console.log('order: ', this.order)
+  computed: {
+    isChipnet () {
+      return this.$store.getters['global/isChipnet']
+    },
   },
+  emits: ['close'],
   methods: {
+    openURL,
+    explorerLink (linkType='txid') { // linktype: ['txid', 'address']
+      let url = ''
+
+      if (this.isChipnet) {
+        url = 'https://chipnet.imaginary.cash'
+      } else {
+        url = 'https://blockchair.com/bitcoin-cash'
+      }
+
+      if (linkType === 'txid') {
+        const txid = this.order?.transactions.outputs[0].txid
+        url = this.isChipnet ? `${url}/tx/` : `${url}/transaction/`
+        return `${url}${txid}`
+      } else {
+        const address = this.order?.payout_address.replace('bitcoincash:', '')
+        url = `${url}/address/`
+        return `${url}${address}`
+      }
+
+      // if (this.transaction.asset.id.split('/')[0] === 'ct') {
+      //   url = 'https://explorer.bitcoinunlimited.info/tx/'
+      // }
+    },
     arrangeAddressText (address) {
       return address.slice(0, 15) + '.....' + address.slice(50)
     },
     arrangeDate (date) {
       const createDate = new Date(date)
 
-      return createDate.toLocaleDateString()
+      // return createDate.toLocaleDateString()
+      return new Intl.DateTimeFormat('en-US', { dateStyle: 'medium', timeStyle: 'medium' }).format(createDate)
     },
     arrangePaymentMethod (method) {
-      let text = `${method.payment_type.full_name}: `
+      // let text = `${method.payment_type.full_name}: `
+      let text = ''
 
       method.values.forEach(val => {
-        text =  `${text}(${val.value}) `
+        text =  `${text}[ ${val.value} ] `
       })
       return text
     },
@@ -80,3 +120,14 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+.sm-font-size {
+  font-size: small;
+}
+.md-font-size {
+  font-size: medium;
+}
+.lg-font-size {
+  font-size: large;
+}
+</style>
