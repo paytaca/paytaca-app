8d3fee3f260385001da358897091e24f47ae770e
ubuntu <rongrammer@hotmail.com>
2025-05-02 14:38:10 +0800

    add helper methods

--- src/composables/multisig/helpers.js
+++ src/composables/multisig/helpers.js
@@ -1,23 +1,48 @@
 import { useStore } from 'vuex'
 import { computed } from 'vue'
 import { loadWallet } from 'src/wallet'
-import { MultisigWallet } from 'src/lib/multisig'
+import { MultisigWallet, MultisigTransaction } from 'src/lib/multisig'
 import { useRoute, useRouter } from 'vue-router'
+import Watchtower from 'src/lib/watchtower'
+import { CashAddressNetworkPrefix } from 'bitauth-libauth-v3'
 
 export const useMultisigHelpers = () => {
   const $store = useStore()
   const route = useRoute()
   const router = useRouter()
-  const signerWallets = computed(() => {
+
+  const localWallets = computed(() => {
     return $store.getters['global/getVault']
   })
 
+  const isChipnet = computed(() => {
+    return $store.getters['global/isChipnet']
+  })
+
+  const cashAddressNetworkPrefix = computed(() => {
+    if (isChipnet.value) {
+      return CashAddressNetworkPrefix.testnet
+    }
+    return CashAddressNetworkPrefix.mainnet
+  })
+
+  const multisigWallets = computed(() => {
+    const wallets = $store.getters['multisig/getWallets']?.map((walletObject) => {
+      // const wallet = new MultisigWallet(walletObject)
+      const wallet = MultisigWallet.createInstanceFromObject(walletObject)
+      return wallet.resolveDefaultAddress({ addressIndex: 0, cashAddressNetworkPrefix: cashAddressNetworkPrefix.value})
+    })
+    console.log('MULTISIG WALLETS in helpers', wallets)
+    return wallets
+  })
+  
+
   const getSignerWalletFromVault = ({ xpub }) => {
-    const vaultIndex = signerWallets.value.findIndex((signerWallet) => {
+    const vaultIndex = localWallets.value.findIndex((signerWallet) => {
       return signerWallet.wallet.bch.xPubKey === xpub
     })
     if (vaultIndex === -1) return
-    const wallet = signerWallets.value[vaultIndex]
+    const wallet = localWallets.value[vaultIndex]
     return { wallet, vaultIndex }
   }
 
@@ -32,6 +57,14 @@ export const useMultisigHelpers = () => {
     return hdKeys.hdPrivateKey
   }
 
+  const getSignerMnemonic = async ({ xpub }) => {
+    const signerWallet = getSignerWalletFromVault({ xpub })
+    console.log('🚀 ~ getSignerXPrv ~ signerWallet:', xpub, signerWallet)
+    if (!signerWallet) return
+    const { mnemonic } = await loadWallet('BCH', signerWallet.vaultIndex)
+    return mnemonic
+  }
+
   /**
    * Among the signers, check which belongs to this Paytaca Wallet.
    * Use the first one found.
@@ -39,7 +72,7 @@ export const useMultisigHelpers = () => {
    */
   const identifyPossiblePstCreator = async ({ signers }) => {
     for (const signerEntityIndex of Object.keys(signers)) {
-      const walletFound = signerWallets.value.find((signerWallet) => {
+      const walletFound = localWallets.value.find((signerWallet) => {
         return signerWallet.wallet.bch.xPubKey === signers[signerEntityIndex].xpub
       })
       if (walletFound) {
@@ -67,14 +100,60 @@ export const useMultisigHelpers = () => {
       multisigTransaction
     )
   }
+  
+  const updateTransaction = async ({index, multisigTransaction}) => {
+    await $store.dispatch('multisig/updateTransaction', { index, multisigTransaction })
+  }
+
+  const saveMultisigWallet = async (multisigWallet) => {
+    await $store.dispatch(
+      'multisig/saveWallet',
+      multisigWallet
+    )
+    const watchtower = new Watchtower($store.getters['global/isChipnet'])
+    watchtower.subscribe({ address: multisigWallet.address })
+  }
+
+  const getMultisigWalletBchBalance = async (address) => {
+    const watchtower = new Watchtower($store.getters['global/isChipnet'])
+    const bch = await watchtower.getAddressBchBalance(address)
+    console.log('🚀 ~ onMounted ~ balance:', bch)
+    return bch.balance
+  } 
+  
+  const getTransactionsByWalletAddress = ({ address }) => {
+    const transactions =
+         $store.getters['multisig/getTransactionsByWalletAddress']({
+          address: address
+         })
+ 
+      return transactions.map(t => {
+        const multisigWallet = multisigWallets.value?.find((wallet) => {
+          return address === wallet.address
+        })
+        console.log('MULTISIG WALLET', multisigWallet)
+        //t.multisigWallet = multisigWallet
+ 
+        // return MultisigTransaction.createInstanceFromObject(structuredClone(t))
+        return new MultisigTransaction({ ...structuredClone(t), multisigWallet: multisigWallet})
+      })
+
+  }
 
   return {
-    signerWallets,
+    localWallets,
     getSignerWalletFromVault,
     getSignerXPrv,
+    getSignerMnemonic,
     identifyPossiblePstCreator,
     deleteTransaction,
     saveTransaction,
-    transactionsLastIndex
+    updateTransaction,
+    saveMultisigWallet,
+    transactionsLastIndex,
+    cashAddressNetworkPrefix,
+    multisigWallets,
+    getTransactionsByWalletAddress,
+    getMultisigWalletBchBalance
   }
 }
