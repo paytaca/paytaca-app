bfbde7a986a36fab2c1e331738df84a278ed91b2
sherelot <chillprocessor@gmail.com>
2025-05-07 17:20:36 +0800

    Centralize error handling in reusable command, move localStorage restore and save logic to reusable commands

--- cypress/command.js
+++ cypress/command.js
@@ -62,6 +62,54 @@ Cypress.Commands.add('measureSettingsLoadTime', (labelText, waitForSelector, wai
     })
 })
 
+// Custom command to restore localStorage from storage.json
+Cypress.Commands.add('restoreLocalStorage', () => {
+cy.readFile('cypress/fixtures/storage.json').then((data) => {
+  if (data) {
+    cy.window().then((win) => {
+      Object.keys(data).forEach((key) => {
+        win.localStorage.setItem(key, data[key]);
+      });
+      cy.log('Restored data: ', data);
+    });
+  } else {
+    cy.log('No data found in storage.json');
+  }
+});
+});
+
+// Custom command to save localStorage to storage.json
+Cypress.Commands.add('saveLocalStorage', () => {
+cy.window().then((win) => {
+  cy.readFile('cypress/fixtures/storage.json').then((existingData) => {
+    const allLocalStorageData = {};
+    // Get new data
+    Object.keys(win.localStorage).forEach((key) => {
+      allLocalStorageData[key] = win.localStorage.getItem(key);
+    });
+    // Merge and write data
+    const mergedData = { ...existingData, ...allLocalStorageData };
+    cy.writeFile('cypress/fixtures/storage.json', mergedData);
+    cy.log('Appended');
+    cy.log(mergedData);
+  });
+});
+});
+
+// Custom command for error handling
+Cypress.Commands.add('setPushErrorHandling', () => {
+Cypress.on('uncaught:exception', (err) => {
+  if (
+    (err.message && err.message.includes('"PushNotifications" plugin is not implemented on web')) ||
+    (err.message && err.message.includes('Request failed with status code 403'))
+  ) {
+    return false;
+  }
+  return true;
+});
+});
+
+
 
 
 
