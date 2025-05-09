2a39225819909efb55618aafe1ef642909d8d755
sherelot <chillprocessor@gmail.com>
2025-05-09 22:00:18 +0800

    Created a reusable testTabNavigation helper; Removed unnecessary cy.wait()

--- cypress/homepage.cy.js
+++ cypress/homepage.cy.js
@@ -1,91 +1,28 @@
 describe('Home Page', () => {
-  
   beforeEach(() => {
-    //Restore localStorage and sessionStorage
-    cy.readFile('cypress/fixtures/storage.json').then((data) => {
-      //restore wallet info into localStorage
-      if(data){
-        cy.window().then((win) => {
-          Object.keys(data).forEach((key) => {
-            win.localStorage.setItem(key, data[key])
-          })
-          cy.log('Restored data: ', data)
-        })
-      }
-      else{
-        cy.log('no data found in storage.json')
-      }
-    })
-    cy.visit('/')
-  })
+    cy.restoreLocalStorage();
+    cy.setPushErrorHandling();
+    cy.visit('http://localhost:9000/#/apps');
+  });
 
   afterEach(() => {
-    cy.window().then((win) => {
-      cy.wait(4000)
-      
-      //reading existing data
-      cy.readFile('cypress/fixtures/storage.json').then((existingData) => {
-        const allLocalStorageData = {}
-        cy.log(existingData)
+    cy.saveLocalStorage();
+  });
 
-        //get new data
-        Object.keys(win.localStorage).forEach((key) => {
-          allLocalStorageData[key] = win.localStorage.getItem(key)
-        })
+  const testTabNavigation = (buttonSelector, expectedUrl, checkSelector) => {
+    cy.get(buttonSelector).click();
+    cy.url().should('equal', expectedUrl);
+    cy.get(checkSelector).should('exist');
+  };
 
-        //merge the existing data with the new data 
-        const mergedData = {
-          ...existingData,
-          ...allLocalStorageData
-        }
+  it('Checks Dashboard Tabs', () => {
+    cy.visit('/', { timeout: 10000 });
 
-        //write the merged data to storage.json
-        cy.writeFile('cypress/fixtures/storage.json',mergedData)
-        cy.log('Appended')
-        cy.log(mergedData)  
-      })
-  
-    })
-    
-  })
+    cy.measureHomeButtonLoadTime('#home-button', '#home-button', 'Home');
 
-  it('Checks Dashboard Tabs',() => {
-    cy.visit('/', { timeout: 10000 })
-
-    cy.measureHomeButtonLoadTime(
-      '#home-button',        // button selector
-      '#home-button',   // element to wait for
-      'Home'                 // label to expect
-    )
-    
-    cy.get('#send-button').click()
-    cy.wait(5000)
-    cy.url().should('equal', 'http://localhost:9000/#/send/select-asset')
-    cy.get('#SEND')
-    
-    cy.get('#receive-button').click()
-    cy.wait(5000)
-    cy.url().should('equal', 'http://localhost:9000/#/receive/select-asset')
-    cy.get('#RECEIVE')
-
-    cy.get('#apps-button').click()
-    cy.wait(5000)
-    cy.url().should('equal', 'http://localhost:9000/#/apps')
-    cy.get("#Applications")
-
-    cy.get('#qr-reader-button').click()
-    cy.wait(5000)
-    cy.url().should('equal', 'http://localhost:9000/#/qr-reader')
-    cy.get('#qr-reader-body')
-  })
-})
-      
-    
-  
-  
- 
-  
-
-
-
-
+    testTabNavigation('#send-button', 'http://localhost:9000/#/send/select-asset', '#SEND');
+    testTabNavigation('#receive-button', 'http://localhost:9000/#/receive/select-asset', '#RECEIVE');
+    testTabNavigation('#apps-button', 'http://localhost:9000/#/apps', '#Applications');
+    testTabNavigation('#qr-reader-button', 'http://localhost:9000/#/qr-reader', '#qr-reader-body');
+  });
+});
