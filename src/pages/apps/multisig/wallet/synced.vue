7256666ee67af8163400c556360af05a0aba69af
ubuntu <rongrammer@hotmail.com>
2025-05-27 12:40:42 +0800

    add page for importing synced wallets

---
+++ src/pages/apps/multisig/wallet/synced.vue
@@ -0,0 +1,88 @@
+<template>
+  <q-pull-to-refresh
+    id="app-container"
+    :class="getDarkModeClass(darkMode)"
+    @refresh="refreshPage"
+  >
+    <HeaderNav
+      :title="$t('Import Wallets')"
+      backnavpath="/apps/multisig"
+      class="apps-header"
+    />
+    <div class="row q-mt-lg justify-center">
+      <div class="col-xs-12 q-px-xs q-gutter-y-md">
+          <q-list >
+            <q-item>
+             <q-item-section overline>
+              <q-item-label>These are the wallets that you can import from the server.</q-item-label>
+             </q-item-section>
+             <q-item-section side top>
+               <q-btn icon="refresh" @click="fetchWallets" flat dense></q-btn>
+             </q-item-section>
+            </q-item>
+            <q-item v-for="wallet in multisigWallets?.filter(w => w.id && !w.enabled)">
+               <q-item-section>
+                 <q-item-label>{{ wallet.template?.name }}</q-item-label>
+                 <q-item-label caption lines="2">
+                  <span> 
+                    {{
+                      getSignerInfos(wallet).map((info) => `${info.signerEntityIndex}-${info.name}`).join(', ')
+                    }}
+                  </span>
+                 </q-item-label>
+               </q-item-section>
+               <q-item-section side top> 
+                   <q-btn color="primary" icon="cloud_download" @click="enableWallet(wallet)"  flat no-caps dense></q-btn>
+               </q-item-section>               
+            </q-item>
+            
+          </q-list>
+      </div>
+    </div>
+  </q-pull-to-refresh>
+</template>
+
+<script setup>
+
+import { useStore } from 'vuex'
+import { useI18n } from 'vue-i18n'
+import { computed, onMounted, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { useQuasar } from 'quasar'
+import HeaderNav from 'components/header-nav'
+import { getDarkModeClass } from 'src/utils/theme-darkmode-utils'
+import { shortenString, getSignerInfos } from 'src/lib/multisig'
+import { useMultisigHelpers } from 'src/composables/multisig/helpers'
+const $store = useStore()
+const $q = useQuasar()
+const { t: $t } = useI18n()
+const route = useRoute()
+const router = useRouter()
+const { getMultisigWalletBchBalance, getSignerXPrv, multisigWallets, localWallets } = useMultisigHelpers()
+
+const darkMode = computed(() => {
+  return $store.getters['darkmode/getStatus']
+})
+
+const enableWallet = (multisigWallet) => {
+  $store.commit('multisig/enableWallet', multisigWallet)
+}
+
+const fetchWallets = async () => {
+ localWallets.value?.forEach(async (localWallet) => {
+   if (localWallet?.deleted) return 
+   if (!localWallet?.wallet?.bch?.xPubKey) return
+   await $store.dispatch('multisig/fetchWallets', { xpub: localWallet.wallet.bch.xPubKey})
+ })
+}
+
+onMounted(async () => {
+  await fetchWallets()
+})
+</script>
+
+<style scoped>
+.light {
+  color: #141414;
+}
+</style>i
