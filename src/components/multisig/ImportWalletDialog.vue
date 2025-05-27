81f57eb3a2b852cb62e605e224b5510ed5eb1f78
ubuntu <rongrammer@hotmail.com>
2025-05-27 21:15:05 +0800

    update wallet import dialog

--- src/components/multisig/ImportWalletDialog.vue
+++ src/components/multisig/ImportWalletDialog.vue
@@ -4,23 +4,23 @@
     >
     <q-card class="q-dialog-plugin q-pb-md pt-card" :class="getDarkModeClass(darkMode)">
        <q-toolbar>
-          <q-toolbar-title><span class="text-weight-bold q-mr-sm">Import Wallet</span><q-icon name="import_export"/></q-toolbar-title>
+          <q-toolbar-title><span class="q-mr-sm">Import Wallet</span><q-icon name="import_export"/></q-toolbar-title>
           <q-btn flat round dense icon="close" v-close-popup />
         </q-toolbar>
       <q-card-section class="flex flex-wrap justify-around">
-        <q-btn flat dense no-caps @click="$emit('importFromFile')">
+        <q-btn flat dense no-caps @click="$emit('importFromFile')" class="tile">
           <template v-slot:default>
             <div class="row justify-center">
               <q-icon name="file_open" class="col-12" color="primary"></q-icon>
-              <div class="col-12">From File</div>
+              <div class="col-12 tile-label">From File</div>
             </div>
           </template>
         </q-btn>
-        <q-btn flat dense no-caps @click="$emit('importFromServer')">
+        <q-btn flat dense no-caps @click="$emit('importFromServer')" class="tile">
           <template v-slot:default>
             <div class="row justify-center">
               <q-icon name="cloud_download" class="col-12" color="primary"></q-icon>
-              <div class="col-12">From Server</div>
+              <div class="col-12 tile-label">From Server</div>
             </div>
           </template>
         </q-btn>
