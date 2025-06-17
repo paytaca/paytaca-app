bb15df0b6f518ce36fcd4bc73e6c1d62c654e1b0
ubuntu <rongrammer@hotmail.com>
2025-06-17 09:35:39 +0800

    add wallet getter by locking bytecode

--- src/store/multisig/getters.js
+++ src/store/multisig/getters.js
@@ -1,33 +1,8 @@
 import { CashAddressNetworkPrefix, hashTransaction, binToHex, cashAddressToLockingBytecode } from 'bitauth-libauth-v3'
-import { MultisigWallet } from 'src/lib/multisig'
+import { getLockingBytecode } from 'src/lib/multisig'
 
 export function getSettings (state) {
   return state.settings
-}
-
-export function getWallet (state) {
-  return ({ address }) => {
-    console.log('address provided', address)
-
-    const wallet = state.wallets.filter((walletObject) => {
-      const cashAddressNetworkPrefix = address.startsWith(CashAddressNetworkPrefix.testnet) ? CashAddressNetworkPrefix.testnet : CashAddressNetworkPrefix.mainnet
-      const wallet = new MultisigWallet(walletObject)
-      wallet.resolveDefaultAddress({
-        address: 0,
-        CashAddressNetworkPrefix: CashAddressNetworkPrefix.testnet
-      })
-
-      if (address.startsWith(CashAddressNetworkPrefix.testnet)) {
-        wallet.resolveDefaultAddress({
-          address: 0,
-          CashAddressNetworkPrefix: CashAddressNetworkPrefix.testnet
-        })
-      }
-      const defaultWalletAddress = wallet.getAddress({ addressIndex: 0, cashAddressNetworkPrefix })
-      return defaultWalletAddress === address
-    })
-    return wallet?.[0]
-  }
 }
 
 export function getWallets (state) {
@@ -38,6 +13,13 @@ export function getTransactionsLastIndex (state) {
   return state.transactions.length - 1
 }
 
+export function getWalletByLockingBytecode (state) {
+  return ({ lockingBytecodeHex }) => {
+      return state.wallets.find(w => {
+          return lockingBytecodeHex === getLockingBytecode({ ...w, hex: true }).bytecode
+      })
+  }
+}
 
 export function getTransactionsByLockingBytecode (state) {
   return ({ lockingBytecodeHex }) => {
@@ -73,12 +55,4 @@ export function getTransactionIndexByHash (state) {
   }
 }
 
-export function getPsts (state) {
-  return state.psts
-}
 
-export function getPstById (state) {
-  return ({ id }) => {
-    return state.psts.filter((i) => i.id === id)[0]
-  }
-}
