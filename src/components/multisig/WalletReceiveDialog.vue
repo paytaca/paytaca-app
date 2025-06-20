df4534c3996f4b1c69c43b066ba9c9e872b32dc5
ubuntu <rongrammer@hotmail.com>
2025-06-20 14:38:51 +0800

    inject copyText helper

--- src/components/multisig/WalletReceiveDialog.vue
+++ src/components/multisig/WalletReceiveDialog.vue
@@ -11,27 +11,25 @@
             v-model="isCashtoken"
             class="text-bow"
             style="margin: auto;"
-    	    keep-color
-    	    color="teal-5"
-   	    size="lg"
-    	    checked-icon="img:ct-logo.png"
+            keep-color
+            color="teal-5"
+            size="lg"
+            checked-icon="img:ct-logo.png"
             unchecked-icon="img:bch-logo.png"
             :class="getDarkModeClass(darkMode)"
-           />
+          />
           <q-img @click="isCashtoken = true" src="ct-logo.png" height="35px" width="35px" />
           <span @click="isCashtoken = true">&nbsp;{{ $t('CashToken') }}</span>
         </div>
         <qr-code :text="address" :size="220" :icon="isCashtoken ? 'ct-logo.png': 'bch-logo.png' "></qr-code>
         <div v-if="address" class="text-center text-caption flex flex-wrap justify-center items-center q-mt-sm q-gutter-x-sm">
             <q-btn icon="content_copy"
-             @click="$copyText(address)"
-             flat dense no-caps
-             >Address-{{ addressIndex }}: {{shortenString(address, 35)}}
+              @click="$copyText(address)"
+              flat dense no-caps
+            >
+              Address-{{ addressIndex }}: {{shortenAddressForDisplay(address)}}
             </q-btn>
         </div>
-        <!--div class="text-center text-caption flex flex-wrap justify-center items-center q-gutter-x-sm">
-            <span>Address Index:</span> <q-input v-model="addressIndex" type="number" outlined dense readonly></q-input>
-        </div -->
       </q-card-section>
       <q-card-actions>
         <q-btn label="Close" @click="onDialogOK" color="red" v-close-popup></q-btn>
@@ -40,47 +38,52 @@
   </q-dialog>
 </template>
 <script setup>
-import { useStore } from 'vuex'
-import { ref, onMounted, computed, watch, inject } from 'vue'
+import { ref, onMounted, watch, inject } from 'vue'
 import { useDialogPluginComponent } from 'quasar'
 import { getDarkModeClass } from 'src/utils/theme-darkmode-utils'
-import { useMultisigHelpers } from 'src/composables/multisig/helpers'
-import { shortenString, getMultisigCashAddress } from 'src/lib/multisig'
-import { CashAddressType, decodeCashAddress, encodeCashAddress, CashAddressNetworkPrefix } from 'bitauth-libauth-v3'
+import { getMultisigCashAddress } from 'src/lib/multisig'
+import { shortenAddressForDisplay } from 'src/utils/address-utils'
+import { CashAddressType, decodeCashAddress, encodeCashAddress } from 'bitauth-libauth-v3'
 
-const $copyText = inject('$copyText')
-
+// eslint-disable-next-line no-undef
 const props = defineProps({
   multisigWallet: Object,
   darkMode: Boolean,
   cashAddressNetworkPrefix: String
 })
-
+const $copyText = inject('$copyText')
 const isCashtoken = ref(false)
 const address = ref()
 const addressIndex = ref(0)
-const { dialogRef, onDialogHide, onDialogOK, onDialogCancel } = useDialogPluginComponent()
+const { dialogRef, onDialogOK } = useDialogPluginComponent()
 
 watch(() => isCashtoken.value, (isTrue) => {
- if (!address.value) return
- const decoded = decodeCashAddress(address.value)
- console.log('decoded', decoded)
- if (isTrue) {
-   const encoded = encodeCashAddress({ prefix: props.cashAddressNetworkPrefix, type: CashAddressType.p2shWithTokens, payload: decoded.payload })
-   address.value = encoded.address
-   return
- }
-  const encoded = encodeCashAddress({ prefix: props.cashAddressNetworkPrefix, type: CashAddressType.p2sh, payload: decoded.payload})
+  if (!address.value) return
+  const decoded = decodeCashAddress(address.value)
+  if (isTrue) {
+    const encoded = encodeCashAddress({
+      prefix: props.cashAddressNetworkPrefix,
+      type: CashAddressType.p2shWithTokens,
+      payload: decoded.payload
+    })
+    address.value = encoded.address
+    return
+  }
+  const encoded = encodeCashAddress({
+    prefix: props.cashAddressNetworkPrefix,
+    type: CashAddressType.p2sh,
+    payload: decoded.payload
+  })
   address.value = encoded.address
 })
 
 onMounted(() => {
-   address.value = getMultisigCashAddress({ 
+  address.value = getMultisigCashAddress({
     lockingData: props.multisigWallet.lockingData,
     template: props.multisigWallet.template,
     cashAddressNetworkPrefix: props.cashAddressNetworkPrefix
-   })
-   addressIndex.value = props.multisigWallet.lockingData.hdKeys.addressIndex 
+  })
+  addressIndex.value = props.multisigWallet.lockingData.hdKeys.addressIndex
 })
 
 </script>
