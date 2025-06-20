0c4b822665976a6837f6302446a2d2fd5a981b89
ubuntu <rongrammer@hotmail.com>
2025-06-20 12:07:36 +0800

    finalized local send bch tx proposal creation

--- src/pages/apps/multisig/wallet/transaction/create-send-bch-proposal.vue
+++ src/pages/apps/multisig/wallet/transaction/create-send-bch-proposal.vue
@@ -11,56 +11,56 @@
             />
             <div class="row justify-center">
               <div class="col-xs-12 q-px-sm">
-               <template v-if="multisigWallet">
-		<q-form class="q-gutter-md">
+              <template v-if="multisigWallet">
+                <q-form class="q-gutter-md">
                   <h6 style="margin-block-end: .5em; text-align: center" >
-                   Bitcoin Cash <q-icon name="img:bitcoin-cash-circle.svg"/>
+                  Bitcoin Cash <q-icon name="img:bitcoin-cash-circle.svg"/>
                   </h6>
-		  <div class="q-gutter-y-sm">
-                   <q-label class="text-bold">From</q-label>
-                   <q-input :model-value="multisigWallet.template.name" readonly outlined dense bottom-slots>
-                     <template v-slot:append>
-                       <q-btn icon="content_copy"
-                         @click="$copyText(getMultisigCashAddress({ ...multisigWallet, cashAddressNetworkPrefix }))"
-                         no-capse dense flat
-                       />
-                     </template>
-                  <template v-slot:hint>
+                  <div class="q-gutter-y-sm">
+                  <q-label class="text-bold">From</q-label>
+                  <q-input :model-value="multisigWallet.template.name" readonly outlined dense bottom-slots>
+                    <template v-slot:append>
+                      <q-btn icon="content_copy"
+                        @click="$copyText(getMultisigCashAddress({ ...multisigWallet, cashAddressNetworkPrefix }))"
+                        no-capse dense flat
+                      />
+                    </template>
+                    <template v-slot:hint>
                       <div class="text-bold" style="font-size: 1.2em;color: #0AC18E">Current Balance: {{ assetBalance }} BCH</div>
                     </template>
 
-                   </q-input>
+                  </q-input>
                   </div>
-		  <div class="q-gutter-y-sm">
-                   <q-label class="text-bold">Purpose</q-label>
-                   <q-input
-		    v-model="purpose"
-                    hint="Friendly message for cosigner"
-                    outlined dense>
-                   </q-input>
+                  <div class="q-gutter-y-sm">
+                    <q-label class="text-bold">Purpose</q-label>
+                    <q-input
+                      v-model="purpose"
+                      hint="Friendly message for cosigner"
+                      outlined dense>
+                  </q-input>
                   </div>
                   <div class="q-gutter-y-md">
                     <q-label class="text-bold">To</q-label>
-                    <div v-for="recipient, i in recipients" class="q-gutter-y-md">
-                     <div class="flex justify-between items-center">
+                    <div v-for="(recipient, i) in recipients" :key="i" class="q-gutter-y-md">
+                    <div class="flex justify-between items-center">
                         <span class="text-italic">Recipient {{ i + 1 }}</span>
                         <q-btn v-if="i > 0" @click="removeRecipient(i)" icon="remove" color="red" flat dense ></q-btn>
-                     </div>
-                     <q-input
-                       v-model="recipient.address" :label="`Paste address of recipient ${i + 1}`"
-                       outlined dense>
-                     </q-input>
-                     <q-input
-                       v-model="recipient.amount" label="Amount"
-                       outlined dense>
-                     </q-input>
-                   </div>
-                   <div class="text-right">
+                    </div>
+                    <q-input
+                      v-model="recipient.address" :label="`Paste address of recipient ${i + 1}`"
+                      outlined dense>
+                    </q-input>
+                    <q-input
+                      v-model="recipient.amount" label="Amount"
+                      outlined dense>
+                    </q-input>
+                  </div>
+                  <div class="text-right">
                     <q-btn @click="addRecipient()" icon="add" color="primary" label="Add Recipient" flat dense no-caps></q-btn>
-                   </div>
+                  </div>
                   </div>
                 </q-form>
-               </template>
+              </template>
               </div>
             </div>
             <div class="q-my-lg q-mx-sm"> </div>
@@ -76,17 +76,16 @@
 
 import { useStore } from 'vuex'
 import { useI18n } from 'vue-i18n'
-import { computed, ref, watch, onBeforeMount, onMounted, inject, reactive } from 'vue'
+import { computed, ref, onBeforeMount, onMounted, inject } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import { useQuasar } from 'quasar'
 import {
- cashAddressToLockingBytecode,
- encodeTransactionCommon,
- generateTransaction,
- getMinimumFee,
- hexToBin,
- getDustThreshold,
- isDustOutput
+  cashAddressToLockingBytecode,
+  encodeTransactionCommon,
+  generateTransaction,
+  getMinimumFee,
+  isDustOutput,
+  hexToBin
 } from 'bitauth-libauth-v3'
 
 import Watchtower from 'src/lib/watchtower'
@@ -94,15 +93,11 @@ import HeaderNav from 'components/header-nav'
 import { getDarkModeClass } from 'src/utils/theme-darkmode-utils'
 import {
   getMultisigCashAddress,
-  initEmptyMultisigTransaction,
-  estimateP2SHMultisigInputSize,
-  getRequiredSignatures,
-  getTotalSigners,
   getCompiler,
-  getUnlockingScriptId,
-  getLockingBytecode
+  getLockingBytecode,
+  generateTempProposalId
 } from 'src/lib/multisig'
-import { commonUtxoToLibauthInput, selectUtxos } from 'src/utils/utxo-utils'
+import { commonUtxoToLibauthInput, commonUtxoToLibauthOutput, selectUtxos } from 'src/utils/utxo-utils'
 import { useMultisigHelpers } from 'src/composables/multisig/helpers'
 const $store = useStore()
 const $q = useQuasar()
@@ -112,9 +107,6 @@ const route = useRoute()
 const { t: $t } = useI18n()
 const { cashAddressNetworkPrefix } = useMultisigHelpers()
 const multisigWallet = ref()
-const assetSelectTab = ref('bch')
-const assetSelected = ref('Bitcoin Cash')
-const assetOptions = ref(['Bitcoin Cash'])
 const assetDecimals = ref(8)
 const assetBalance = ref()
 const purpose = ref('Send Bitcoin Cash')
@@ -125,11 +117,9 @@ const darkMode = computed(() => {
 })
 
 const utxos = computed(() => {
-  return structuredClone($store.getters['multisig/getWalletUtxos']({ address: route.params.address }))
-})
-
-const utxosLastUpdate = computed(() => {
-  return utxos.value?.lastUpdate
+  return structuredClone(
+    $store.getters['multisig/getWalletUtxos']({ address: route.params.address })
+  )
 })
 
 const removeRecipient = (index) => {
@@ -143,31 +133,26 @@ const addRecipient = () => {
   })
 }
 
-const createProposal = () => {
- 
+const createProposal = async () => {
   const sendAmount = recipients.value.reduce((amtAccumulator, nextRecipient) => {
     const amountNoDecimals = Math.floor(nextRecipient.amount * `1e${assetDecimals.value}`)
     amtAccumulator += BigInt(amountNoDecimals)
     return amtAccumulator
   }, 0n)
-    
+
   const selectUtxosOptions = {
     targetAmount: sendAmount,
     filterStrategy: 'bch-only',
     sortStrategy: 'smallest'
   }
 
-  let selected = selectUtxos(utxos.value.utxos, selectUtxosOptions)
+  const selected = selectUtxos(utxos.value.utxos, selectUtxosOptions)
   console.log('SELECTED BEFORE FEE ESTIMATE', selected)
   // Construct inputs
   const inputs = selected.selectedUtxos
-  //const inputs = selected.selectedUtxos.map(u => {
-    //return commonUtxoToLibauthInput(u, sampleUnlockingBytecode)
-  //})
-  
   // Construct outputs
   const outputs = recipients.value.map((recipient) => {
-    let valueSatoshis = BigInt(Math.floor(recipient.amount * `1e${assetDecimals.value}`))
+    const valueSatoshis = BigInt(Math.floor(recipient.amount * `1e${assetDecimals.value}`))
     const output = {
       lockingBytecode: cashAddressToLockingBytecode(recipient.address).bytecode,
       valueSatoshis
@@ -185,38 +170,43 @@ const createProposal = () => {
   })
 
   const unlockingBytecode = scenario.program.transaction.inputs[0].unlockingBytecode
-  
+
   const changeOutput = {
     lockingBytecode: getLockingBytecode(multisigWallet.value).bytecode,
     valueSatoshis: selected.total - sendAmount // Temp value, tx fee not yet accounted for, will just be used for scenario
   }
 
   scenario.program.transaction.inputs = inputs.map(u => commonUtxoToLibauthInput(u, unlockingBytecode))
-  scenario.program.transaction.outputs = [ ...outputs, changeOutput ]
-  
+  scenario.program.transaction.outputs = [...outputs, changeOutput]
+
   if (!isDustOutput(changeOutput)) {
     scenario.program.transaction.outputs.push(changeOutput)
   }
-  
+
   const dustRelayFeeSatPerKb = 1100n // 1.1 per byte
   const transactionForFeeEstimation = generateTransaction(scenario.program.transaction)
   const estimatedTransactionSize = encodeTransactionCommon(transactionForFeeEstimation.transaction).length
+  console.log('Transaction Size', estimatedTransactionSize)
   const minimumFee = getMinimumFee(BigInt(estimatedTransactionSize), dustRelayFeeSatPerKb)
-  
+
   // selectWithFee
   selectUtxosOptions.targetAmount = sendAmount + minimumFee
   const finalSelected = selectUtxos(utxos.value.utxos, selectUtxosOptions)
-  const finalInputs = finalSelected.selectedUtxos.map(u => commonUtxoToLibauthInput(u)) // without unlocking bytecode
+  console.log('final selected', finalSelected)
+  
+  const finalInputs = finalSelected.selectedUtxos.map(u => commonUtxoToLibauthInput(u, [])) // without unlocking bytecode
+  console.log('final inputs', finalInputs)
   const finalChangeOutput = {
-   lockingBytecode: getLockingBytecode(multisigWallet.value).bytecode,
-   valueSatoshis: finalSelected.total - sendAmount - minimumFee
+    lockingBytecode: getLockingBytecode(multisigWallet.value).bytecode,
+    valueSatoshis: finalSelected.total - sendAmount - minimumFee
   }
-  const changeOutputIsDust = isDustOutput(finalChangeOutput, dustRelayFeeSatPerKb)
-  console.log('Change output is dust', changeOutputIsDust)
+
+  console.log('Change output is dust', isDustOutput(finalChangeOutput))
+
   if (!isDustOutput(finalChangeOutput)) {
     outputs.push(finalChangeOutput)
   }
-  
+
   const finalTransaction = {
     locktime: 0,
     version: 2,
@@ -224,27 +214,44 @@ const createProposal = () => {
     outputs: outputs
   }
 
-  console.log('Final utxo selections', finalSelected) 
+  console.log('Final utxo selections', finalSelected)
   if (finalSelected.total < sendAmount + minimumFee) {
     $q.dialog({ message: 'Insufficient Balance' })
   }
-  
-  //TODO: prepare multisig tx proposal for signing
-  console.log('TRANSACTION', scenario.program.transaction)
+
+  // TODO: prepare multisig tx proposal for signing
+  console.log('Final inputs', finalInputs)
+  console.log('Total Output', finalSelected.total)
+  console.log('Send Amount', sendAmount)
+  console.log('Minimum Fee', minimumFee)
+  console.log('Change Amount', finalChangeOutput.valueSatoshis)
   console.log('FINAL TRANSACTION', finalTransaction)
+  const sourceOutputs =
+    finalSelected.selectedUtxos
+     .map(u => {
+        const utxo = commonUtxoToLibauthOutput(u)
+        return {
+          ...utxo,
+          lockingBytecode: finalChangeOutput.lockingBytecode,
+          outpointTransactionHash: hexToBin(u.txid),
+          outpointIndex: Number(u.vout)
+        }
+     })
+  console.log('SOURCE OUTPUTS', sourceOutputs)
+  const multisigTransaction = {
+    origin: 'paytaca',
+    purpose: purpose.value,
+    transaction: finalTransaction,
+    sourceOutputs,
+    addressIndex: multisigWallet.value.lockingData.hdKeys.addressIndex,
+    address: route.params.address
+  }
+  console.log('Multisig Transaction', multisigTransaction)
+  await $store.dispatch('multisig/createTransaction', { multisigWallet: multisigWallet.value, multisigTransaction })
+  const index = $store.getters['multisig/getTransactionsLastIndex']  
+  console.log('LAST INDEX', index)
+  router.push({ name: 'app-multisig-wallet-transaction-view', params: { address: route.params.address, index } })
 }
-
-const updateAssetsOptions = (utxos) => {
-  const options = new Set()
-  utxos?.forEach((utxo) => {
-    if (utxo?.token?.category && utxo.token?.amount && BigInt(utxo.token?.amount) > 0n) options.add(utxo.token.category)
-  })
-  assetOptions.value = ['Bitcoin Cash', ...Array.from(options)]
-}
-
-watch(utxosLastUpdate.value, (value, oldValue) => {
-  updateAssetsOptions(utxos.value?.utxos)
-})
 
 onBeforeMount(async () => {
   await $store.dispatch('multisig/fetchWalletUtxos', route.params.address)
@@ -258,6 +265,5 @@ onMounted(async () => {
   }
   multisigWallet.value = $store.getters['multisig/getWalletByAddress']({ address: route.params.address })
   addRecipient()
-  updateAssetsOptions(utxos.value?.utxos)
 })
 </script>
