ce112cda74c4737cf443353aaf494edd91e7750a
ubuntu <rongrammer@hotmail.com>
2025-06-28 16:20:01 +0800

    disable wallet share button if its already synced

--- src/components/multisig/WalletActionsDialog.vue
+++ src/components/multisig/WalletActionsDialog.vue
@@ -5,17 +5,22 @@
     <q-card class="q-dialog-plugin q-pb-md pt-card text-bow" :class="getDarkModeClass(darkMode)">
        <q-toolbar>
           <q-toolbar-title>
-            <span class="q-mr-sm">{{ $t('Options')}}</span>
+            <span class="q-mr-sm"></span>
           </q-toolbar-title>
           <q-btn flat round dense icon="close" v-close-popup />
         </q-toolbar>
       <q-item-label header style="padding-top: 0px;padding-bottom:0px;">Wallet</q-item-label>
       <q-card-section class="flex flex-wrap justify-evenly"> 
-        <q-btn flat dense no-caps @click="$emit('uploadWallet')" class="tile" v-close-popup>
+        <q-btn flat dense no-caps @click="$emit('uploadWallet')" class="tile" :disable="isMultisigWalletSynced" v-close-popup>
           <template v-slot:default>
             <div class="row justify-around">
-              <q-icon name="cloud_upload" class="col-12" color="primary"></q-icon>
-              <div class="col-12 tile-label">Share Online</div>
+              <q-icon 
+                :name="!isMultisigWalletSynced ? 'cloud_upload': 'mdi-cloud-check'" 
+                class="col-12" 
+                :color="!isMultisigWalletSynced? 'primary': 'green' ">
+              </q-icon>
+              <div class="col-12 tile-label">Share</div>
+              
             </div>
           </template>
         </q-btn>
@@ -23,7 +28,7 @@
           <template v-slot:default>
             <div class="row justify-center">
               <q-icon name="mdi-file-export" class="col-12" color="primary"></q-icon>
-              <div class="col-12 tile-label">Export Wallet File</div>
+              <div class="col-12 tile-label">Export</div>
             </div>
           </template>
         </q-btn>
@@ -31,7 +36,7 @@
           <template v-slot:default>
             <div class="row justify-center">
               <q-icon name="delete_forever" class="col-12" color="red"></q-icon>
-              <div class="col-12 tile-label">Delete Wallet</div>
+              <div class="col-12 tile-label">Delete</div>
             </div>
           </template>
         </q-btn>
@@ -42,7 +47,7 @@
           <template v-slot:default>
             <div class="row justify-center">
               <q-icon name="mdi-file-import" class="col-12" color="primary"></q-icon>
-              <div class="col-12 tile-label">Import Tx</div>
+              <div class="col-12 tile-label">Import</div>
             </div>
           </template>
         </q-btn>
@@ -50,26 +55,26 @@
           <template v-slot:default>
             <div class="row justify-center">
               <q-icon name="mdi-text-box-multiple" class="col-12" color="primary" style="position:relative">
-                <q-badge color="red" v-if="txProposals?.length > 0" style="margin-right: 20px;" floating>
-                 {{txProposals.length}}
+                <q-badge color="red" v-if="txproposals?.length > 0" style="margin-right: 20px;" floating>
+                 {{txproposals.length}}
                 </q-badge>
               </q-icon>
-              <div class="col-12 tile-label">Tx Proposals</div>
+              <div class="col-12 tile-label">Proposals</div>
             </div>
           </template>
         </q-btn>
-        <q-btn flat dense no-caps @click="$emit('createTxProposal')" class="tile" v-close-popup>
+        <!--q-btn flat dense no-caps @click="$emit('createTxProposal')" class="tile" v-close-popup>
           <template v-slot:default>
             <div class="row justify-center">
               <q-icon name="add" class="col-12" color="primary"></q-icon>
               <div class="col-12 tile-label">New Tx</div>
             </div>
           </template>
-        </q-btn>
+        </q-btn-->
          <q-btn flat dense no-caps @click="$emit('createSendBchProposal')" class="tile" v-close-popup>
           <template v-slot:default>
             <div class="row justify-center">
-              <q-icon name="add" class="col-12" color="primary"></q-icon>
+              <q-icon name="send" class="col-12" color="primary"></q-icon>
               <div class="col-12 tile-label">Send BCH</div>
             </div>
           </template>
@@ -77,8 +82,8 @@
        <q-btn flat dense no-caps @click="$emit('receive')" class="tile" v-close-popup>
           <template v-slot:default>
             <div class="row justify-center">
-              <q-icon name="call_received" class="col-12" color="primary"></q-icon>
-              <div class="col-12 tile-label">Receive</div>
+              <q-icon name="send_and_archive" class="col-12" color="primary"></q-icon>
+              <div class="col-12 tile-label">Deposit</div>
             </div>
           </template>
         </q-btn>
@@ -106,6 +111,7 @@ const $emit = defineEmits ([
 
 defineProps({
   darkMode: Boolean,
-  txProposals: Array
+  txProposals: Array,
+  isMultisigWalletSynced: Boolean
 })
 </script>
