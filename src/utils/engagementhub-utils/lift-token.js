f9503467faeb31b840116f9ef7763ddabee24be6
LukeHeimen <kdlanante@up.edu.ph>
2025-07-08 21:14:56 +0800

    Add function to generate signature to be used in the swap contract in the backend

--- src/utils/engagementhub-utils/lift-token.js
+++ src/utils/engagementhub-utils/lift-token.js
@@ -1,7 +1,15 @@
+import { cashAddressToLockingBytecode, hexToBin } from "@bitauth/libauth"
+import { hash256 } from '@cashscript/utils'
+import { HashType, SignatureTemplate } from "cashscript"
 import { createSighashPreimage, publicKeyToP2PKHLockingBytecode } from "cashscript/dist/utils"
 import { getWalletHash } from 'src/utils/engagementhub-utils/shared'
 
 import axios from 'axios'
+import Watchtower from "watchtower-cash-js"
+
+
+const watchtower = new Watchtower(false)
+const bchGenesisBlockTime = 1501834939
 
 export const SaleGroup = {
   SEED: 'seed',
@@ -19,12 +27,65 @@ const ENGAGEMENT_HUB_URL =
   process.env.ENGAGEMENT_HUB_URL || 'https://engagementhub.paytaca.com/api/'
 const LIFTTOKEN_URL = axios.create({ baseURL: `${ENGAGEMENT_HUB_URL}lifttoken/` })
 
+
+function hexToUint8Array(hexString) {
+  if (hexString.length % 2 !== 0) {
+    throw new Error("Hex string must have an even number of characters.");
+  }
+  const uint8Array = new Uint8Array(hexString.length / 2);
+  for (let i = 0; i < hexString.length; i += 2) {
+    uint8Array[i / 2] = parseInt(hexString.substring(i, i + 2), 16);
+  }
+  return uint8Array;
+}
+
 // ================================
 // non-Promise functions
 // ================================
 
-export function processGenerateSigParams() {
-  
+export async function generateSignature(txId, wif) {
+  const tx = await watchtower.BCH._api
+    .post(`transactions/${txId}`)
+    .then(resp => {
+      return resp.data.details
+    })
+
+  const locktime = Math.floor((tx.timestamp - bchGenesisBlockTime) / 600)
+  const utxoLockingBytecode = cashAddressToLockingBytecode(tx.inputs[0].address)
+
+  const transaction = {
+    version: 2,
+    locktime,
+    inputs: [{
+      outpointIndex: tx.inputs[0].spent_index,
+      outpointTransactionHash: hexToBin(txId),
+      sequenceNumber: 0xfffffffe,
+      unlockingBytecode: new Uint8Array(),
+    }],
+    outputs: [{
+      lockingBytecode: utxoLockingBytecode,
+      valueSatoshis: 1000n
+    }]
+  }
+  const sourceOutputs = [{
+    lockingBytecode: utxoLockingBytecode,
+    valueSatoshis: BigInt(tx.outputs[0].value),
+  }]
+
+  const sigHashType = HashType.SIGHASH_SINGLE | HashType.SIGHASH_ANYONECANPAY // 0x83
+  const signatureTemplate = new SignatureTemplate(wif, sigHashType)
+
+  const signature = generateP2PKHSig({
+    template: signatureTemplate,
+    transaction,
+    inputIndex: 0,
+    sourceOutputs,
+  })
+
+  return {
+    signature: Buffer.from(signature).toString('hex'),
+    txFee: tx.tx_fee
+  }
 }
 
 /**
