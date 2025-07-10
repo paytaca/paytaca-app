355189526fc7458939bd535f554df7ac1b941110
khirvy019 <khirvy_19@yahoo.com>
2025-07-10 08:05:56 +0800

    remove sanitize vault process

--- src/boot/vuex.js
+++ src/boot/vuex.js
@@ -1,7 +1,6 @@
 import { boot } from 'quasar/wrappers'
 import { migrateVuexStorage } from 'src/utils/indexed-db-rollback/rollback-vuex-storage'
 import { populateMissingVaults, recoverWalletsFromStorage } from 'src/utils/indexed-db-rollback/wallet-recovery'
-import { sanitizeVault } from 'src/utils/indexed-db-rollback/wallet-vault'
 import { updatePreferences } from 'src/utils/indexed-db-rollback/update-preferences'
 import { resetWalletsAssetsList } from 'src/utils/indexed-db-rollback/reset-asset-list'
 import useStore from 'src/store'
@@ -43,12 +42,10 @@ export default boot(async (obj) => {
 
     app.use(store)
 
-    sanitizeVault()
     await recoverWalletsFromStorage()
     await resetWalletsAssetsList()
     updatePreferences()
     populateMissingVaults()
-    
   } catch (err) {
     console.error('Error initializing Vuex store:', err)
     // Initialize store with default state if hydration fails
