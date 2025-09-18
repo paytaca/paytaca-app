261b6ccc2feb33e3ef93da7d18c34df98d5c28fa
LukeHeimen <kdlanante@up.edu.ph>
2025-09-18 17:12:34 +0800

    Add a function for parsing locale-formatted amount for proper parsing when using the custom keybord

--- src/utils/custom-keyboard-utils.js
+++ src/utils/custom-keyboard-utils.js
@@ -1,3 +1,5 @@
+import { getLocaleSeparators } from "./denomination-utils"
+
 /**
  * Parses the key clicked in the custom keyboard and inserts
  * it into the text from the currently-focused input textfield.
@@ -71,4 +73,38 @@ export function adjustSplicedAmount (text, caretPosition, addedItem = null) {
     return text.split('').toSpliced(caretPosition, 0, addedItem).join('')
   }
   return text.split('').toSpliced(caretPosition, 1).join('')
+}
+
+/**
+ * Parse the locale-formatted `amount` by removing the group/thousand separators
+ * and setting the decimal separator to a dot (**.**). This is done in order to
+ * properly format the amount when using the custom keyboard.
+ * @param {number | string} amount the formatted amount to be parsed
+ * @param {boolean} isDecimalClicked determined whether the key clicked previously
+ * is a decimal; if it is, then it will add a decimal point to the parsed amount
+ * @returns the parsed amount
+ */
+export function parseFormattedAmount (amount, isDecimalClicked) {
+  const separators = getLocaleSeparators()
+
+  let parsedAmount = String(amount)
+  if (parsedAmount !== '') {
+    // split decimal from amount
+    const splittedAmount = parsedAmount.split(separators.decimal)
+
+    // remove thousand separators
+    parsedAmount = splittedAmount[0].replaceAll(separators.group, '')
+  
+    // if decimal separator is not '.', replace it with '.'
+    if (parseInt(splittedAmount[1]) > 0) {
+      // remove dangling zero in decimal
+      const nonZeroDec = splittedAmount[1].replaceAll('0', '')
+      parsedAmount = `${parsedAmount}.${nonZeroDec}`
+    }
+
+    if (isDecimalClicked) parsedAmount = `${parsedAmount}.`
+  }
+
+
+  return parsedAmount
 }
