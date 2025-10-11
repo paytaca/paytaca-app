a171955fbc44b935a9b7e106d2194cd2d99c672b
LukeHeimen <kdlanante@up.edu.ph>
2025-10-11 15:12:18 +0800

    Remove q-input and custom keyboard in generate qr page and replace with custom input component

--- src/pages/qr/generate-qr.vue
+++ src/pages/qr/generate-qr.vue
@@ -95,8 +95,9 @@
             <div class="receive-label q-mt-md" style="font-size: 15px;">
               {{ $t('YouWillReceive') }}
             </div>
-            <div class="text-weight-light receive-amount-label" style="font-size: 18px;">
-              {{ amount }} {{ setAmountInFiat ? String(selectedMarketCurrency()).toUpperCase() : 'BCH' }}
+            <div class="receive-amount-label" style="font-size: 18px;">
+              {{ formatWithLocale(amount, decimalObj) }}
+              {{ setAmountInFiat ? String(selectedMarketCurrency()).toUpperCase() : 'BCH' }}
             </div>
           </div>
         </div>
@@ -106,7 +107,7 @@
           :class="getDarkModeClass(darkMode)"
           v-if="!isCt"
         >
-          <span class="cursor-pointer" @click="amountDialog = true; customKeyboardState = 'show';">
+          <span class="cursor-pointer" @click="amountDialog = true">
             {{ amount ? $t('Update') : $t('Set') }} {{ $t('Amount') }}
           </span>
           <span class="q-ml-md text-negative cursor-pointer" @click="amount = ''">
@@ -123,28 +124,20 @@
             size="lg"
             icon="close"
             class="close-button"
-            @click="setReceiveAmount('close')"
+            @click="amountDialog = false"
           />
         </div>
         <div :style="`margin-top: ${$q.screen.height * .15}px`">
         <div class="text-center text-bow text-h6" :class="getDarkModeClass(darkMode)">{{ $t('SetReceiveAmount') }}</div>
         <div class="col q-mt-md q-px-lg text-center">
-          <q-input
-            type="text"
-            inputmode="none"
-            @focus="openCustomKeyboard(true)"
-            filled
-            v-model="tempAmount"
-            :label="$t('Amount')"
-            :readonly="readonlyState"
-            :dark="darkMode"
-          >
-            <template v-slot:append>
-              <div class="q-pr-sm text-weight-bold" style="font-size: 15px;">
-                {{setAmountInFiat ? String(selectedMarketCurrency()).toUpperCase() : 'BCH'}}
-              </div>
-            </template>
-          </q-input>
+          <custom-input
+            v-model="amount"
+            :inputSymbol="setAmountInFiat ? String(selectedMarketCurrency()).toUpperCase() : 'BCH'"
+            :inputRules="[val => Boolean(val) || $t('InvalidAmount')]"
+            :asset="null"
+            :decimalObj="decimalObj"
+            @on-check-click="amountDialog = false"
+          />
         </div>
         <div
           class="q-pt-md text-subtitle1 button button-text-primary set-amount-button cursor-pointer"
@@ -157,12 +150,6 @@
       </div>
     </div>
   </div>
-
-  <customKeyboard
-    :custom-keyboard-state="customKeyboardState"
-    v-on:addKey="setAmount"
-    v-on:makeKeyAction="makeKeyAction"
-  />
 </template>
 
 <script>
@@ -170,9 +157,11 @@ import { getDarkModeClass, isNotDefaultTheme } from 'src/utils/theme-darkmode-ut
 import { getWatchtowerWebsocketUrl, convertCashAddress } from 'src/wallet/chipnet'
 import { Address } from 'src/wallet'
 import { useWakeLock } from '@vueuse/core'
+import { formatWithLocale } from 'src/utils/denomination-utils'
+
 import HeaderNav from 'src/components/header-nav'
 import ProgressLoader from 'src/components/ProgressLoader'
-import customKeyboard from 'src/components/CustomKeyboard.vue'
+import CustomInput from 'src/components/CustomInput.vue'
 
 export default {
   name: 'GenerateQR',
@@ -180,7 +169,7 @@ export default {
   components: {
     HeaderNav,
     ProgressLoader,
-    customKeyboard
+    CustomInput
   },
 
   data () {
@@ -192,10 +181,7 @@ export default {
       addresses: [],
       incomingTransactions: [],
       amountDialog: false,
-      customKeyboardState: 'dismiss',
       amount: '',
-      tempAmount: '',
-      readonlyState: false,
       setAmountInFiat: true
     }
   },
@@ -233,20 +219,17 @@ export default {
     },
     selectedAssetMarketPrice () {
       return this.$store.getters['market/getAssetPrice']('bch', this.selectedMarketCurrency())
+    },
+    decimalObj () {
+      return this.setAmountInFiat ? { min: 0, max: 4 } : { min: 0, max: 8 }
     }
   },
 
   methods: {
     getDarkModeClass,
     isNotDefaultTheme,
-    setReceiveAmount (state) {
-      if (state !== 'close') {
-        this.amount = this.tempAmount
-      }
-      this.readonlyState = false
-      this.amountDialog = false
-      this.customKeyboardState = 'dismiss'
-    },
+    formatWithLocale,
+
     selectedMarketCurrency () {
       const currency = this.$store.getters['market/selectedCurrency']
       return currency && currency.symbol
@@ -254,60 +237,6 @@ export default {
     displayAddress (address) {
       if (address) {
         return address.substring(0, 16) + '...' + address.substring(address.length - 4)
-      }
-    },
-    setAmount (key) {
-      let receiveAmount, finalAmount, tempAmountFormatted = ''
-
-      receiveAmount = this.tempAmount
-
-      receiveAmount = receiveAmount === null ? '' : receiveAmount
-      if (key === '.' && receiveAmount === '') {
-        finalAmount = '0.'
-      } else {
-        finalAmount = receiveAmount.toString()
-        const hasPeriod = finalAmount.indexOf('.')
-        if (hasPeriod < 1) {
-          if (Number(finalAmount) === 0 && Number(key) > 0) {
-            finalAmount = key
-          } else {
-            // Check amount if still zero
-            if (Number(finalAmount) === 0 && Number(finalAmount) === Number(key)) {
-              finalAmount = 0
-            } else {
-              finalAmount += key.toString()
-            }
-          }
-        } else {
-          finalAmount += key !== '.' ? key.toString() : ''
-        }
-      }
-      // // Set the new amount
-      this.tempAmount = finalAmount
-    },
-    makeKeyAction (action) {
-      if (action === 'backspace') {
-        // Backspace
-        this.tempAmount = String(this.tempAmount).slice(0, -1)
-      } else if (action === 'delete') {
-        // Delete
-        this.tempAmount = ''
-      } else {
-        // Enabled submit slider
-        if (this.tempAmount) {
-          this.setReceiveAmount('gen')
-        }
-        this.customKeyboardState = 'dismiss'
-        this.readonlyState = false
-      }
-    },
-    openCustomKeyboard (state) {
-      this.readonlyState = state
-
-      if (state) {
-        this.customKeyboardState = 'show'
-      } else {
-        this.customKeyboardState = 'dismiss'
       }
     },
     convertFiatToSelectedAsset (amount) {
