89e6e24e8b9cb4513e9625c8dbbcda50a51e1644
hantyrram <rongrammer@hotmail.com>
2025-10-22 23:53:05 +0800

    add computed prop helper

--- src/composables/multisig/helpers.js
+++ src/composables/multisig/helpers.js
@@ -22,6 +22,10 @@ export const useMultisigHelpers = () => {
     return $store.getters['global/isChipnet']
   })
 
+  const network = computed(() => {
+    return isChipnet.value ? WatchtowerNetwork.chipnet : WatchtowerNetwork.mainnet
+  })
+
   const multisigCoordinationServer = computed(() => {
     return new WatchtowerCoordinationServer({
       network: $store.getters['global/isChipnet'] ? WatchtowerNetwork.chipnet: WatchtowerNetwork.mainnet 
@@ -67,6 +71,7 @@ export const useMultisigHelpers = () => {
     return wallets
   })
 
+  // transactions.send
   const getSignerWalletFromVault = ({ xpub }) => {
     const vaultIndex = localWallets.value.findIndex((signerWallet) => {
       return signerWallet.wallet.bch.xPubKey === xpub
@@ -86,65 +91,65 @@ export const useMultisigHelpers = () => {
     return hdKeys.hdPrivateKey
   }
 
-  const getSignerMnemonic = async ({ xpub }) => {
-    const signerWallet = getSignerWalletFromVault({ xpub })
-    if (!signerWallet) return
-    const { mnemonic } = await loadWallet('BCH', signerWallet.vaultIndex)
-    return mnemonic
-  }
+  // const getSignerMnemonic = async ({ xpub }) => {
+  //   const signerWallet = getSignerWalletFromVault({ xpub })
+  //   if (!signerWallet) return
+  //   const { mnemonic } = await loadWallet('BCH', signerWallet.vaultIndex)
+  //   return mnemonic
+  // }
 
   /**
    * Among the signers, check which belongs to this Paytaca Wallet.
    * Use the first one found.
    * @returns {number|undefined} 1 if signer_1, 2 if signer_2, ...
    */
-  const identifyPossiblePstCreator = async ({ signers }) => {
-    for (const signerEntityIndex of Object.keys(signers)) {
-      const walletFound = localWallets.value.find((signerWallet) => {
-        return signerWallet.wallet.bch.xPubKey === signers[signerEntityIndex].xpub
-      })
-      if (walletFound) {
-        return signerEntityIndex
-      }
-    }
-    return undefined
-  }
+  // const identifyPossiblePstCreator = async ({ signers }) => {
+  //   for (const signerEntityIndex of Object.keys(signers)) {
+  //     const walletFound = localWallets.value.find((signerWallet) => {
+  //       return signerWallet.wallet.bch.xPubKey === signers[signerEntityIndex].xpub
+  //     })
+  //     if (walletFound) {
+  //       return signerEntityIndex
+  //     }
+  //   }
+  //   return undefined
+  // }
 
-  const transactionsLastIndex = computed(() => {
-    return $store.getters['multisig/getTransactionsLastIndex']
-  })
+  // const transactionsLastIndex = computed(() => {
+  //   return $store.getters['multisig/getTransactionsLastIndex']
+  // })
 
-  const saveTransaction = async (multisigTransaction) => {
-    await $store.dispatch(
-      'multisig/saveTransaction',
-      multisigTransaction
-    )
-  }
+  // const saveTransaction = async (multisigTransaction) => {
+  //   await $store.dispatch(
+  //     'multisig/saveTransaction',
+  //     multisigTransaction
+  //   )
+  // }
 
-  const updateTransaction = async ({ index, multisigTransaction }) => {
-    await $store.dispatch('multisig/updateTransaction', { index, multisigTransaction })
-  }
+  // const updateTransaction = async ({ index, multisigTransaction }) => {
+  //   await $store.dispatch('multisig/updateTransaction', { index, multisigTransaction })
+  // }
 
-  const getMultisigWalletBchBalance = async (address) => {
-    const watchtower = new Watchtower($store.getters['global/isChipnet'])
-    const response = await watchtower.getAddressBchBalance(address)
-    return response?.data?.balance
-  }
+  // const getMultisigWalletBchBalance = async (address) => {
+  //   const watchtower = new Watchtower($store.getters['global/isChipnet'])
+  //   const response = await watchtower.getAddressBchBalance(address)
+  //   return response?.data?.balance
+  // }
 
-  const getTransactionsByWalletAddress = ({ address }) => {
-    const transactions =
-         $store.getters['multisig/getTransactionsByWalletAddress']({
-           address: address
-         }).filter((transaction) => {
-	   return transaction.metadata.address === address
-	 }).map(transaction => structuredClone(transaction))
-   return transactions     
-  }
+  // const getTransactionsByWalletAddress = ({ address }) => {
+  //   const transactions =
+  //        $store.getters['multisig/getTransactionsByWalletAddress']({
+  //          address: address
+  //        }).filter((transaction) => {
+	//    return transaction.metadata.address === address
+	//  }).map(transaction => structuredClone(transaction))
+  //  return transactions     
+  // }
 
-  const getTransactionsByMultisigWallet = (wallet) => {
-    const lockingBytecodeHex = binToHex(getLockingBytecode({ template: wallet.template, lockingData: wallet.lockingData}).bytecode)
-    return $store.getters['multisig/getTransactionsByLockingBytecode']({ lockingBytecodeHex})
-  }
+  // const getTransactionsByMultisigWallet = (wallet) => {
+  //   const lockingBytecodeHex = binToHex(getLockingBytecode({ template: wallet.template, lockingData: wallet.lockingData}).bytecode)
+  //   return $store.getters['multisig/getTransactionsByLockingBytecode']({ lockingBytecodeHex})
+  // }
 
   // const getMultisigWalletByHash = (walletHash) => {
   //   const savedWallet = $store.getters['multisig/getWalletByHash'](walletHash)
@@ -218,21 +223,13 @@ export const useMultisigHelpers = () => {
     localWallets,
     getSignerWalletFromVault,
     getSignerXPrv,
-    getSignerMnemonic,
-    identifyPossiblePstCreator,
-    saveTransaction,
-    updateTransaction,
-    transactionsLastIndex,
     cashAddressNetworkPrefix,
     multisigWallets,
-    getTransactionsByWalletAddress,
-    getTransactionsByMultisigWallet,
-    getMultisigWalletBchBalance,
     txExplorerUrl,
-    // getMultisigWalletByHash,
     getAssetTokenIdentity,
     multisigNetworkProvider: multisigNetworkProvider.value,
     multisigCoordinationServer: multisigCoordinationServer.value,
-    resolveXprvOfXpub: resolveXPrvOfXpub.value
+    resolveXprvOfXpub: resolveXPrvOfXpub.value,
+    network: network.value
   }
 }
