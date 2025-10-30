89c22d6fc1b93341b93497f8219d90e982a8a966
LukeHeimen <kdlanante@up.edu.ph>
2025-10-30 10:07:16 +0800

    Add initial page UI for address book page

---
+++ src/components/address-book/AddContactSticky.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page-sticky position="bottom-right" :offset="[18, 18]">
+    <q-btn
+      fab
+      icon="mdi-account-plus"
+      color="primary"
+    >
+      <q-tooltip
+        anchor="center left"
+        self="center right"
+        :offset="[10, 10]"
+      >
+        Add New Contact
+      </q-tooltip>
+    </q-btn>
+  </q-page-sticky>
+</template>
+
+<script>
+export default {
+  name: 'AddContactSticky'
+}
+</script>
--- src/pages/apps/address-book/index.vue
+++ src/pages/apps/address-book/index.vue
@@ -1,32 +1,86 @@
 <template>
-  <div
-    id="app-container"
-    class="sticky-header-container"
-    :class="getDarkModeClass(darkMode)"
-  >
-    <header-nav
-      class="apps-header"
-      backnavpath="/apps"
-      :title="'Address Book'"
-      id="header-nav"
-    />
+  <q-layout>
+    <q-page-container>
+      <q-page>
+        <div
+          id="app-container"
+          class="sticky-header-container"
+          :class="getDarkModeClass(darkMode)"
+        >
+          <header-nav
+            class="apps-header"
+            backnavpath="/apps"
+            :title="'Address Book'"
+            id="header-nav"
+          />
+      
+          <div class="q-px-md">
+            <!-- search bar & filter -->
+            <div class="row justify-around items-center">
+              <q-input
+                rounded
+                outlined
+                clearable
+                dense
+                label="Search name or address"
+                style="width: 75vw"
+              >
+                <template v-slot:prepend>
+                  <q-icon name="search"></q-icon>
+                </template>
+              </q-input>
+              <q-btn
+                round
+                icon="filter_alt"
+                color="primary"
+              />
+            </div>
 
-    <div>
-      yey
-    </div>
-  </div>
+            <!-- lists container -->
+            <div class="q-mt-md">
+              <!-- favorites list -->
+              <div v-if="favoritesList.length > 0">
+                yey
+              </div>
+  
+              <!-- contacts list -->
+               <div v-if="contactsList.length > 0">
+                yey
+               </div>
+  
+               <div v-else>
+                hehe
+               </div>
+            </div>
+          </div>
+
+          <!-- add new contact sticky button -->
+          <add-contact-sticky />
+        </div>
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
 import { getDarkModeClass } from 'src/utils/theme-darkmode-utils'
 
 import HeaderNav from 'src/components/header-nav.vue'
+import AddContactSticky from 'src/components/address-book/AddContactSticky.vue'
 
 export default {
   name: 'AddressBook',
 
   components: {
-    HeaderNav
+    HeaderNav,
+    AddContactSticky
+  },
+
+  data () {
+    return {
+      favoritesList: [],
+      contactsList: []
+    }
   },
 
   computed: {
