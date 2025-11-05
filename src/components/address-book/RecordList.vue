b259c0c54626794a4e88759d06b880048aa28eac
LukeHeimen <kdlanante@up.edu.ph>
2025-11-05 11:05:12 +0800

    Add RecordList custom component for displaying contacts list

---
+++ src/components/address-book/RecordList.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-list>
+    <template
+      v-for="record in list"
+      :key="record.letter_group"
+    >
+      <q-item-label header>
+        {{ record.letter_group.toLocaleUpperCase() }}
+      </q-item-label>
+
+      <q-card
+        class="q-mb-sm q-mx-md"
+        v-for="rec in record.data"
+        :key="rec.id"
+      >
+        <q-item
+          clickable
+          v-ripple
+        >
+          <q-item-section>
+            <q-item-label>{{ rec.name }}</q-item-label>
+            <q-item-label caption>
+              {{ rec.address_count }}
+              {{ rec.address_count === 1 ? 'address' : 'addresses' }}
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-card>
+    </template>
+  </q-list>
+</template>
+
+<script>
+export default {
+  name: 'RecordList',
+
+  props: {
+    list: { type: Array, default: new Array() }
+  }
+}
+</script>
--- src/pages/apps/address-book/index.vue
+++ src/pages/apps/address-book/index.vue
@@ -4,7 +4,7 @@
       <q-page>
         <div
           id="app-container"
-          class="sticky-header-container"
+          class="sticky-header-container text-bow"
           :class="getDarkModeClass(darkMode)"
         >
           <header-nav
@@ -13,49 +13,56 @@
             :title="'Address Book'"
             id="header-nav"
           />
+
+          <!-- loading skeletons animation -->
       
           <div class="q-px-md">
-            <!-- search bar & filter -->
-            <div class="row justify-around items-center">
+            <!-- search bar -->
+            <div
+              class="full-width q-mb-md"
+              id="search-filter-div"
+            >
               <q-input
                 rounded
                 outlined
                 clearable
                 dense
-                label="Search name or address"
-                style="width: 75vw"
+                label="Search name"
               >
                 <template v-slot:prepend>
                   <q-icon name="search"></q-icon>
                 </template>
               </q-input>
-              <q-btn
-                round
-                icon="filter_alt"
-                color="primary"
-              />
             </div>
 
             <!-- lists container -->
-            <div class="q-mt-md">
+            <div id="lists-container">
               <!-- favorites list -->
               <div v-if="favoritesList.length > 0">
-                yey
+                <record-list :list="favoritesList" />
               </div>
   
               <!-- contacts list -->
-               <div v-if="contactsList.length > 0">
-                yey
+               <div v-if="recordsList.length > 0">
+                <record-list :list="recordsList" />
                </div>
   
-               <div v-else>
-                hehe
+               <div
+                class="text-center text-h6 q-mt-md"
+                :class="darkMode ? 'text-grey-5' : 'text-grey-7'"
+                v-else
+              >
+                <p>Empty address book</p>
+                <p>
+                  Click on <q-icon name="mdi-account-plus" size="sm" />
+                  to add a new record
+                </p>
                </div>
             </div>
           </div>
 
-          <!-- add new contact sticky button -->
-          <add-contact-sticky />
+          <!-- add new record sticky button -->
+          <add-record-sticky />
         </div>
       </q-page>
     </q-page-container>
@@ -66,20 +73,120 @@
 import { getDarkModeClass } from 'src/utils/theme-darkmode-utils'
 
 import HeaderNav from 'src/components/header-nav.vue'
-import AddContactSticky from 'src/components/address-book/AddContactSticky.vue'
+import AddRecordSticky from 'src/components/address-book/AddRecordSticky.vue'
+import RecordList from 'src/components/address-book/RecordList.vue'
 
 export default {
   name: 'AddressBook',
 
   components: {
     HeaderNav,
-    AddContactSticky
+    AddRecordSticky,
+    RecordList
   },
 
   data () {
     return {
-      favoritesList: [],
-      contactsList: []
+      /** 
+       * id: Number
+       * name: String
+       * is_favorite: Boolean
+       * date_created: Date
+       * date_updated: Date
+       * address_list: Array
+       *   address: String
+       *   type: BCH | CT
+      */
+     favoritesList: [
+      {
+        letter_group: 'favorites',
+        data: [
+          {
+            id: 1,
+            name: '_hearty737',
+            address_count: 5
+          },
+          {
+            id: 1,
+            name: 'Fiona Apple',
+            address_count: 1
+          },
+          {
+            id: 1,
+            name: 'Wendy Darling',
+            address_count: 0
+          }
+        ]
+      },
+     ],
+     // non-favorite contacts
+     recordsList: [
+      {
+        letter_group: '...',
+        data: [
+          {
+            id: 1,
+            name: '_hearty737',
+            address_count: 5
+          },
+          {
+            id: 1,
+            name: '_hearty737',
+            address_count: 1
+          },
+          {
+            id: 1,
+            name: '_hearty737',
+            address_count: 0
+          }
+        ]
+      },
+      {
+        letter_group: 'a',
+        data: [
+          {
+            id: 1,
+            name: 'Alice Johnson',
+            address_count: 5
+          }
+        ]
+      },
+      {
+        letter_group: 'b',
+        data: [
+          {
+            id: 1,
+            name: 'Bob Smith',
+            address_count: 5
+          },
+        ]
+      },
+      {
+        letter_group: 'j',
+        data: [
+          {
+            id: 1,
+            name: 'Jane Doe',
+            address_count: 5
+          },
+          {
+            id: 1,
+            name: 'John Doe',
+            address_count: 5
+          },
+        ]
+      },
+      {
+        letter_group: 'l',
+        data: [
+          {
+            id: 1,
+            name: 'Laura Croft',
+            address_count: 5
+          }
+        ]
+      },
+     ]
     }
   },
 
@@ -91,6 +198,28 @@ export default {
 
   methods: {
     getDarkModeClass
+  },
+
+  mounted () {
+    const headerHeight = document.getElementById('header-nav').clientHeight
+    const searchFilterEl = document.getElementById('search-filter-div')
+    searchFilterEl.style.top = `${headerHeight}px`
+
+    const aboveDivsHeight = searchFilterEl.clientHeight + headerHeight
+    const listsContainerHeight = this.$q.screen.height - aboveDivsHeight - 70
+    document.getElementById('lists-container').style.height = `${listsContainerHeight}px`
   }
 }
 </script>
+
+<style lang="scss" scoped>
+#search-filter-div {
+  position: sticky;
+  z-index: 100;
+}
+
+#lists-container {
+  height: 100%;
+  overflow-y: auto;
+}
+</style>
