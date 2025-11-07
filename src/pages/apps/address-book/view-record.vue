f749827e1dc24dead994c4d7ead5e72380c30cbb
LukeHeimen <kdlanante@up.edu.ph>
2025-11-07 08:49:14 +0800

    Update UI for view record page

--- src/pages/apps/address-book/view-record.vue
+++ src/pages/apps/address-book/view-record.vue
@@ -16,14 +16,13 @@
 
       <!-- <template> -->
       <!-- buttons div -->
-      <div class="row justify-end items-center q-gutter-x-sm">
+      <!-- <div id="buttons-div" class="row justify-end items-center q-gutter-x-sm">
         <q-btn
           round
           flat
           :icon="record.is_favorite ? 'mdi-star' : 'mdi-star-outline'"
           color="primary"
         />
-        <!-- icon="mdi-star-outline" -->
         <q-btn
           round
           flat
@@ -36,22 +35,51 @@
           icon="delete"
           color="primary"
         />
-      </div>
+      </div> -->
+
       <!-- record details -->
       <div class="q-mt-sm">
-        <!-- name and date created -->
-        <q-card class="text-center q-pa-sm q-mb-md">
-          <p class="text-h6 q-mb-none">{{ record.name }}</p>
-          <p class="text-caption q-mb-none">
-            Created last {{ record.date_created }}
-          </p>
+        <q-card id="record-details" class="q-pa-md q-mb-md record-card">
+          <!-- name and date created -->
+          <div class="text-left">
+            <p class="text-h6 q-mb-none">{{ record.name }}</p>
+            <p class="text-caption q-mb-md">
+              Created last {{ record.date_created }}
+            </p>
+          </div>
+
+          <!-- buttons div -->
+          <div class="row justify-evenly items-center">
+            <q-btn
+              round
+              :outline="!record.is_favorite"
+              :icon="record.is_favorite ? 'mdi-star' : 'mdi-star-outline'"
+              color="primary"
+              @click="record.is_favorite = !record.is_favorite"
+            />
+            <q-btn
+              round
+              icon="edit"
+              color="primary"
+            />
+            <q-btn
+              round
+              icon="delete"
+              color="primary"
+            />
+          </div>
         </q-card>
 
         <!-- addresses list -->
         <div>
-          <span class="row text-subtitle1 text-weight-bold">Addresses List</span>
+          <span
+            id="addresses-list-label"
+            class="row text-subtitle1 text-weight-bold"
+          >
+            Addresses List
+          </span>
 
-          <div v-if="record.address_list.length > 0">
+          <div id="addresses-list" v-if="record.address_list.length > 0">
             <q-list>
               <q-item v-for="address in record.address_list">
                 <q-item-section>
@@ -64,6 +92,7 @@
                   <q-btn
                     icon-right="mdi-send"
                     color="primary"
+                    size="sm"
                   />
                 </q-item-section>
               </q-item>
@@ -102,6 +131,7 @@ export default {
         name: 'Name Name name_yey',
         is_favorite: false,
         date_created: new Date(),
+        // address_list: []
         address_list: [
           {
             address: 'bitcoincash:qze93uuw8vt8v358f7eupg3t4jtkpz8ltguhg62pde',
@@ -170,6 +200,24 @@ export default {
     console.log(this.$route.params)
 
     // retrieve details of params.id from watchtower
+
+    // set height of addresses-list
+    if (this.record?.address_list.length > 0) {
+      const headerHeight = document.getElementById('header-nav').clientHeight
+      const recordDetailsHeight = document.getElementById('record-details').clientHeight
+      const addressesListLabel = document.getElementById('addresses-list-label').clientHeight
+  
+      const aboveDivsHeight = headerHeight + recordDetailsHeight + addressesListLabel
+      const overallHeight = this.$q.screen.height - aboveDivsHeight - 75
+      document.getElementById('addresses-list').style.height = `${overallHeight}px`
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+#addresses-list {
+  height: 100%;
+  overflow-y: auto;
+}
+</style>
