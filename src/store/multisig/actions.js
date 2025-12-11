85df6ab7a96d5c910afff215ba327c13687e88c3
hantyrram <rongrammer@hotmail.com>
2025-12-11 11:31:02 +0800

    clean up store actions

--- src/store/multisig/actions.js
+++ src/store/multisig/actions.js
@@ -1,9 +1,7 @@
 import axios from 'axios'
 import {
-  CashAddressNetworkPrefix,
   decodeCashAddress,
 } from 'bitauth-libauth-v3'
-import * as ms from 'src/lib/multisig'
 import { generateAuthCredentialsForFirstSignerWithPrivateKey, generateAuthCredentialsForXPub } from 'src/utils/multisig-utils'
 import { watchtowerUtxoToCommonUtxo } from 'src/utils/utxo-utils'
 import Watchtower from 'src/lib/watchtower'
@@ -155,123 +153,6 @@ export async function deletePsbt ({ commit }, { pst, sync = false }) {
   }
   return pst
 }
-
-// export async function syncPst ({ commit, rootGetters }, { pst }) {
-  
-//   console.log('Syncing PST', pst)
-
-//   const authCredentials = await generateAuthCredentialsForFirstSignerWithPrivateKey({ 
-//     signers: pst.wallet.signers,
-//     walletVault: rootGetters['global/getVault']
-//   })
-
-//   console.log('Auth Credentials', authCredentials)
-//     if (authCredentials) {
-//       const watchtower = rootGetters['global/getWatchtowerBaseUrl']
-//       const response = await axios.post(
-//         `${watchtower}/api/multisig/psts/${pst.unsignedTransactionHash}`,
-//         { headers: { 'Content-Type': 'application/json', ...authCredentials } }
-//       )
-//     }
-//   return pst
-// }
-
-// export async function addPstPartialSignature({ commit }, { pst, inputIndex, partialSignature, sync = false }) {
-//   commit('addPstPartialSignature', { pst, inputIndex, partialSignature })
-//   if (sync) {
-//     const authCredentials = await generateAuthCredentialsForFirstSignerWithPrivateKey({ 
-//       multisigWallet,
-//       walletVault: rootGetters['global/getVault'] 
-//     })
-//     console.log('Auth Credentials', authCredentials)
-//     if (authCredentials) {
-//       const watchtower = rootGetters['global/getWatchtowerBaseUrl']
-//       const response = await axios.post(
-//         `${watchtower}/api/multisig/psts/${pst.unsignedTransactionHash}/sign/`,
-//         pst,
-//         { headers: { 'Content-Type': 'application/json', ...authCredentials } }
-//       )
-//       if (response.data?.id) {
-//         commit('updatePstId', { unsignedTransactionHash: pst.unsignedTransactionHash, newId: response.data.id })
-//       }
-//     }
-//   }
-//   return pst
-// }
-
-
-// export async function updateWalletLastIssuedDepositAddressIndex({ commit, rootGetters }, { wallet, lastIssuedDepositAddressIndex }) {
-//   commit('updateWalletLastIssuedDepositAddressIndex', { wallet, lastIssuedDepositAddressIndex })
-//   return
-//   const authCredentials = await generateAuthCredentialsForFirstSignerWithPrivateKey({ 
-//     wallet,
-//     walletVault: rootGetters['global/getVault'] 
-//   })
-//   if (authCredentials) {
-//     const watchtower = rootGetters['global/getWatchtowerBaseUrl']
-//     await axios.post(
-//       `${watchtower}/api/multisig/wallets/${wallet.id}/last-issued-deposit-address-index/`,
-//       { lastIssuedDepositAddressIndex },
-//       { headers: { 'Content-Type': 'application/json', ...authCredentials } }
-//     )
-//   }
-// }
-
-// export async function updateWalletLastIssuedChangeAddressIndex({ commit, rootGetters }, { wallet, lastIssuedChangeAddressIndex }) {
-//   commit('updateWalletLastIssuedChangeAddressIndex', { wallet, lastIssuedChangeAddressIndex })
-//   return
-//   const authCredentials = await generateAuthCredentialsForFirstSignerWithPrivateKey({ 
-//     wallet,
-//     walletVault: rootGetters['global/getVault'] 
-//   })
-//   if (authCredentials) {
-//     const watchtower = rootGetters['global/getWatchtowerBaseUrl']
-//     await axios.post(
-//       `${watchtower}/api/multisig/wallets/${wallet.id}/last-issued-change-address-index/`,
-//       { lastIssuedChangeAddressIndex },
-//       { headers: { 'Content-Type': 'application/json', ...authCredentials } }
-//     )
-//   }
-// }
-
-// export async function updateWalletLastUsedDepositAddressIndex({ commit, rootGetters }, { wallet, lastUsedDepositAddressIndex }) {
-//   console.log('Updating last used deposit address indexz', wallet, lastUsedDepositAddressIndex)
-//   commit('updateWalletLastUsedDepositAddressIndex', { wallet, lastUsedDepositAddressIndex })
-//   return
-//   const authCredentials = await generateAuthCredentialsForFirstSignerWithPrivateKey({ 
-//     wallet,
-//     walletVault: rootGetters['global/getVault'] 
-//   })
-//   if (authCredentials) {
-//     const watchtower = rootGetters['global/getWatchtowerBaseUrl']
-//     await axios.post(
-//       `${watchtower}/api/multisig/wallets/${wallet.id}/last-used-deposit-address-index/`,
-//       { lastUsedDepositAddressIndex },
-//       { headers: { 'Content-Type': 'application/json', ...authCredentials } }
-//     )
-//   }
-// }
-
-// export async function updateWalletLastUsedChangeAddressIndex({ commit, rootGetters }, { wallet, lastUsedChangeAddressIndex }) {
-//   commit('updateWalletLastUsedChangeAddressIndex', { wallet, lastUsedChangeAddressIndex })
-//   return
-//   const authCredentials = await generateAuthCredentialsForFirstSignerWithPrivateKey({ 
-//     wallet,
-//     walletVault: rootGetters['global/getVault'] 
-//   })
-//   if (authCredentials) {
-//     const watchtower = rootGetters['global/getWatchtowerBaseUrl']
-//     await axios.post(
-//       `${watchtower}/api/multisig/wallets/${wallet.id}/last-used-change-address-index/`,
-//       { lastUsedDepositAddressIndex },
-//       { headers: { 'Content-Type': 'application/json', ...authCredentials } }
-//     )
-//   }
-// }
-
-// export async function updatePstBroadcastResult({ commit }, { pst, broadcastResult }) {
-//   commit('updatePstBroadcastResult', { pst, broadcastResult })
-// }
 
 export async function subscribeWalletAddress ({ rootGetters }, address) {
   const watchtower = new Watchtower(rootGetters['global/isChipnet'])
