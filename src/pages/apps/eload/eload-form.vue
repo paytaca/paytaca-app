eb409529551cd340981fa8cc37f9495b80488081
SeanNikki123 <sslino@up.edu.ph>
2026-01-22 17:35:31 +0800

    WIP: buy load form page | fixed routing

---
+++ src/pages/apps/eload/eload-form.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="text-center text-black">
+		
+		<div v-for="type in purchase_type">
+			{{ type.name }}
+		</div>
+	</div>
+</template>
+<script>
+
+export default {
+	data() {
+		return {
+			purchase_type: [
+				{
+					'name': 'ELOAD'
+				},
+				{
+					'name': 'CABLE'
+				},
+				{
+					'name': 'GAMEPINS'
+				}
+			]
+		}
+	}
+}
+</script>
--- src/pages/apps/eload/index.vue
+++ src/pages/apps/eload/index.vue
@@ -1,6 +1,37 @@
 <template>
-	<h1>Hello World</h1>
+	<div id="app-container" class="row" :class="getDarkModeClass(darkMode)">
+		<HeaderNav title="Eload Service" backnavpath="/apps" class="header-nav" />
+		<router-view v-if="isloaded" :key="$route.path"></router-view>
+
+		<div v-else>
+			<div class="text-black">Add loader here</div>
+		</div>
+	</div>
 </template>
 <script>
-	
+import { getDarkModeClass } from 'src/utils/theme-darkmode-utils'
+import HeaderNav from 'src/components/header-nav.vue'
+
+export default{
+	data (){
+		return {
+			darkMode: this.$store.getters['darkmode/getStatus'],
+			isloaded: true, // change later			
+		}
+	},
+	components: {
+		HeaderNav
+	},
+	methods: {
+		getDarkModeClass
+	},
+	mounted () {
+		const vm = this
+		// Request Services & check api availability
+
+		if (vm.isloaded) {
+			vm.$router.push({ name: 'eload-service-form' })
+		}
+	}
+}
 </script>
