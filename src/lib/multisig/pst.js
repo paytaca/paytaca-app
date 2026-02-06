39c40d68aef70b3cfcb60a8ec8fe8526d5545b00
hantyrram <rongrammer@hotmail.com>
2026-02-06 22:21:03 +0800

    add transaction signature verifier function, add outpoint tx resolver function

--- src/lib/multisig/pst.js
+++ src/lib/multisig/pst.js
@@ -100,7 +100,11 @@ import {
   hash160,
   encodeLockingBytecodeP2sh20,
   decodeHdPublicKey,
-  decodeTransactionBch
+  decodeTransactionBch,
+  hash256,
+  secp256k1,
+  sha256,
+  numberToBinInt32TwosCompliment
 } from 'bitauth-libauth-v3'
 
 import { getCompiler, getWalletHash, MultisigWallet, sortPublicKeysBip67 } from './wallet.js'
@@ -109,6 +113,7 @@ import { bip32ExtractRelativePath } from './utils.js'
 import { Psbt } from './psbt.js'
 import { MultisigTransactionBuilder } from './transaction-builder.js'
 import { WatchtowerNetworkProvider } from './network.js'
+import { encodeSigningSerializationBCH, generateSigningSerializationBCH, generateSigningSerializationComponentsBCH } from '@bitauth/libauth'
 
 export const SIGNING_PROGRESS = {
   UNSIGNED: 'unsigned',
@@ -371,7 +376,6 @@ export const combine = (psts) => {
         }
         absorberPstInput.signatures[publicKeyOfPartialSignature] = partialSignaturesOfInput[publicKeyOfPartialSignature]
       }
-
     }
   }
   
@@ -516,6 +520,39 @@ export const publicKeySigned = ({ publicKey, pst }) => {
     if (!allInputsAreSigned.length) return false
 
     return allInputsAreSigned.every(isSigned => isSigned)
+}
+
+
+/**
+ * Context object for signature verification and transaction processing.
+ *
+ * @typedef {Object} Context
+ * @property {number} inputIndex - The index of the input being processed in the transaction.
+ * @property {Array} sourceOutputs - Array of source outputs for the transaction inputs.
+ * @property {Object} transaction - The transaction object (decoded).
+ *
+ * Verifies a Schnorr signature for a given transaction input context.
+ *
+ * @param {Object} params - The parameters for verification.
+ * @param {Uint8Array} params.signature - The Schnorr signature to verify.
+ * @param {Uint8Array} params.publicKey - The public key corresponding to the signature.
+ * @param {Context} params.context - Context object containing transaction, input index, and source outputs.
+ * @returns {boolean} True if signature is valid, otherwise false.
+ */
+export const verifyTransactionSignature = ({ signature, publicKey, redeemScript, context }) => {
+  console.log({ signature, publicKey, redeemScript, context })
+  const sigHashFlag = signature.slice(-1)[0]
+  const signingSerialization = generateSigningSerializationBCH(context, { 
+    coveredBytecode: redeemScript, 
+    signingSerializationType: new Uint8Array([sigHashFlag]) // Uint8Array([65]) for allOutputs
+  })
+  const sigHash = hash256(signingSerialization)
+  let signatureFormat = signature.length > 65 ? 'ecdsa' : 'schnorr'
+  if (signatureFormat === 'schnorr') {
+    const signatureVerificationResult = secp256k1.verifySignatureSchnorr(signature.slice(0, 64), publicKey, sigHash)
+    return signatureVerificationResult
+  }
+  return secp256k1.verifySignatureDERLowS(signature.slice(0, signature.length - 1), publicKey, sigHash)
 }
 
 export class Pst {
@@ -688,19 +725,19 @@ export class Pst {
       }
     } // end for
 
-    
     const signAttempt = generateTransaction({ ...transaction })
     
     if (signAttempt.success) return
 
     for (const [inputIndex, error] of Object.entries(signAttempt?.errors || {})) {
       const signerResolvedVariables = extractResolvedVariables({ ...signAttempt, errors: [error] })
-      const sigValue = Object.values(signerResolvedVariables)[0]
+      const signature = Object.values(signerResolvedVariables)[0]
       const inputUnlockingBytecode = transaction.inputs[inputIndex].unlockingBytecode
 
       const script = compileScript('lock', inputUnlockingBytecode.data, inputUnlockingBytecode.compiler.configuration)
       // const lockingScript = script.bytecode
       const redeemScript = script.reduce.bytecode
+
       const keyVariable = `${Object.keys(inputUnlockingBytecode.data.keys.privateKeys)[0]}.public_key`
       this.inputs[inputIndex].redeemScript = redeemScript
 
@@ -708,9 +745,9 @@ export class Pst {
         this.inputs[inputIndex].signatures = {}
       }
 
-      const signerPublicKeyForThisInput = binToHex(inputUnlockingBytecode.data.bytecode[keyVariable])
-      this.inputs[inputIndex].signatures[signerPublicKeyForThisInput] = sigValue
-
+      const signerPublicKey = inputUnlockingBytecode.data.bytecode[keyVariable]
+      this.inputs[inputIndex].signatures[binToHex(signerPublicKey)] = signature
+      
       if (this.options?.store) {
         this.save()
       }
@@ -750,7 +787,6 @@ export class Pst {
       allInputsAreSigned.push(
         publicKeySigned({ publicKey: publicKeyDerivedFromXpubForThisInput, pst: this })
       )
-
     }
 
     if (!allInputsAreSigned.length) return false
@@ -974,6 +1010,10 @@ export class Pst {
       walletHash: getWalletHash(this.wallet),
     }
 
+    if (this.metadata) {
+      data.metadata = this.metadata
+    }
+
     if (this.signedTransactionHash) {
       data.signedTransactionHash = this.signedTransactionHash
     }
@@ -987,6 +1027,29 @@ export class Pst {
     }
 
     return JSON.parse(JSON.stringify(data, Pst.exportSafeJSONReplacer))
+  }
+
+  /**
+   * Resolves the outpoint transaction of each input 
+   */
+  async resolveInputsTransactionData() {
+    for (const [i, input] of this.inputs.entries()) {
+      if (!input.outpointTransaction) {
+        const tx = await this.options.provider.getRawTransaction(binToHex(input.outpointTransactionHash))
+        input.outpointTransaction = hexToBin(tx)
+      }
+
+      const computedTransactionHash = hashTransaction(input.outpointTransaction)
+      if (computedTransactionHash !== binToHex(input.outpointTransactionHash)) {
+        throw new Error(`Input ${i} references a previous transaction that doesn't much its outpoint transaction hash.`)
+      }
+
+      const decodedTx = decodeTransactionBch(input.outpointTransaction)
+      const expectedSourceOutput = decodedTx.outputs[input.outpointIndex]
+      if (!expectedSourceOutput) {
+        throw new Error(`Input ${i} references an output that does not exist in the previous transaction.`)
+      } 
+    }
   }
 
   async toPsbt(version = 145) {
@@ -1008,7 +1071,6 @@ export class Pst {
         throw new Error(`Input ${i} references an output that does not exist in the previous transaction.`)
       } 
     }
-
 
     const psbt = new Psbt()
     psbt.encode(this, version)
@@ -1057,30 +1119,21 @@ export class Pst {
   }
 
   async upload() {
-    
     if (!this.options?.coordinationServer) return
-    
-    const remotePst = await this.options?.coordinationServer?.uploadPst(this)
-    
-    
-    if (!remotePst?.id || !(/^[0-9]+$/.test(remotePst.id))) {
-      this.isSynced = false
-      return
-    }
+    const response = 
+      await this.options?.coordinationServer?.uploadProposal({
+        payload: {
+          wallet: this.wallet.id,
+          proposal: (await this.toPsbt()).toString(),
+          proposalFormat: 'psbt'
+        },
+        authCredentialsGenerator: this.wallet
+      })
 
-    this.isSynced = true
-    
-    if (!this.updatedAt) {
-      Object.assign(this, remotePst)
-      this.save()
-      return this
-    }
-
-    if (new Date(remotePst.updatedAt) > new Date(this.updatedAt)) {
-      Object.assign(this, remotePst)
+    if (response?.id && !this.id) {
+      this.id = response.id
       this.save()
     }
-    return this
   }
 
   /**
