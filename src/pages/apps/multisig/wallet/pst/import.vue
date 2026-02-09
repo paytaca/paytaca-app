38aa53230f643b168b07c738d4b7fc118c2a7df5
hantyrram <rongrammer@hotmail.com>
2026-02-09 10:54:32 +0800

    notify error when combining different transactions

--- src/pages/apps/multisig/wallet/pst/import.vue
+++ src/pages/apps/multisig/wallet/pst/import.vue
@@ -1,31 +1,40 @@
 <template>
     <div class="static-container">
-      <div id="app-container" class="sticky-header-container multisig-app" :class="getDarkModeClass(darkMode)">
+      <div id="app-container" class="sticky-header-container multisig-app text-bow" :class="getDarkModeClass(darkMode)">
         <HeaderNav
-          :title="$t(route.query.title || 'Import Tx')"
+          :title="$t('Import')"
           :backnavpath="`/apps/multisig/wallet/${route.params.wallethash}`"
           class="q-px-sm apps-header gift-app-header"
         />
           <div class="text-caption text-center text-bow-muted">
             <q-banner class="q-ma-lg rounded" :class="getDarkModeClass(darkMode)">
-              <q-icon name="info" color="grad" size="sm" class="q-mr-sm"></q-icon>{{ $t(route.query.description || 'Import Tx Proposal') }}
+              <q-icon name="info" color="grad" size="sm" class="q-mr-sm"></q-icon>{{ $t(route.query.description || 'Import') }}
             </q-banner>
           </div>
-          <div class="flex column text-center q-gutter-y-xl" style="margin-top: 20px;">
+          <div class="flex column text-center q-gutter-y-xl">
               <div>
-                <q-btn @click="$router.push({ name: 'qr-reader', query: { hideFooter: true, hideGenerateQR: true, hideUploadQR: true } })" color="primary" class="button-default" :class="darkMode ? 'dark' : 'light'" round size="lg">
-                  <q-icon class="default-text-color"  size="lg" name="qr_code" />
+                <q-btn @click="$router.push({ name: 'qr-reader', query: { hideFooter: true, hideGenerateQR: true, hideUploadQR: true } })" color="primary" class="button-default" :class="darkMode ? 'dark' : 'light'" round>
+                  <q-icon class="default-text-color"  name="qr_code" />
                 </q-btn>
-                <div class="q-pt-xs text-center text-capitalize">{{ $t('ScanQrCode') }}</div>
+                <div class="q-pt-xs text-center text-capitalize text-bold">{{ $t('ScanQrCode') }}</div>
+                <div class="text-subtitle-2 text-center text-bow text-bow-muted">{{ $t('ScanPstQRCodeDescription', {}, 'Ask one of your cosigners to show you the transaction QR code then scan it') }}</div>
               </div>
               <div>
-                <q-btn color="primary" class="button-default" :class="darkMode ? 'dark' : 'light'" round size="lg">
-                  <q-icon class="default-text-color"  size="lg" name="upload_file" @click="importPsbt"/>
+                <q-btn color="primary" class="button-default" :class="darkMode ? 'dark' : 'light'" round>
+                  <q-icon class="default-text-color"  name="upload_file" @click="importPsbt"/>
                 </q-btn>
-                <div class="q-pt-xs text-center text-capitalize">{{ $t('LoadFromFile') }}</div>
+                <div class="q-pt-xs text-center text-capitalize text-bold">{{ $t('FromFile') }}</div>
+                <div class="text-subtitle-2 text-center text-bow-muted">{{ $t('PstFromFileDescription', {}, 'Browse and import a Partially Signed Transaction File you get from your cosigner') }}</div>
               </div>
               <div>
-                <q-btn size="lg" :label="$t('Cancel')" @click="router.back()" color="red" v-close-popup></q-btn>
+                <q-btn color="primary" class="button-default" :class="darkMode ? 'dark' : 'light'" round>
+                  <q-icon class="default-text-color"  name="upload_file" @click="importPsbt"/>
+                </q-btn>
+                <div class="q-pt-xs text-center text-capitalize text-bold">{{ $t('FromServer') }}</div>
+                <div class="text-subtitle-2 text-center text-bow-muted">{{ $t('PstFromServerDescription', {}, 'Download a Partially Signed Transaction Submitted by a Cosigner from Paytaca\'s Multisig Coordinator Server') }}</div>
+              </div>
+              <div>
+                <q-btn :label="$t('Cancel')" @click="router.back()" color="red" v-close-popup></q-btn>
               </div>
           </div>
        </div>
@@ -48,7 +57,9 @@ import HeaderNav from 'components/header-nav'
 import { getDarkModeClass } from 'src/utils/theme-darkmode-utils'
 import { useRoute, useRouter } from 'vue-router'
 import { Pst } from 'src/lib/multisig'
+import { useQuasar } from 'quasar'
 const $store = useStore()
+const $q = useQuasar()
 const { t: $t } = useI18n()
 const route = useRoute()
 const router = useRouter()
@@ -69,25 +80,43 @@ const onUpdatePstFile = (file) => {
     const reader = new FileReader()
     reader.onload = () => {
       const importedPst = Pst.import(reader.result)
-      if (canonicalPsbt.value) {
-        const canonicalPst = Pst.import(canonicalPsbt.value)
-        canonicalPst.combine([importedPst])
-        canonicalPst.setStore($store)
-        canonicalPst.save()
-      } else {
-        importedPst.setStore($store)
-        importedPst.save()
-      }
-      router.push({ 
-        name: 'app-multisig-wallet-pst-view', 
-        params: { 
-          unsignedtransactionhash: importedPst.unsignedTransactionHash,
-          wallethash: route.params.wallethash
+      console.log('IMPORTED PST', importedPst)
+      
+      canonicalPsbt.value = 
+        $store.getters['multisig/getPsbtByUnsignedTransactionHash'](route.params.unsignedtransactionhash)
+      console.log('Canocical', canonicalPsbt.value)
+
+      try {
+        if (canonicalPsbt.value) {
+          const canonicalPst = Pst.import(canonicalPsbt.value)
+          console.log('canonicalPst', canonicalPst)
+          canonicalPst.combine([importedPst])
+          canonicalPst.setStore($store)
+          canonicalPst.save()
+        } else {
+          importedPst.setStore($store)
+          importedPst.save()
         }
-      })
+        router.push({ 
+          name: 'app-multisig-wallet-pst-view', 
+          params: { 
+            unsignedtransactionhash: importedPst.unsignedTransactionHash,
+            wallethash: route.params.wallethash
+          }
+        })  
+      } catch (error) {
+        $q.notify({
+          message: error?.toString(),
+          color: 'negative'
+        })
+      }
+      
     }
-    reader.onerror = (err) => {
-      console.err(err)
+    reader.onerror = (error) => {
+      $q.notify({
+        message: error?.toString(),
+        color: 'negative'
+      })
     }
     reader.readAsText(file)
   }
@@ -96,7 +125,7 @@ const onUpdatePstFile = (file) => {
 onMounted(() => {
   if (route.params.unsignedTransactionHash) { 
     canonicalPsbt.value = 
-      $store.getters['multisig/getPsbtByUnsignedTransactionHash'](route.params.unsignedTransactionHash)
+      $store.getters['multisig/getPsbtByUnsignedTransactionHash'](route.params.unsignedtransactionhash)
   }
 })
 
