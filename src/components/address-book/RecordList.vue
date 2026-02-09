c33ec70782a29a49f88dfdf25c76cd8ea610c78c
LukeHeimen <kdlanante@up.edu.ph>
2026-02-09 16:15:00 +0800

    Wrap address book records in a template to conditionally render only when data is present

--- src/components/address-book/RecordList.vue
+++ src/components/address-book/RecordList.vue
@@ -4,35 +4,37 @@
       v-for="record in list"
       :key="record.letter_group"
     >
-      <q-item-label 
-        header 
-        class="text-weight-bold"
-        :id="`letter-group-${record.letter_group}`"
-      >
-        {{ record.letter_group.toLocaleUpperCase() }}
-      </q-item-label>
-
-      <q-card
-        flat
-        bordered
-        class="q-mb-sm q-mx-md record-card"
-        v-for="rec in record.data"
-        :key="rec.id"
-      >
-        <q-item
-          clickable
-          v-ripple
-          @click="$router.push(`view-record/${rec.id}/`)"
+      <template v-if="record.data.length > 0">
+        <q-item-label 
+          header 
+          class="text-weight-bold"
+          :id="`letter-group-${record.letter_group}`"
         >
-          <q-item-section>
-            <q-item-label id="name-label">{{ rec.name }}</q-item-label>
-            <q-item-label caption>
-              {{ rec.address_count }}
-              {{ rec.address_count === 1 ? 'address' : 'addresses' }}
-            </q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-card>
+          {{ record.letter_group.toLocaleUpperCase() }}
+        </q-item-label>
+  
+        <q-card
+          flat
+          bordered
+          class="q-mb-sm q-mx-md record-card"
+          v-for="rec in record.data"
+          :key="rec.id"
+        >
+          <q-item
+            clickable
+            v-ripple
+            @click="$router.push(`view-record/${rec.id}/`)"
+          >
+            <q-item-section>
+              <q-item-label id="name-label">{{ rec.name }}</q-item-label>
+              <q-item-label caption>
+                {{ rec.address_count }}
+                {{ rec.address_count === 1 ? 'address' : 'addresses' }}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-card>
+      </template>
     </template>
   </q-list>
 </template>
