01c62be87b68eeb39e1a0011ff88c6d0257f48cf
LukeHeimen <kdlanante@up.edu.ph>
2026-02-10 16:34:20 +0800

    Adjust raiseNotifyError and raiseNotifySuccess to add an optional icon parameter

--- src/utils/notify-utils.js
+++ src/utils/notify-utils.js
@@ -1,20 +1,22 @@
 import { Notify } from "quasar"
 
-export function raiseNotifySuccess (message, timeout = 3000, position = 'bottom') {
+export function raiseNotifySuccess (message, timeout=3000, position='bottom', icon='check-circle') {
   Notify.create({
     type: 'positive',
     timeout,
     message,
-    position
+    position,
+    icon
   })
 }
 
-export function raiseNotifyError (message, timeout = 3000, position = 'bottom') {
+export function raiseNotifyError (message, timeout=3000, position='bottom', icon='error') {
   Notify.create({
     type: 'negative',
     color: 'red-4',
     timeout,
     message,
-    position
+    position,
+    icon
   })
 }
