3cb6aedf05aaf29799e9ab083353d1e7638699f4
hantyrram <rongrammer@hotmail.com>
2026-02-12 23:17:14 +0800

    refactor, move multisig coordination helpers to separate module

---
+++ src/lib/multisig/coordination.js
@@ -0,0 +1,61 @@
+import {
+    decodeHdPublicKey,
+    decodeHdPrivateKey,
+    deriveHdPublicKey,
+    utf8ToBin,
+    binToHex,
+    sha256,
+    secp256k1,
+  } from 'bitauth-libauth-v3'
+
+export const generateCoordinationServerSignature = (privateKey, message) => {
+    const hash = sha256.hash(utf8ToBin(message))
+    const schnorr = secp256k1.signMessageHashSchnorr(privateKey, hash)
+    const der = secp256k1.signMessageHashDER(privateKey, hash)
+    return `schnorr=${binToHex(schnorr)};der=${binToHex(der)}`
+}
+  
+export const generateCoordinatorServerIdentityFromXprv = ({ name, xprv }) => {
+    const { hdPublicKey } = deriveHdPublicKey(xprv)
+    const privateKey = decodeHdPrivateKey(xprv).node.privateKey
+    const publicKey  = decodeHdPublicKey(hdPublicKey).node.publicKey
+    const message = `iam:${binToHex(publicKey)}:${Date.now()}`
+    return {
+      name,
+      publicKey: binToHex(publicKey),
+      message,
+      signature: generateCoordinationServerSignature(privateKey, message)
+    }
+}
+  
+export const generateCoordinatorServerIdentityFromMnemonic = ({ name, mnemonic, network = 'mainnet', hdPath = `m/4501'/145'/0'/0'` }) => {
+    const { hdPrivateKey } = deriveHdKeysFromMnemonic({ 
+      mnemonic,
+      network, 
+      hdPath
+    })
+    return generateCoordinatorServerIdentityFromXprv({ name, xprv: hdPrivateKey, network })
+}
+  
+export const generateCoordinationServerCredentialsFromXprv = ({ xprv }) => {
+    const { hdPublicKey } = deriveHdPublicKey(xprv)
+    const privateKey = decodeHdPrivateKey(xprv).node.privateKey
+    const publicKey  = decodeHdPublicKey(hdPublicKey).node.publicKey
+    const message = `multisig:${Date.now()}`
+    
+    return {
+        'X-Auth-PubKey': binToHex(publicKey),
+        'X-Auth-Signature': generateCoordinationServerSignature(privateKey, message),
+        'X-Auth-Message': message
+    }
+  }
+
+export const generateCoordinationServerCredentialsFromMnemonic = ({ mnemonic, network = 'mainnet', hdPath = `m/4501'/145'/0'/0'` }) => {
+    const { hdPrivateKey } = deriveHdKeysFromMnemonic({ 
+      mnemonic,
+      network, 
+      hdPath 
+    })
+    return generateCoordinationServerCredentialsFromXprv({ xprv: hdPrivateKey })
+}
+  
--- src/lib/multisig/wallet.js
+++ src/lib/multisig/wallet.js
@@ -147,6 +147,7 @@ import { PsbtWallet, WALLET_MAGIC } from './psbt-wallet.js'
 import { retryWithBackoff } from './utils.js'
 import { encryptECIESMessage } from './ecies.js'
 import { BsmsDescriptor, BsmsKeyRecord } from './bsms.js'
+import { generateCoordinationServerCredentialsFromMnemonic } from './coordination.js'
 
 export const getLockingData = ({ signers, addressDerivationPath }) => {
   const signersWithPublicKeys = derivePublicKeys({ signers, addressDerivationPath })
@@ -379,57 +380,6 @@ export const getMasterFingerprintUintLE = (mnemonic) => {
         )
       )
   )
-}
-
-export const generateCoordinationServerSignature = (privateKey, message) => {
-  const hash = sha256.hash(utf8ToBin(message))
-  const schnorr = secp256k1.signMessageHashSchnorr(privateKey, hash)
-  const der = secp256k1.signMessageHashDER(privateKey, hash)
-  return `schnorr=${binToHex(schnorr)};der=${binToHex(der)}`
-}
-
-export const generateCoordinatorServerIdentityFromXprv = ({ name, xprv }) => {
-  const { hdPublicKey } = deriveHdPublicKey(xprv)
-  const privateKey = decodeHdPrivateKey(xprv).node.privateKey
-  const publicKey  = decodeHdPublicKey(hdPublicKey).node.publicKey
-  const message = `iam:${binToHex(publicKey)}:${Date.now()}`
-  return {
-    name,
-    publicKey: binToHex(publicKey),
-    message,
-    signature: generateCoordinationServerSignature(privateKey, message)
-  }
-}
-
-export const generateCoordinatorServerIdentityFromMnemonic = ({ name, mnemonic, network = 'mainnet', hdPath = `m/4501'/145'/0'/0'` }) => {
-  const { hdPrivateKey } = deriveHdKeysFromMnemonic({ 
-    mnemonic,
-    network, 
-    hdPath
-  })
-  return generateCoordinatorServerIdentityFromXprv({ name, xprv: hdPrivateKey, network })
-}
-
-export const generateCoordinationServerCredentialsFromXprv = ({ xprv }) => {
-  const { hdPublicKey } = deriveHdPublicKey(xprv)
-  const privateKey = decodeHdPrivateKey(xprv).node.privateKey
-  const publicKey  = decodeHdPublicKey(hdPublicKey).node.publicKey
-  const message = `multisig:${Date.now()}`
-  
-  return {
-      'X-Auth-PubKey': binToHex(publicKey),
-      'X-Auth-Signature': generateCoordinationServerSignature(privateKey, message),
-      'X-Auth-Message': message
-  }
-}
-
-export const generateCoordinationServerCredentialsFromMnemonic = ({ mnemonic, network = 'mainnet', hdPath = `m/4501'/145'/0'/0'` }) => {
-  const { hdPrivateKey } = deriveHdKeysFromMnemonic({ 
-    mnemonic,
-    network, 
-    hdPath 
-  })
-  return generateCoordinationServerCredentialsFromXprv({ xprv: hdPrivateKey })
 }
 
 export class PriceOracle {
