d031d632bbb329e6187d7d0ef350f7173bcb42b0
LukeHeimen <kdlanante@up.edu.ph>
2026-02-18 09:52:25 +0800

    Add missing id param for address book and gifts

--- src/pages/apps/index.vue
+++ src/pages/apps/index.vue
@@ -192,6 +192,7 @@ export default {
           smartBCHOnly: false
         },
         {
+          id: 'address-book',
           name: this.$t('AddressBook'),
           description: this.$t('Apps.AddressBook.Description', {}, 'Create and manage contacts and addresses.'),
           iconName: 'mdi-book-account',
@@ -201,6 +202,7 @@ export default {
           smartBCHOnly: false
         },
         {
+          id: 'gifts',
           name: this.$t('Gifts'),
           description: this.$t('Apps.Gifts.Description', {}, 'Create and redeem BCH gifts.'),
           iconName: 'mdi-gift',
