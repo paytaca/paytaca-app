340bb21b1c354d9abfd6667d06e382a80164e11a
sherelot <chillprocessor@gmail.com>
2026-03-18 10:56:05 +0800

    added card header component

---
+++ src/pages/apps/card/CardPageHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row items-center q-pa-md">
+    <q-btn
+      flat
+      round
+      dense
+      icon="arrow_back"
+      color="primary"
+      @click="$router.back()"
+    />
+    <div class="col">
+      <h5 class="text-primary text-weight-bold text-center q-ma-none">{{ title }}</h5>
+    </div>
+    <div class="q-pa-xs" style="width: 32px"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardPageHeader',
+  props: {
+    title: {
+      type: String,
+      default: 'Card Management'
+    }
+  }
+}
+</script>
