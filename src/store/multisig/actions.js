72764eba7e4b6a9245c7c914009ac5eb759afa39
hantyrram <rongrammer@hotmail.com>
2026-04-20 10:14:45 +0800

    implement address discovery worker listener/handler

---
+++ src/store/multisig/actions.js
@@ -0,0 +1,69 @@
+import { getMultisigWorker } from 'src/workers/index';
+
+export async function discoverAddresses({ commit, state, rootState, rootGetters }, payload) {
+
+    let workerId = ''
+    try {
+        const worker = getMultisigWorker();
+        const network = rootState.isChipnet ? 'chipnet': 'mainnet'
+        
+        const onProgress = (workerProgress) => {  
+            workerId = workerProgress.id
+            if (!state.workers?.[workerProgress.id]) {
+                commit('addWorker', {
+                    id: workerProgress.id,
+                    status: 'started'
+                })
+            }
+            // Save progress
+            if (workerProgress.success) {
+                commit('updateWalletLastUsedDepositAddressIndex', { 
+                    wallet: payload.multisigWallet,
+                    lastUsedDepositAddressIndex: workerProgress.lastUsedDepositAddressIndex,
+                    network
+                })
+                commit('updateWalletLastUsedChangeAddressIndex', { 
+                    wallet: payload.multisigWallet,
+                    lastUsedChangeAddressIndex: workerProgress.lastUsedChangeAddressIndex,
+                    network
+                })
+            }
+        }
+
+        const options = {
+            watchtowerBaseUrl: rootGetters['global/getWatchtowerBaseUrl'],
+            gapLimit: payload.addressDiscoveryGapLimit || state.settings.addressDiscoveryGapLimit || 20,
+            network: rootState.global.isChipnet ? 'testnet': 'mainnet',
+            fullScan: payload.fullScan || true
+        }
+
+        const workerResult = await worker.startAddressDiscovery(
+            payload.multisigWallet, 
+            payload.options || options,
+            onProgress
+        );
+        
+        if (workerResult.success) {
+            commit('updateWalletLastUsedDepositAddressIndex', { 
+                wallet: payload.multisigWallet,
+                lastUsedDepositAddressIndex: workerResult.lastUsedDepositAddressIndex,
+                network
+            })
+            commit('updateWalletLastUsedChangeAddressIndex', { 
+                wallet: payload.multisigWallet,
+                lastUsedChangeAddressIndex: workerResult.lastUsedChangeAddressIndex,
+                network
+            })
+            commit('removeWorker', workerResult.id)
+
+        }
+    } catch (error) {
+        if (workerId) {
+            commit('updateWorkerStatus', { id: workerId, status: 'error', error: error }) 
+        }
+    } 
+}
+
+export async function cleanupWorker() {    
+    getMultisigWorker().stop();
+}
--- src/store/multisig/state.js
+++ src/store/multisig/state.js
@@ -3,13 +3,16 @@ export default function () {
   return {
     wallets: [],
     walletsUtxos: {},
+    /* { [workerId]: { type: string, status: 'started'|'paused'|'error' } } */
+    workers: {}, 
     /**
      * Partially Signed Transactions (Adopting Bitauth's Term)
      */
     transactions: [], /* { transaction, sourceOutputs, address, sessionRequest? } [] */
        
     settings: {
-      defaultSignerWalletIndex: 0 /* The index of the personal wallet that'll be used as signer */
+      defaultSignerWalletIndex: 0, /* The index of the personal wallet that'll be used as signer */
+      addressDiscoveryGapLimit: 20
     },
     psts: [],
     psbts: []
