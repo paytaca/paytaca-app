6dd4cf05a84783c2910618b44d8f90e190239e30
LukeHeimen <kdlanante@up.edu.ph>
2026-05-08 10:47:37 +0800

    Adjust referral code to display in all caps

--- src/components/rewards/dialogs/ReferralQrDialog.vue
+++ src/components/rewards/dialogs/ReferralQrDialog.vue
@@ -96,7 +96,7 @@ export default {
       return this.$store.getters['darkmode/getStatus']
     },
     referralCodeFull () {
-      return `${this.promoType}-${this.code}-${this.promoId}`
+      return `${this.promoType}-${this.code}-${this.promoId}`.toUpperCase()
     },
     copyButtonIcon () {
       if (this.copySuccess) return 'check'
