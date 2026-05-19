bdbfff99d6e8511f88792ee4be75e526686fce62
sherelot <chillprocessor@gmail.com>
2026-05-19 15:46:37 +0800

    remove all-cards route since carousel now displays all cards directly

--- src/router/routes.js
+++ src/router/routes.js
@@ -413,11 +413,6 @@ const routes = [
             name: 'card-details',
             component: () => import('src/pages/apps/card/card.vue')
           },
-          {
-            path: 'all-cards',
-            name: 'all-cards',
-            component: () => import('src/pages/apps/card/allCardsPage.vue')
-          }
         ]
       },
       {
