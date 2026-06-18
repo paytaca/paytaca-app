a89900601fa16316b5c6a6872906b9304b2871b0
nikkanap <nenaputo@up.edu.ph>
2026-06-18 17:04:02 +0800

    Replace all instances of lotId with bidId, buyer with bidder, and seller with auctioneer

--- src/auction/contract.js
+++ src/auction/contract.js
@@ -10,13 +10,13 @@ class AuctionEscrowContract {
    * Creates a new RampContract instance.
    * @param {Object} publicKeys - The public keys of the parties involved in the contract.
    * @param {Object} fees - The fees associated with the contract.
-   * @param {Object} lotId - The lot associated with the contract.
+   * @param {Object} bidId - The lot associated with the contract.
    * @param {boolean} [isChipnet=true] - A boolean indicating whether the contract is on the Chipnet network or not. Defaults to true.
    */
-  constructor (publicKeys, fees, lotId, isChipnet = true) {
+  constructor (publicKeys, fees, bidId, isChipnet = true) {
     this.publicKeys = publicKeys
     this.fees = fees
-    this.lotId = lotId
+    this.bidId = bidId
     this.network = (isChipnet) ? "chipnet" : "mainnet";
 
     this.initialize()
@@ -26,23 +26,23 @@ class AuctionEscrowContract {
     this.provider = new ElectrumNetworkProvider(this.network)
 
     const arbiterPkh = this.getPubKeyHash(this.publicKeys.arbiter)
-    const buyerPkh = this.getPubKeyHash(this.publicKeys.buyer) // include lang muna
-    const sellerPkh = this.getPubKeyHash(this.publicKeys.seller)
+    const bidderPkh = this.getPubKeyHash(this.publicKeys.bidder) // include lang muna
+    const auctioneerPkh = this.getPubKeyHash(this.publicKeys.auctioneer)
     const servicerPkh = this.getPubKeyHash(this.publicKeys.servicer)
 
     this.hash = this.sha256Hash(
       this.publicKeys.arbiter,
-      this.publicKeys.buyer, // include lang muna
-      this.publicKeys.seller,
+      this.publicKeys.bidder, // include lang muna
+      this.publicKeys.auctioneer,
       this.publicKeys.servicer,
-      this.lotId
+      this.bidId
     )
     console.log(this.hash)
 
     const contractParams = [
       arbiterPkh,
-      buyerPkh, // include lang muna
-      sellerPkh,
+      bidderPkh, // include lang muna
+      auctioneerPkh,
       servicerPkh,
       BigInt(parseInt(this.fees.serviceFee)),
       BigInt(parseInt(this.fees.arbitrationFee)),
@@ -66,8 +66,8 @@ class AuctionEscrowContract {
    * @param {number} timestamp - The timestamp to be included in the hash.
    * @returns {Promise<string>} A promise that resolves with the generated hash as a string.
    */
-  sha256Hash (arbiterPk, buyerPk, sellerPk, servicerPk, lotId) {
-    const message = arbiterPk + buyerPk + sellerPk + servicerPk + lotId
+  sha256Hash (arbiterPk, bidderPk, auctioneerPk, servicerPk, bidId) {
+    const message = arbiterPk + bidderPk + auctioneerPk + servicerPk + bidId
     return CryptoJS.SHA256(message).toString()
   }
 
