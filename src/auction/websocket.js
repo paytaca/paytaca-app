43a008b1127de601a87b1d2f68a4e2c51848cc22
nikkanap <nenaputo@up.edu.ph>
2026-09-17 12:28:39 +0800

    Update websocket urls

--- src/auction/websocket.js
+++ src/auction/websocket.js
@@ -2,21 +2,21 @@ const APIURL = new URL(process.env.AUCTION_HUB_API)
 
 // change ws to ws if listening to HTTPS not HTTP
 export function callIndexAuctionWebsocket(){
-  return new WebSocket(`wss://${APIURL.hostname}/ws/all-auctions/`) 
+  return new WebSocket(`ws://${APIURL.hostname}:8000/ws/index/`) 
 }
 
 export function callActivityWebsocket(username){
-  return new WebSocket(`wss://${APIURL.hostname}/ws/activity/${username}/`) 
+  return new WebSocket(`ws://${APIURL.hostname}:8000/ws/activity/${username}/`) 
 }
 
 export function callAuctionWebsocket(auctionId){
-  return new WebSocket(`wss://${APIURL.hostname}/ws/auction/${auctionId}/`) 
+  return new WebSocket(`ws://${APIURL.hostname}:8000/ws/auction/${auctionId}/`) 
 }
 
 export function callLotWebsocket(lotId){
-  return new WebSocket(`wss://${APIURL.hostname}/ws/lot/${lotId}/`)   
+  return new WebSocket(`ws://${APIURL.hostname}:8000/ws/lot/${lotId}/`)   
 }
 
 export function callRefundCountdownWebsocket(deliveryTrackingId){
-  return new WebSocket(`wss://${APIURL.hostname}/ws/refund-countdown/${deliveryTrackingId}/`) 
+  return new WebSocket(`ws://${APIURL.hostname}:8000/ws/refund-countdown/${deliveryTrackingId}/`) 
 } 
